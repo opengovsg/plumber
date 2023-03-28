31fe06551a9dc53491bddd8e7f74cd6968886bf2
Ian Chen <ian-@gmx.com>
2023-03-28 11:05:56 +0000

    feat: verify formsg credentials

--- packages/backend/src/apps/formsg/auth/verify-credentials.ts
+++ packages/backend/src/apps/formsg/auth/verify-credentials.ts
@@ -1,25 +1,44 @@
 import { IGlobalVariable } from '@plumber/types'
 
+import formsgSdk from '@opengovsg/formsg-sdk'
+import get from 'lodash.get'
+
 // ref: https://stackoverflow.com/questions/475074/regex-to-parse-or-validate-base64-data/475217#475217
 const BASE64_REGEX =
   /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/
 
-const verifyFormExists = async ($: IGlobalVariable) => {
+const verifyFormCreds = async ($: IGlobalVariable) => {
+  if (!$.auth.data?.formId) {
+    throw new Error('No form id provided')
+  }
   let formTitle = ''
+  let publicKey = ''
   try {
-    const { data } = await $.http.get(`/v3/forms/${$.auth.data?.formId}`)
-    formTitle = data.form.title
+    const { data } = await $.http.get(`/v3/forms/${$.auth.data.formId}`)
+    formTitle = get(data, 'form.title')
+    publicKey = get(data, 'form.publicKey')
   } catch (error) {
     if (error.response?.status === 404) {
       if (error.response.data?.isPageFound) {
         // form is valid but not public
-        formTitle = error.response.data.formTitle
+        throw new Error('Ensure form is public')
       }
     }
+    throw new Error('Form not found')
   }
+
   if (!formTitle) {
     throw new Error('Form does not exist')
   }
+
+  if (!publicKey) {
+    throw new Error('Form is not a storage mode form')
+  }
+
+  if (!formsgSdk().crypto.valid(publicKey, $.auth.data?.privateKey as string)) {
+    throw new Error('Invalid secret key')
+  }
+
   await $.auth.set({
     screenName: `${$.auth.data.formId} - ${formTitle}`,
   })
@@ -35,8 +54,8 @@ const verifySecretKeyFormat = ($: IGlobalVariable) => {
 }
 
 const verifyCredentials = async ($: IGlobalVariable) => {
-  await verifyFormExists($)
   verifySecretKeyFormat($)
+  await verifyFormCreds($)
 }
 
 export default verifyCredentials
