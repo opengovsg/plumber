e5178c8629f6d4194672560c179b5912783400da
Ian Chen <ian-@gmx.com>
2023-04-06 15:26:46 +0800

    chore: increase channel limit for slack

--- packages/backend/src/apps/slack/dynamic-data/list-channels/index.ts
+++ packages/backend/src/apps/slack/dynamic-data/list-channels/index.ts
@@ -13,7 +13,13 @@ export default {
       error: null,
     }
 
-    const response = await $.http.get('/conversations.list')
+    const response = await $.http.get('/conversations.list', {
+      params: {
+        exclude_archived: true,
+        types: 'public_channel',
+        limit: 1000,
+      },
+    })
 
     if (response.data.ok === false) {
       throw new Error(response.data)
