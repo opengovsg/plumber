67212d1c569ac38fc92d087d2272bce30fdcfea1
Ian Chen <ian-@gmx.com>
2023-05-11 19:50:07 +0800

    chore: add env to dd-trace

--- packages/backend/src/helpers/tracer.ts
+++ packages/backend/src/helpers/tracer.ts
@@ -1,11 +1,14 @@
 import tracer from 'dd-trace'
 
+import appConfig from '../config/app'
+
 import logger from './logger'
 
 tracer.init({
   logInjection: true,
   service: 'plumber',
   logger,
+  env: appConfig.appEnv,
 })
 
 export default tracer
