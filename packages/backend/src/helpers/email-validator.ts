8feb287695aef17667a7cdbd03e25ca38b2aa83e
Kuan Wee Loong <weeloong@open.gov.sg>
2023-06-12 17:47:51 +0800

    feat: enable whitelisting VAPT accounts

--- packages/backend/src/helpers/email-validator.ts
+++ packages/backend/src/helpers/email-validator.ts
@@ -1,11 +1,28 @@
 import validator from 'email-validator'
+import assert from 'node:assert'
+
+// FIXME (ogp-weeloong): Remove after VAPT
+function getWhitelistedEmails(): ReadonlyArray<string> {
+  try {
+    const emails = JSON.parse(process.env.WHITELISTED_EMAILS)
+    assert(Array.isArray(emails))
+    return emails
+  } catch {
+    return []
+  }
+}
+
+const WHITELISTED_EMAILS = getWhitelistedEmails()
 
 export function validateAndParseEmail(input: unknown): string | false {
   if (typeof input !== 'string') {
     return false
   }
   const email = input.toLowerCase().trim()
-  if (!validator.validate(email) || !email.endsWith('.gov.sg')) {
+  if (
+    !validator.validate(email) ||
+    !(email.endsWith('.gov.sg') || WHITELISTED_EMAILS.includes(email))
+  ) {
     return false
   }
   return email
