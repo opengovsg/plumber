0fda766c374dd1aaed3fc8e389652891f47ccb60
Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-06-13 14:06:49 +0800

    Enable Material & Chakra co-existence (#35)
    * chore: bump design system to 1.1.0
    
    * chore: bump mui to 5.12.0
    
    * feat: make ThemeProvider load both chakra and material UI themes
    
    * fix: restore powerinput margins removed by chakra
    
    * fix: login page not stretching vertically

--- packages/frontend/src/pages/Landing/index.tsx
+++ packages/frontend/src/pages/Landing/index.tsx
@@ -1,8 +1,5 @@
 import { LightMode } from '@chakra-ui/react'
-import { ThemeProvider } from '@opengovsg/design-system-react'
 import { Footer } from 'components/Footer'
-
-import { theme } from '../../theme'
 
 import { AutomationLanding } from './AutomationLanding'
 import { BottomLanding } from './BottomLanding'
@@ -10,14 +7,12 @@ import { MainLanding } from './MainLanding'
 
 const Landing = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <LightMode>
-        <MainLanding />
-        <AutomationLanding />
-        <BottomLanding />
-        <Footer />
-      </LightMode>
-    </ThemeProvider>
+    <LightMode>
+      <MainLanding />
+      <AutomationLanding />
+      <BottomLanding />
+      <Footer />
+    </LightMode>
   )
 }
 
