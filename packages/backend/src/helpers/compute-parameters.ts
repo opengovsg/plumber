541012dc8941f1a1851af4ca24594d7929627b34
Kuan Wee Loong <weeloong@open.gov.sg>
2023-07-01 19:22:58 +0800

    feat: support variable substitution in array parameters

--- packages/backend/src/helpers/compute-parameters.ts
+++ packages/backend/src/helpers/compute-parameters.ts
@@ -6,6 +6,32 @@ import Step from '../models/step'
 
 const variableRegExp = /({{step\.[\da-zA-Z-]+(?:\.[\da-zA-Z-_]+)+}})/g
 
+function findAndSubstituteVariables(
+  rawValue: string,
+  executionSteps: ExecutionStep[],
+): string {
+  const parts = rawValue.split(variableRegExp)
+
+  return parts
+    .map((part: string) => {
+      const isVariable = part.match(variableRegExp)
+      if (isVariable) {
+        const stepIdAndKeyPath = part.replace(/{{step.|}}/g, '') as string
+        const [stepId, ...keyPaths] = stepIdAndKeyPath.split('.')
+        const keyPath = keyPaths.join('.')
+        const executionStep = executionSteps.find((executionStep) => {
+          return executionStep.stepId === stepId
+        })
+        const data = executionStep?.dataOut
+        const dataValue = get(data, keyPath)
+        return dataValue
+      }
+
+      return part
+    })
+    .join('')
+}
+
 export default function computeParameters(
   parameters: Step['parameters'],
   executionSteps: ExecutionStep[],
@@ -13,30 +39,22 @@ export default function computeParameters(
   const entries = Object.entries(parameters)
   return entries.reduce((result, [key, value]: [string, unknown]) => {
     if (typeof value === 'string') {
-      const parts = value.split(variableRegExp)
-
-      const computedValue = parts
-        .map((part: string) => {
-          const isVariable = part.match(variableRegExp)
-          if (isVariable) {
-            const stepIdAndKeyPath = part.replace(/{{step.|}}/g, '') as string
-            const [stepId, ...keyPaths] = stepIdAndKeyPath.split('.')
-            const keyPath = keyPaths.join('.')
-            const executionStep = executionSteps.find((executionStep) => {
-              return executionStep.stepId === stepId
-            })
-            const data = executionStep?.dataOut
-            const dataValue = get(data, keyPath)
-            return dataValue
-          }
-
-          return part
-        })
-        .join('')
+      const computedValue = findAndSubstituteVariables(value, executionSteps)
 
       return {
         ...result,
         [key]: computedValue,
+      }
+    } else if (Array.isArray(value)) {
+      const computedValues = value.map((element) =>
+        typeof element === 'string'
+          ? findAndSubstituteVariables(element, executionSteps)
+          : element,
+      )
+
+      return {
+        ...result,
+        [key]: computedValues,
       }
     }
 
