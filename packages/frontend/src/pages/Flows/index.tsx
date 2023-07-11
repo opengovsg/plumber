43fd9da3368c7baff43050af6fc2652a65d27121
Kuan Wee Loong <weeloong@open.gov.sg>
2023-07-11 19:25:50 +0800

    fix: padding on small screens

--- packages/frontend/src/pages/Flows/index.tsx
+++ packages/frontend/src/pages/Flows/index.tsx
@@ -103,7 +103,7 @@ export default function Flows(): React.ReactElement {
 
   return (
     <Box sx={{ py: 3 }}>
-      <Container maxW={1152} p={0}>
+      <Container maxW={1152} py={0}>
         <Grid container sx={{ mb: [0, 3] }} columnSpacing={1.5} rowSpacing={3}>
           <Grid container item xs sm alignItems="center" order={{ xs: 0 }}>
             <PageTitle>{formatMessage('flows.title')}</PageTitle>
