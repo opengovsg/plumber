a1c3ffef177cc8f8ea8dd1f3c6775712c5066b77
Kuan Wee Loong <weeloong@open.gov.sg>
2023-07-11 18:43:28 +0800

    chore: refactor getDataOutMetadata inside triggers and actions

--- packages/backend/src/apps/formsg/triggers/new-submission/index.ts
+++ packages/backend/src/apps/formsg/triggers/new-submission/index.ts
@@ -1,4 +1,8 @@
-import { IGlobalVariable } from '@plumber/types'
+import {
+  IDataOutMetadata,
+  IExecutionStep,
+  IGlobalVariable,
+} from '@plumber/types'
 
 import isEmpty from 'lodash/isEmpty'
 
@@ -57,5 +61,60 @@ export default defineTrigger({
         },
       })
     }
+  },
+
+  async getDataOutMetadata(
+    executionStep: IExecutionStep,
+  ): Promise<IDataOutMetadata> {
+    const data = executionStep.dataOut
+    if (!data) {
+      return null
+    }
+
+    const fieldMetadata: IDataOutMetadata = Object.create(null)
+
+    for (const [fieldId, fieldData] of Object.entries(data.fields)) {
+      fieldMetadata[fieldId] = {
+        question: {
+          type: 'text',
+          isVisible: true,
+          label: fieldData.order ? `Question ${fieldData.order}` : null,
+          order: fieldData.order ? fieldData.order : null,
+        },
+        answer: {
+          type: 'text',
+          isVisible: true,
+          label: fieldData.order ? `Response ${fieldData.order}` : null,
+          order: fieldData.order ? fieldData.order + 0.1 : null,
+        },
+        fieldType: { isVisible: false },
+        order: { isVisible: false },
+      }
+    }
+
+    return {
+      fields: fieldMetadata,
+      submissionId: { isVisible: false },
+    }
+
+    // Reference dataOut
+    // ---
+    // {
+    //   "fields": {
+    //     "647edbd2026dc800116b21f9": {
+    //       "answer": "zzz",
+    //       "question": "What is the air speed velocity of an unladen swallow?",
+    //       "fieldType": "textfield"
+    //       "order": 2
+    //     },
+    //     "648fe18a9175ce001196b3d5": {
+    //       "answer": "aaaa",
+    //       "question": "What is your name?",
+    //       "fieldType": "textfield"
+    //       "order": 1
+    //     }
+    //   },
+    //   "submissionId": "649306c1ac8851001149af0a"
+    // }
   },
 })
