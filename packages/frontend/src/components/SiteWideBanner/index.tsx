fae60ffbd92b1f129e6b892353e4b025e10a8ad6
m0nggh <malcolmong27@gmail.com>
2023-08-04 12:50:00 +0800

    add initial draft for removing banner with LD

--- packages/frontend/src/components/SiteWideBanner/index.tsx
+++ packages/frontend/src/components/SiteWideBanner/index.tsx
@@ -1,28 +1,53 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
 import { Box, IconButton } from '@mui/material'
-import { getItemForSession, setItemForSession } from 'helpers/storage'
+import { getItem, getItemForSession, setItemForSession } from 'helpers/storage'
 import useFormatMessage from 'hooks/useFormatMessage'
+import { LaunchDarklyContext } from 'contexts/LaunchDarkly'
+import { set } from 'lodash'
 
-const BANNER_TEXT_ID = 'bannerText'
+const LAUNCH_DARKLY_BANNER_KEY = 'app_banner_display'
+const EMPTY_BANNER_MESSAGE = ''
 
 const SiteWideBanner = (): JSX.Element | null => {
-  const formatMessage = useFormatMessage()
-  // seems like this cant return empty string, so using _ as hacky empty string alternative
-  const message = formatMessage(BANNER_TEXT_ID)
+  const [bannerMessage, setBannerMessage] = useState('')
   const [showBanner, setShowBanner] = useState(false)
-  useEffect(() => {
-    const bannerTextStored = getItemForSession('hide-banner')
-    setShowBanner(message !== bannerTextStored)
-  }, [])
 
+  const launchDarkly = useContext(LaunchDarklyContext)
+
+  useEffect(() => {
+    const bannerMessageStored = getItemForSession('banner-text')
+    // either banner should never be displayed based on LD or banner is 'closed'
+    setShowBanner(
+      !!bannerMessageStored && bannerMessageStored !== EMPTY_BANNER_MESSAGE,
+    )
+  }, [showBanner])
+
+  // TODO: check why use callback when there are no dependencies?
   const closeBanner = useCallback(() => {
     setShowBanner(false)
-    setItemForSession('hide-banner', message)
+    setItemForSession('banner-text', EMPTY_BANNER_MESSAGE)
   }, [])
 
-  // this means the banner text is empty, so dont show the banner
-  if (message === BANNER_TEXT_ID) {
+  // check for feature flag (takes time to load) to display banner: by default it should be not visible
+  useEffect(() => {
+    if (
+      launchDarkly.flags &&
+      launchDarkly.flags[LAUNCH_DARKLY_BANNER_KEY] !== 'none' // this is the value set in LD
+    ) {
+      // message needs to be fetched everytime the page is re-rendered
+      const message = launchDarkly.flags[LAUNCH_DARKLY_BANNER_KEY]
+      setBannerMessage(message)
+      if (getItemForSession('banner-text') === null) {
+        // banner should only be enabled once at the start
+        setShowBanner(true)
+        setItemForSession('banner-text', message)
+      }
+    }
+  }, [launchDarkly])
+
+  // this means the banner text is empty, so dont show the banner even if session storage is not cleared yet!
+  if (bannerMessage === EMPTY_BANNER_MESSAGE) {
     return null
   }
   return (
@@ -44,7 +69,7 @@ const SiteWideBanner = (): JSX.Element | null => {
         },
       }}
     >
-      {message}
+      {bannerMessage}
       <IconButton
         size="small"
         onClick={closeBanner}
