75b56ee33d1fc9e72d9246ba665066d90d29599e
m0nggh <malcolmong27@gmail.com>
2023-09-11 10:30:45 +0800

    migrate to chakra for flowrow

--- packages/frontend/src/components/FlowRow/index.tsx
+++ packages/frontend/src/components/FlowRow/index.tsx
@@ -1,18 +1,28 @@
 import type { IFlow } from '@plumber/types'
 
 import * as React from 'react'
-import { BiChevronRight } from 'react-icons/bi'
+import { BiChevronRight, BiSolidCircle } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
-import { Card } from '@chakra-ui/react'
-import CardActionArea from '@mui/material/CardActionArea'
-import { Badge, IconButton } from '@opengovsg/design-system-react'
+import {
+  Card,
+  CardBody,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import {
+  Badge,
+  BadgeLeftIcon,
+  IconButton,
+} from '@opengovsg/design-system-react'
 import FlowAppIcons from 'components/FlowAppIcons'
 import FlowContextMenu from 'components/FlowContextMenu'
 import * as URLS from 'config/urls'
 import useFormatMessage from 'hooks/useFormatMessage'
 import { DateTime } from 'luxon'
-
-import { Apps, CardContent, ContextMenu, Title, Typography } from './style'
 
 type FlowRowProps = {
   flow: IFlow
@@ -43,66 +53,110 @@ export default function FlowRow(props: FlowRowProps): React.ReactElement {
   return (
     <>
       <Card
-        // sx={{ mb: 1 }}
-        mb={5}
         boxShadow="none"
         _hover={{ bg: '#FEF8FB' }}
         borderRadius="0"
         borderBottom="1px solid"
-        borderBottomColor="base.divider.subtle"
+        borderBottomColor="base.divider.medium"
       >
-        <CardActionArea component={Link} to={URLS.FLOW(flow.id)}>
-          <CardContent>
-            <Apps direction="row" gap={1} sx={{ gridArea: 'apps' }}>
-              <FlowAppIcons steps={flow.steps} />
-            </Apps>
+        <CardBody
+          sx={{ cursor: 'pointer' }}
+          p={0}
+          as={Link}
+          to={URLS.FLOW(flow.id)}
+        >
+          <Grid
+            templateAreas={{
+              base: `
+                "apps menu"
+                "title menu"
+              `,
+              sm: `"apps title menu"`,
+            }}
+            gridTemplateRows={{ base: 'auto auto', sm: 'auto' }}
+            gridTemplateColumns={{
+              base: 'minmax(0, auto) min-content',
+              sm: 'calc(30px * 3 + 8px * 2) minmax(0, auto) min-content',
+            }}
+            gap={6}
+            alignItems="center"
+            p="1.5rem 2rem"
+          >
+            <GridItem area="apps">
+              <HStack>
+                <FlowAppIcons steps={flow.steps} />
+              </HStack>
+            </GridItem>
 
-            <Title
-              justifyContent="center"
-              alignItems="flex-start"
-              spacing={1}
-              sx={{ gridArea: 'title' }}
-            >
-              <Typography variant="h6" noWrap>
-                {flow?.name}
-              </Typography>
-
-              <Typography variant="caption">
-                {isUpdated &&
-                  formatMessage('flow.updatedAt', {
-                    datetime: relativeUpdatedAt,
-                  })}
-                {!isUpdated &&
-                  formatMessage('flow.createdAt', {
-                    datetime: relativeCreatedAt,
-                  })}
-              </Typography>
-            </Title>
-
-            <ContextMenu>
-              <Badge
-                style={{
-                  borderRadius: '3.125rem',
-                  padding: '0.25rem 0.5rem',
-                }}
-                colorScheme={flow?.active ? 'success' : 'grey'}
-                variant="subtle"
+            <GridItem area="title">
+              <VStack
+                alignItems="flex-start"
+                justifyContent="center"
+                spacing={2}
               >
-                {formatMessage(flow?.active ? 'flow.published' : 'flow.draft')}
-              </Badge>
+                <Text
+                  whiteSpace="nowrap"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  display="inline-block"
+                  w="100%"
+                  maxW="85%"
+                  textStyle="subhead-1"
+                >
+                  {flow?.name}
+                </Text>
+                <Text
+                  display="inline-block"
+                  w="100%"
+                  maxW="85%"
+                  color="base.content.medium"
+                  textStyle="body-2"
+                >
+                  {isUpdated &&
+                    formatMessage('flow.updatedAt', {
+                      datetime: relativeUpdatedAt,
+                    })}
+                  {!isUpdated &&
+                    formatMessage('flow.createdAt', {
+                      datetime: relativeCreatedAt,
+                    })}
+                </Text>
+              </VStack>
+            </GridItem>
+            <GridItem area="menu">
+              <Flex alignItems="center" gap={1.5}>
+                <Badge
+                  style={{
+                    borderRadius: '3.125rem',
+                    padding: '0.25rem 0.5rem',
+                  }}
+                  colorScheme={flow?.active ? 'success' : 'grey'}
+                  variant="subtle"
+                >
+                  <React.Fragment>
+                    <BadgeLeftIcon boxSize={2} mr={2} as={BiSolidCircle} />
+                    <Text textStyle="body-2">
+                      {formatMessage(
+                        flow?.active ? 'flow.published' : 'flow.draft',
+                      )}
+                    </Text>
+                  </React.Fragment>
+                </Badge>
 
-              <IconButton
-                aria-label="open context menu"
-                icon={<BiChevronRight />}
-                size="md"
-                variant="clear"
-                color="interaction.sub.default"
-                ref={contextButtonRef}
-                onClick={onContextMenuClick}
-              />
-            </ContextMenu>
-          </CardContent>
-        </CardActionArea>
+                <IconButton
+                  aria-label="open context menu"
+                  icon={<BiChevronRight />}
+                  size="md"
+                  variant="clear"
+                  color="interaction.sub.default"
+                  ref={contextButtonRef}
+                  onClick={onContextMenuClick}
+                  border={0}
+                />
+              </Flex>
+            </GridItem>
+          </Grid>
+        </CardBody>
       </Card>
 
       {anchorEl && (
