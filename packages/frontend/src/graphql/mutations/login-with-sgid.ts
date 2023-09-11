6f4d9701bf4d55402077dfe5ca3083fe4840a671
Kuan Wee Loong <weeloong@open.gov.sg>
2023-09-11 19:49:45 +0800

    chore: forgot frontend mutation bit

--- packages/frontend/src/graphql/mutations/login-with-sgid.ts
+++ packages/frontend/src/graphql/mutations/login-with-sgid.ts
@@ -3,7 +3,11 @@ import { gql } from '@apollo/client'
 export const LOGIN_WITH_SGID = gql`
   mutation LoginWithSgid($input: LoginWithSgidInput!) {
     loginWithSgid(input: $input) {
-      nextUrl
+      publicOfficerEmployments {
+        # Temporary only; next PRs will fetch more data to render list if user
+        # has multiple work emails.
+        __typename
+      }
     }
   }
 `
