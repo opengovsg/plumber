b4e02d7fee0a35d8862749e5fdc753da400f09fd
Ian Chen <ian-@gmx.com>
2023-09-13 17:14:37 +0800

    feat: read launch darkly client id from app config

--- packages/frontend/src/config/app.ts
+++ packages/frontend/src/config/app.ts
@@ -1,9 +1,40 @@
-type Config = {
-  [key: string]: string
+interface AppConfig {
+  launchDarklyClientId: string
+  sgidClientId: string
+  env: string
+  version: string
 }
 
-const config: Config = {
-  graphqlUrl: `/graphql`,
+function getAppConfig(): AppConfig {
+  const env = import.meta.env.MODE
+  const version = import.meta.env.PACKAGE_VERSION
+  const commonEnv = {
+    env,
+    version,
+  }
+
+  switch (env) {
+    case 'prod':
+      return {
+        launchDarklyClientId: '64bf4b539077f112ef24e4ae',
+        sgidClientId: 'TBC',
+        ...commonEnv,
+      }
+    case 'staging':
+      return {
+        launchDarklyClientId: '65016ca0b45b7712e6c95703',
+        sgidClientId: 'TBC',
+        ...commonEnv,
+      }
+    default:
+      return {
+        launchDarklyClientId: '64bf4b539077f112ef24e4ad',
+        sgidClientId: 'TBC',
+        ...commonEnv,
+      }
+  }
 }
 
-export default config
+const appConfig = getAppConfig()
+
+export default appConfig
