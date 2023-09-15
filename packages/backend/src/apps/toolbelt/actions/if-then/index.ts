d45e1aa5903ae6e61ee5fefcd1c99236f766b0fd
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-09-15 17:27:33 +0800

    PLU-81:: [IF-THEN-21] Optimize if-then to O(n) processing per overall run (#190)

--- packages/backend/src/apps/toolbelt/actions/if-then/index.ts
+++ packages/backend/src/apps/toolbelt/actions/if-then/index.ts
@@ -4,8 +4,6 @@ import defineAction from '@/helpers/define-action'
 import Flow from '@/models/flow'
 import Step from '@/models/step'
 
-const MAX_BRANCHES = 50
-
 function evalCondition(parameters: IJSONObject): boolean {
   const { field, is, condition, value } = parameters
 
@@ -47,188 +45,6 @@ function shouldTakeBranch($: IGlobalVariable): boolean {
   return conditions.every((condition) =>
     evalCondition(condition as IJSONObject),
   )
-}
-
-/**
- * This function computes the step to skip to if a branch is not taken. It's
- * slightly tricky to understand - if you want to read the explanation, buckle
- * up.
- *
- * @param runningStep The currently running branch step.
- * @param ifThenSteps All if-then steps in the flow.
- * @returns The step to skip to, or null if the flow should end instead.
- */
-function getStepToSkipTo(runningStep: Step, ifThenSteps: Step[]): Step | null {
-  /*
-   * The main problem we're dealing with is that branch depth can be undefined
-   * if the user creates a branch via the "Select app & event" substep; for such
-   * cases, it's not immediately apparent what is the next step to skip to.
-   *
-   * First, let's define a _branch series_ as "a sequence of branch steps of the
-   * same depth that don't cross a parent branch". For example:
-   * =====
-   * [T = trigger S = normal step, B = branch]
-   *
-   * T
-   * |
-   * B1
-   *   \
-   *    B1.1
-   *      \
-   *       S
-   *      /
-   *    B1.2
-   *      \
-   *       S
-   *     /
-   *    /
-   * B2
-   *   \
-   *    B2.1
-   *      \
-   *       S
-   *
-   * - B1 and B2 form a branch series with depth = 0.
-   * - B1.1 and B1.2 form another branch series with depth = 1.
-   * - B2.1 forms another branch series by itself, also with with depth = 1.
-   * =====
-   *
-   * Next, also keep in mind these lemmas:
-   *
-   * _Lemma 1:_ Only the _1st branch step_ of a branch series can have undefined
-   * depth. Other branches in the series have to be created via our createBranch
-   * mutation on the front end, which sets the depth explicitly.
-   *
-   * _Lemma 2:_ The user cannot re-order branches.
-   *
-   * Next, let's describe the problem that having an undefined depth presents:
-   * ===
-   * T
-   * |
-   * B1
-   *   \
-   *    B1.1
-   *       \
-   *       B1.1.1
-   *          \
-   *            S
-   *          /
-   *       B1.1.2
-   *          \
-   *            S
-   *          /
-   *         /
-   *    B1.2
-   *   /
-   * B2
-   *
-   * Suppose B1, B1,1 and B1.1.1 all have undefined depths, and I am currently
-   * at step B1.1. I don't know if I should skip to B1.1.2, B1.2 or B2, because
-   * I don't know my depth.
-   *
-   * - Simply jumping to the first later branch step with a concrete depth does
-   *   not work, because B1.1.2 has a concrete depth (by lemma 1) and we know
-   *   that's the wrong step.
-   * - Another idea is to iterate through later branch steps, and keep track of
-   *   the number of steps with undefined depths we've seen thus far; this count
-   *   represents a "depth delta".
-   *
-   *   We could conceivably decrement this delta when we see transitions to a
-   *   shallower depth (i.e. bigger depth number to smaller depth number) and
-   *   jump to the 1st branch step where delta = 0. However, this doesn't work
-   *   because:
-   *
-   *   = We don't know if transitioning from an undefined depth number to a
-   *     concrete depth number is going to a deeper depth or shallower depth.
-   *   = We may need to decrement the delta by more than 1 during such
-   *     transititons, but we can't tell when this is the case.
-   *
-   *     Concrete counter-example - if we are currently at B1.1, and all branch
-   *     steps except for B2 have an undefined depth, we will never realise that
-   *     we have to jump to B2, This is because B1.1.1.1 to B2 represents going
-   *     shallower in depth by 3 levels, but we can't know this - we can't do
-   *     comparisons since B1.1's depth is undefined.
-   *
-   *     T
-   *     |
-   *     B1
-   *       \
-   *        B1.1
-   *           \
-   *           B1.1.1
-   *               \
-   *             B1.1.1.1
-   *               /
-   *             /
-   *           /
-   *         /
-   *       /
-   *     B2
-   *       \
-   *        S
-   * ===
-   *
-   * Thus, we have no choice but to solve this the brute force way: start from
-   * the beginning of the pipe to correctly re-compute branch depths for steps
-   * with undefined depths, and from these, find the right jump.
-   *
-   * If we start from the beginning of the pipe, by lemma 1, we know for sure
-   * that every undefined depth we encounter must represent going deeper by
-   * exactly one level; thus we know the exact value of all undefined depths.
-   * By lemma 1 and 2, we know that any concrete depth we encounter must be
-   * correct, so we can safely perform depth comparisons.
-   *
-   * With this, the approach boils down to finding the first branch step whose
-   * depth <= the currently running step's depth.
-   *
-   * NOTE: There is a small optimization where if the currently running step's
-   * depth is concrete, we can use another approach which doesn't need to start
-   * from the beginning of the pipe. But that approach is also O(n); to keep
-   * code complexity down, I decided not to implement it.
-   *
-   */
-
-  let depth = -1
-  let runningStepDepth = null
-
-  // WARNING: This is a naive but simple implementation - it's O(n) for each
-  // branch step, leading to O(n * b) for the entire test run. This means pipes
-  // have to be limited to around ~50 branches max, otherwise we risk web tier
-  // starvation since our executeFlow is completely run on our web machines
-  // instead of workers.
-  //
-  // FIXME (ogp-weeloong): later PR to optimize to O(n) per entire test run;
-  // we actually only need to compute this once for entire pipe. Achieving O(n)
-  // will require extra code to pass "once-per-test-run" data (e.g. memoization)
-  // and I want to put it in separate PR for easier review.
-  if (ifThenSteps.length > MAX_BRANCHES) {
-    throw new Error(`Max number of branches (${MAX_BRANCHES}) exceeded!`)
-  }
-
-  for (const step of ifThenSteps) {
-    const stepDepth = parseInt(step.parameters.depth as string)
-
-    if (isNaN(stepDepth)) {
-      // If depth is NaN, then `step` must be the 1st step of a new branch
-      // series that the user just created, so we have gone down in depth.
-      depth += 1
-    } else {
-      // Otherwise, user is editing a previously published pipe, or the 2nd+
-      // step in the branch series. Either way, the stored depth is correct.
-      depth = stepDepth
-    }
-
-    if (step.id === runningStep.id) {
-      runningStepDepth = depth
-      continue
-    }
-
-    if (runningStepDepth !== null && depth <= runningStepDepth) {
-      return step
-    }
-  }
-
-  return null
 }
 
 export default defineAction({
@@ -319,42 +135,6 @@ export default defineAction({
     },
   ],
 
-  async testRun($) {
-    const isBranchTaken = shouldTakeBranch($)
-    $.setActionItem({
-      raw: { isBranchTaken },
-    })
-
-    if (isBranchTaken) {
-      return {}
-    }
-
-    // For test runs, nextStepIdIfSkipped is not populated yet, so we compute
-    // the next step manually.
-    // FIXME (ogp-weeloong): THE ABOVE IS A LIE NOW, NEXT PR FIXES THIS.
-    const runningStep = await Step.query()
-      .findById($.step.id)
-      .withGraphFetched({ flow: { steps: true } })
-    const ifThenSteps = runningStep.flow.steps.filter(
-      (step) =>
-        step.appKey === 'toolbelt' &&
-        step.type === 'action' &&
-        step.key === 'ifThen',
-    )
-    const stepToSkipTo = getStepToSkipTo(runningStep, ifThenSteps)
-
-    return stepToSkipTo?.id
-      ? {
-          nextStep: {
-            command: 'jump-to-step',
-            stepId: stepToSkipTo.id,
-          },
-        }
-      : {
-          nextStep: { command: 'stop-execution' },
-        }
-  },
-
   async run($) {
     const isBranchTaken = shouldTakeBranch($)
     $.setActionItem({
@@ -368,11 +148,8 @@ export default defineAction({
     const nextStepId = $.step.parameters.nextStepIdIfSkipped
 
     if (nextStepId === null || nextStepId === undefined) {
-      // Abort instead of calling getStepToSkipTo, which is O(n) with DB
-      // queries. Invoking that function at scale risks starving our worker
-      // nodes if we have lots of pipes with this problem.
       throw new Error(
-        'nextStepIdIfSkipped is unset in a published pipe, which should not be possible.',
+        'nextStepIdIfSkipped is unset, which should not be possible.',
       )
     }
 
@@ -397,7 +174,7 @@ export default defineAction({
     )
 
     //
-    // 1st pass - Depth
+    // 1st pass - Compute concrete depths
     //
 
     const depths = new Map<Step['id'], number>()
@@ -424,15 +201,42 @@ export default defineAction({
     //
 
     const stepIdsToSkipTo = new Map<Step['id'], Step['id'] | null>()
+    const stepsToBeUpdated: Step[] = []
 
     for (const step of ifThenSteps) {
-      /**
-       * DANGEROUS: O(n^2), branch limit applied within function. Later PR will
-       * address this (as per FIXME in getStepToSkipTo)
-       */
-      const stepToSkipTo = getStepToSkipTo(step, ifThenSteps)
-      stepIdsToSkipTo.set(step.id, stepToSkipTo?.id)
+      if (stepsToBeUpdated.length === 0) {
+        stepsToBeUpdated.push(step)
+        continue
+      }
+
+      const prevStepDepth = depths.get(
+        stepsToBeUpdated[stepsToBeUpdated.length - 1].id,
+      )
+      const currStepDepth = depths.get(step.id)
+
+      // If we went deeper, continue.
+      if (currStepDepth > prevStepDepth) {
+        stepsToBeUpdated.push(step)
+        continue
+      }
+
+      // Otherwise, the current step must be the next branch for all earlier
+      // steps that have the same or deeper depth.
+      while (
+        stepsToBeUpdated.length > 0 &&
+        currStepDepth <=
+          depths.get(stepsToBeUpdated[stepsToBeUpdated.length - 1].id)
+      ) {
+        const earlierStep = stepsToBeUpdated.pop()
+        stepIdsToSkipTo.set(earlierStep.id, step.id)
+      }
+
+      stepsToBeUpdated.push(step)
     }
+
+    // Note: If there are still steps remaining in stepsToBeUpdated, they all
+    // don't have a next branch, and will instead terminate the pipe if skipped.
+    // So we can ignore them.
 
     //
     // Update DB with results
