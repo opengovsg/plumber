297dc1fcd53fe1b97013dfcd61bebdec80fa3b4a
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-09-15 17:15:38 +0800

    PLU-81: [IF-THEN-14] Implement branch deletion (#183)

--- packages/frontend/src/components/FlowStepGroup/Content/IfThen/Branch.tsx
+++ packages/frontend/src/components/FlowStepGroup/Content/IfThen/Branch.tsx
@@ -1,12 +1,26 @@
 import { type IFlow, type IStep } from '@plumber/types'
 
-import { useMemo } from 'react'
-import { Flex, Text, useDisclosure } from '@chakra-ui/react'
+import { type MouseEventHandler, useCallback, useMemo, useRef } from 'react'
+import { BiTrashAlt } from 'react-icons/bi'
+import { useMutation } from '@apollo/client'
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Flex,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { Button, IconButton } from '@opengovsg/design-system-react'
 import NestedEditor from 'components/FlowStepGroup/NestedEditor'
 import {
   type StepDisplayOverridesContextData,
   StepDisplayOverridesProvider,
 } from 'contexts/StepDisplayOverrides'
+import { DELETE_STEP } from 'graphql/mutations/delete-step'
 
 interface BranchProps {
   flow: IFlow
@@ -35,8 +49,34 @@ export default function Branch(props: BranchProps): JSX.Element {
     [initialStep.id, initialStep.parameters.branchName, defaultName],
   )
 
+  const {
+    isOpen: deleteConfirmationIsOpen,
+    onOpen: openDeleteConfirmationImpl,
+    onClose: closeDeleteConfirmation,
+  } = useDisclosure()
+  const cancelDeleteButton = useRef<HTMLButtonElement>(null)
+  const [deleteStep, { loading: isDeletingBranch }] = useMutation(DELETE_STEP, {
+    refetchQueries: ['GetFlow'],
+  })
+  const openDeleteConfirmation = useCallback<MouseEventHandler>(
+    (e) => {
+      e.stopPropagation()
+      openDeleteConfirmationImpl()
+    },
+    [openDeleteConfirmationImpl],
+  )
+  const deleteBranch = useCallback(async () => {
+    await deleteStep({
+      variables: { input: { ids: steps.map((step) => step.id) } },
+    })
+    closeDeleteConfirmation()
+  }, [deleteStep, steps])
+
   return (
     <>
+      {/*
+       * Branch row
+       */}
       <Flex
         onClick={openEditor}
         h={16}
@@ -51,8 +91,50 @@ export default function Branch(props: BranchProps): JSX.Element {
             ? String(steps[0].parameters.branchName)
             : defaultName}
         </Text>
-        {/* TODO in next PR: delete branch button */}
+        <IconButton
+          onClick={openDeleteConfirmation}
+          variant="clear"
+          aria-label="Delete Branch"
+          icon={<BiTrashAlt />}
+          isLoading={isDeletingBranch}
+          ml="auto"
+        />
       </Flex>
+      {/*
+       * Delete Confirmation Modal
+       */}
+      <AlertDialog
+        isOpen={deleteConfirmationIsOpen}
+        leastDestructiveRef={cancelDeleteButton}
+        onClose={closeDeleteConfirmation}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader>Delete Branch</AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure?{' '}
+              {steps.length > 1 &&
+                `You will delete all ${steps.length} steps in the branch!`}{' '}
+              You cannot undo this action afterwards.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button
+                variant="outline"
+                ref={cancelDeleteButton}
+                onClick={closeDeleteConfirmation}
+              >
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={deleteBranch} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+      {/*
+       * Nexted branch editor (pops up in modal)
+       */}
       <StepDisplayOverridesProvider value={initialStepDisplayOverride}>
         <NestedEditor
           onClose={closeEditor}
