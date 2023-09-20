926b66a184c191f54e5eb416aeae4c13c67b1d0e
Ian Chen <ian-@gmx.com>
2023-09-20 15:09:22 +0800

    chore: modal styles and icon colors (#224)

--- packages/frontend/src/components/FlowStepGroup/NestedEditor/index.tsx
+++ packages/frontend/src/components/FlowStepGroup/NestedEditor/index.tsx
@@ -1,6 +1,14 @@
 import { type IFlow, type IStep } from '@plumber/types'
 
-import { Modal, ModalCloseButton, ModalContent } from '@chakra-ui/react'
+import { BiChevronLeft } from 'react-icons/bi'
+import {
+  Box,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react'
+import { Button } from '@opengovsg/design-system-react'
 import Editor from 'components/Editor'
 
 interface NestedEditorProps {
@@ -14,10 +22,36 @@ export default function NestedEditor(props: NestedEditorProps): JSX.Element {
   const { flow, steps, isOpen, onClose } = props
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="full">
-      <ModalContent bg="base.canvas.brand-subtle" pt={24}>
-        <ModalCloseButton />
-        <Editor flow={flow} steps={steps} />
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="6xl"
+      closeOnEsc={false}
+      motionPreset="none"
+    >
+      <ModalOverlay bg="blackAlpha.400" backdropFilter="blur(4px)" />
+      <ModalContent bg="base.canvas.brand-subtle" mt={{ base: 0, lg: 16 }}>
+        <ModalHeader display="flex" justifyContent="center">
+          <Button
+            left={4}
+            top={4}
+            position="absolute"
+            variant="clear"
+            colorScheme="secondary"
+            pl={0}
+            outline="none"
+            onClick={onClose}
+            leftIcon={<BiChevronLeft size={28} />}
+          >
+            Back to main pipe
+          </Button>
+        </ModalHeader>
+        <Box p={8}>
+          {/* <Text textAlign="center" textStyle="h4" mb={4}>
+            Editing Branch
+          </Text> */}
+          <Editor flow={flow} steps={steps} />
+        </Box>
       </ModalContent>
     </Modal>
   )
