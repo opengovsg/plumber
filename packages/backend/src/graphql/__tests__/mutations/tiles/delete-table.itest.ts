7a5b343cc8de470df80729f294506ebfc3268260
Ian Chen <ian-@gmx.com>
2023-09-21 19:06:48 +0800

    chore: add tests

--- packages/backend/src/graphql/__tests__/mutations/tiles/delete-table.itest.ts
+++ packages/backend/src/graphql/__tests__/mutations/tiles/delete-table.itest.ts
@@ -1,7 +1,60 @@
-import { describe, expect, it } from 'vitest'
+import { randomUUID } from 'crypto'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import deleteTable from '@/graphql/mutations/tiles/delete-table'
+import TableMetadata from '@/models/table-metadata'
+import User from '@/models/user'
+import Context from '@/types/express/context'
 
 describe('delete table mutation', () => {
-  it('is a placeholder test', () => {
-    expect(true).toBeTruthy()
+  let context: Context
+  let dummyTable: TableMetadata
+  // cant use before all here since the data is re-seeded each time
+  beforeEach(async () => {
+    context = {
+      req: null,
+      res: null,
+      currentUser: await User.query().findOne({ email: 'tester@open.gov.sg' }),
+    }
+
+    dummyTable = await TableMetadata.query().insert({
+      name: 'Test Table',
+      userId: context.currentUser.id,
+    })
+
+    await dummyTable.$relatedQuery('columns').insert([
+      {
+        name: 'Test Column',
+      },
+      {
+        name: 'Test Column',
+      },
+    ])
+  })
+
+  it('should delete table and columns', async () => {
+    const success = await deleteTable(
+      null,
+      { input: { id: dummyTable.id } },
+      context,
+    )
+    const tableColumnCount = await dummyTable
+      .$relatedQuery('columns')
+      .resultSize()
+
+    const deletedTable = await TableMetadata.query().findById(dummyTable.id)
+    expect(success).toBe(true)
+    expect(deletedTable).toBeUndefined()
+    expect(tableColumnCount).toBe(0)
+  })
+
+  it('should throw an error if table is not found', async () => {
+    const deleteTableAction = deleteTable(
+      null,
+      { input: { id: randomUUID() } },
+      context,
+    )
+
+    await expect(deleteTableAction).rejects.toThrow()
   })
 })
