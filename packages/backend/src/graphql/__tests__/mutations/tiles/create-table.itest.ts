623fedf8f04971f50a7bf434d4ff0f9fd5233ed5
Ian Chen <ian-@gmx.com>
2023-09-21 19:06:48 +0800

    chore: add tests

--- packages/backend/src/graphql/__tests__/mutations/tiles/create-table.itest.ts
+++ packages/backend/src/graphql/__tests__/mutations/tiles/create-table.itest.ts
@@ -16,12 +16,30 @@ describe('create table mutation', () => {
     }
   })
 
-  it('should create a table', async () => {
+  it('should create a table and a column', async () => {
     const table = await createTable(
       null,
       { input: { name: 'Test Table' } },
       context,
     )
+    const tableColumnCount = await table.$relatedQuery('columns').resultSize()
     expect(table.name).toBe('Test Table')
+    expect(tableColumnCount).toBe(1)
+  })
+
+  it('should be able create tables with the same name', async () => {
+    const table = await createTable(
+      null,
+      { input: { name: 'Test Table' } },
+      context,
+    )
+
+    const table2 = await createTable(
+      null,
+      { input: { name: 'Test Table' } },
+      context,
+    )
+    expect(table.name).toBe('Test Table')
+    expect(table2.name).toBe('Test Table')
   })
 })
