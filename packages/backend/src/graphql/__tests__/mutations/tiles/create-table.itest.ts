8edacf0c263b25e7ea1e7b8a716454039b87c518
Ian Chen <ian-@gmx.com>
2023-09-27 00:28:06 +0800

    feat: add tests + refactor other tests

--- packages/backend/src/graphql/__tests__/mutations/tiles/create-table.itest.ts
+++ packages/backend/src/graphql/__tests__/mutations/tiles/create-table.itest.ts
@@ -1,19 +1,16 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import createTable from '@/graphql/mutations/tiles/create-table'
-import User from '@/models/user'
 import Context from '@/types/express/context'
+
+import { generateMockContext } from './table.mock'
 
 describe('create table mutation', () => {
   let context: Context
 
   // cant use before all here since the data is re-seeded each time
   beforeEach(async () => {
-    context = {
-      req: null,
-      res: null,
-      currentUser: await User.query().findOne({ email: 'tester@open.gov.sg' }),
-    }
+    context = await generateMockContext()
   })
 
   it('should create a table and a column', async () => {
