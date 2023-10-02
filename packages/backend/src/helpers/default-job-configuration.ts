09532bfa6a3aa87bc490c548204b9d397676ed84
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2023-10-02 17:53:56 +0800

    feat(worker): exponential backoff on connectivity issues (#233)

--- packages/backend/src/helpers/default-job-configuration.ts
+++ packages/backend/src/helpers/default-job-configuration.ts
@@ -1,5 +1,7 @@
 import { JobsOptions } from 'bullmq'
 
+import { ActionBackoffStrategy } from './actions'
+
 export const REMOVE_AFTER_30_DAYS = {
   age: 30 * 24 * 3600,
 }
@@ -14,4 +16,8 @@ export const DEFAULT_JOB_DELAY_DURATION = 0
 export const DEFAULT_JOB_OPTIONS: JobsOptions = {
   removeOnComplete: REMOVE_AFTER_7_DAYS_OR_50_JOBS,
   removeOnFail: REMOVE_AFTER_30_DAYS,
+  attempts: 3,
+  backoff: {
+    type: ActionBackoffStrategy.ExponentialConnectivity,
+  },
 }
