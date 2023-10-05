5a690949362c2f3a940415dbb5ad5bbd0fc52bf2
m0nggh <malcolmong27@gmail.com>
2023-10-05 14:03:15 +0800

    add email changes

--- packages/backend/src/helpers/generate-error-email.ts
+++ packages/backend/src/helpers/generate-error-email.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
 
+import appConfig from '@/config/app'
 import { createRedisClient, REDIS_DB_INDEX } from '@/config/redis'
 import { sendEmail } from '@/helpers/send-email'
 import Flow from '@/models/flow'
@@ -7,30 +8,45 @@ import Flow from '@/models/flow'
 const MAX_LENGTH = 80
 const redisClient = createRedisClient(REDIS_DB_INDEX.PIPE_ERRORS)
 
+function redirectFlowLink(flowId: string) {
+  return `${
+    appConfig.isDev ? appConfig.webAppUrl : appConfig.baseUrl
+  }/editor/${flowId}`
+}
+
 function truncateFlowName(flowName: string) {
   return flowName.length > MAX_LENGTH
     ? flowName.slice(0, MAX_LENGTH) + '...'
     : flowName
 }
 
-export function createBodyErrorMessage(flowName: string): string {
+export function createBodyErrorMessage(
+  flowName: string,
+  flowId: string,
+): string {
   const currDateTime = DateTime.now().toFormat('MMM dd yyyy, HH:mm:ss a')
   const bodyMessage = `
-    Dear user,
+    Dear Plumber admin,
     <br>
     <br>
-    We have detected that your pipe: <strong>${flowName}</strong>, from plumber.gov.sg has failed as of ${currDateTime}.
+    We have detected that your pipe: <strong>${flowName}</strong>, has failed as of ${currDateTime}.
     <br>
-    Several scenarios could have caused this issue:
+    <br>
+    This could be because of the following:
     <ol>
       <li>One of the apps you are using may be down.</li>
       <li>Some parts of the pipe you have set up may not be working as intended.</li>
     </ol>
     <br>
-    Please check https://status.plumber.gov.sg/ to see if the status for Plumber is green or not. If one of the apps you are using are down, the plumber team would've already been alerted as well. 
+    <p>What should you do?</p>
+    <ol>
+      <li>Retry the failed execution by heading to the executions tab, filter for failures and click on retry button.</li>
+      <li>Check our status page at https://status.plumber.gov.sg/ to see if Plumber or any of the apps you are using are down.</li>
+      <li>Edit your pipe at ${redirectFlowLink(flowId)}
+    </ol>
     <br>
     <br>
-    If the pipe issue is urgent, please log in to plumber.gov.sg and check the pipe. Please retry the pipe under the executions tab subsequently. Otherwise, you may respond to this email and request assistance from the Plumber support team.
+    If you are still having issues with your pipe, reply to this email and we'd be happy to help you get your pipe running again!
     <br>
     <br>
     Regards,
@@ -60,8 +76,8 @@ export async function sendErrorEmail(flow: Flow, userEmail: string) {
 
   await redisClient.hset(errorKey, errorDetails, () => {
     sendEmail({
-      subject: `${truncatedFlowName} has execution errors`,
-      body: createBodyErrorMessage(truncatedFlowName),
+      subject: `Plumber: Possible error on ${truncatedFlowName}`,
+      body: createBodyErrorMessage(truncatedFlowName, flowId),
       recipient: userEmail,
       replyTo: 'support@plumber.gov.sg',
     })
