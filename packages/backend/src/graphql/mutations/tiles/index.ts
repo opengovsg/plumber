cc0d854dc2d959885b0ce3605f0476003e40ae33
Ian Chen <ian-@gmx.com>
2023-10-16 13:47:03 +0800

    feat: expose tile mutations and queries

---
+++ packages/backend/src/graphql/mutations/tiles/index.ts
@@ -0,0 +1,5 @@
+import createTable from './create-table'
+import deleteTable from './delete-table'
+import updateTable from './update-table'
+
+export default { createTable, deleteTable, updateTable }
