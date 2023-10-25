3bf501203df0abc2a29f9c007749be4c5ff157ea
Kuan Wee Loong <weeloong@open.gov.sg>
2023-10-25 15:51:39 +0800

    fix: prevent thundering herd on 1st retry

--- packages/backend/src/helpers/__tests__/backoff.test.ts
+++ packages/backend/src/helpers/__tests__/backoff.test.ts
@@ -10,35 +10,34 @@ describe('Backoff', () => {
   it('backs off exponentially with jitter disabled', () => {
     vi.spyOn(Math, 'random').mockReturnValue(1)
 
-    expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS)
-    expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS * 2)
-    expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 4)
-    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 8)
+    expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS * 2)
+    expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS * 4)
+    expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 8)
+    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 16)
   })
 
-  it('waits the full duration on the 1st retry', () => {
+  it('applies jitter', () => {
     vi.spyOn(Math, 'random').mockReturnValue(0.5)
 
-    expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS)
-  })
-
-  it('applies jitter after 1st attempt (attemptsMade = $attemptsMade)', () => {
-    vi.spyOn(Math, 'random').mockReturnValue(0.5)
-
-    expect(exponentialBackoffWithJitter(2)).toEqual(
+    expect(exponentialBackoffWithJitter(1)).toEqual(
       INITIAL_DELAY_MS + INITIAL_DELAY_MS / 2,
     )
+    expect(exponentialBackoffWithJitter(2)).toEqual(
+      INITIAL_DELAY_MS * 2 /* Full delay for 1st retry */ +
+        INITIAL_DELAY_MS /* 50% of full delay*/,
+    )
     expect(exponentialBackoffWithJitter(3)).toEqual(
-      INITIAL_DELAY_MS * 2 + INITIAL_DELAY_MS,
+      INITIAL_DELAY_MS * 4 /* Full delay for 2nd retry */ +
+        INITIAL_DELAY_MS * 2 /* 50% of full delay*/,
     )
   })
 
   it('will wait at least the full duration of the previous delay', () => {
     vi.spyOn(Math, 'random').mockReturnValue(0)
 
-    expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS)
-    expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 2)
-    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 4)
-    expect(exponentialBackoffWithJitter(5)).toEqual(INITIAL_DELAY_MS * 8)
+    expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS)
+    expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS * 2)
+    expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 4)
+    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 8)
   })
 })
--- packages/backend/src/helpers/backoff.ts
+++ packages/backend/src/helpers/backoff.ts
@@ -1,6 +1,6 @@
 import { type BackoffStrategy, type Job } from 'bullmq'
 
-export const INITIAL_DELAY_MS = 5000
+export const INITIAL_DELAY_MS = 3000
 
 export const exponentialBackoffWithJitter: BackoffStrategy = function (
   attemptsMade: number,
@@ -9,19 +9,15 @@ export const exponentialBackoffWithJitter: BackoffStrategy = function (
   _job: Job,
 ): number {
   // This implements FullJitter-like jitter, with the following changes:
-  //
-  // * We wait the full delay on the 1st retry.
-  // * We wait at least the full duration of the previous delay.
+  //.
+  // * We wait _at least_ the full duration of the previous delay (or on the 1st
+  //   retry, at least full initial delay).
   // * No cap on wait time; capping should not matter here since we cap total
   //   attempts.
   //
   // Reference:
   // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
 
-  if (attemptsMade === 1) {
-    return INITIAL_DELAY_MS
-  }
-
-  const prevFullDelay = Math.pow(2, attemptsMade - 2) * INITIAL_DELAY_MS
+  const prevFullDelay = Math.pow(2, attemptsMade - 1) * INITIAL_DELAY_MS
   return prevFullDelay + Math.round(Math.random() * prevFullDelay)
 }
