a6ef738f58bca3f3192a9e723f250c6ce51eba30
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-10-25 16:09:16 +0800

    Apply jitter to our exponential backoff (#279)

--- packages/backend/src/helpers/default-job-configuration.ts
+++ packages/backend/src/helpers/default-job-configuration.ts
@@ -12,14 +12,11 @@ export const REMOVE_AFTER_7_DAYS_OR_50_JOBS = {
 export const DEFAULT_JOB_DELAY_DURATION = 0
 export const MAXIMUM_JOB_ATTEMPTS = 4
 
-const EXPONENTIAL_BACKOFF_INITIAL_DELAY_MS = 5000
-
 export const DEFAULT_JOB_OPTIONS: JobsOptions = {
   removeOnComplete: REMOVE_AFTER_7_DAYS_OR_50_JOBS,
   removeOnFail: REMOVE_AFTER_30_DAYS,
   attempts: MAXIMUM_JOB_ATTEMPTS,
   backoff: {
-    type: 'exponential',
-    delay: EXPONENTIAL_BACKOFF_INITIAL_DELAY_MS,
+    type: 'custom',
   },
 }
--- packages/backend/src/workers/action.ts
+++ packages/backend/src/workers/action.ts
@@ -5,6 +5,7 @@ import { UnrecoverableError, Worker } from 'bullmq'
 import appConfig from '@/config/app'
 import { createRedisClient } from '@/config/redis'
 import { handleErrorAndThrow } from '@/helpers/actions'
+import { exponentialBackoffWithJitter } from '@/helpers/backoff'
 import {
   DEFAULT_JOB_OPTIONS,
   MAXIMUM_JOB_ATTEMPTS,
@@ -86,6 +87,9 @@ export const worker = new Worker(
     prefix: '{actionQ}',
     connection: createRedisClient(),
     concurrency: appConfig.workerActionConcurrency,
+    settings: {
+      backoffStrategy: exponentialBackoffWithJitter,
+    },
   },
 )
 
