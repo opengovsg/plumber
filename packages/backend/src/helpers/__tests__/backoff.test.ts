6d67beeb6fb54f5f02590b58f05090ca304aa7c3
Kuan Wee Loong <weeloong@open.gov.sg>
2023-10-25 11:27:42 +0800

    feat: wait at least full duration of prev try

--- packages/backend/src/helpers/__tests__/backoff.test.ts
+++ packages/backend/src/helpers/__tests__/backoff.test.ts
@@ -7,17 +7,38 @@ describe('Backoff', () => {
     vi.restoreAllMocks()
   })
 
-  it('backs off exponentially before jitter', () => {
+  it('backs off exponentially with jitter disabled', () => {
     vi.spyOn(Math, 'random').mockReturnValue(1)
+
     expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS)
     expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS * 2)
     expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 4)
+    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 8)
   })
 
-  it('applies jitter', () => {
+  it('waits the full duration on the 1st retry', () => {
     vi.spyOn(Math, 'random').mockReturnValue(0.5)
-    expect(exponentialBackoffWithJitter(1)).toEqual(
-      Math.round(INITIAL_DELAY_MS / 2),
+
+    expect(exponentialBackoffWithJitter(1)).toEqual(INITIAL_DELAY_MS)
+  })
+
+  it('applies jitter after 1st attempt (attemptsMade = $attemptsMade)', () => {
+    vi.spyOn(Math, 'random').mockReturnValue(0.5)
+
+    expect(exponentialBackoffWithJitter(2)).toEqual(
+      INITIAL_DELAY_MS + INITIAL_DELAY_MS / 2,
     )
+    expect(exponentialBackoffWithJitter(3)).toEqual(
+      INITIAL_DELAY_MS * 2 + INITIAL_DELAY_MS,
+    )
+  })
+
+  it('will wait at least the full duration of the previous delay', () => {
+    vi.spyOn(Math, 'random').mockReturnValue(0)
+
+    expect(exponentialBackoffWithJitter(2)).toEqual(INITIAL_DELAY_MS)
+    expect(exponentialBackoffWithJitter(3)).toEqual(INITIAL_DELAY_MS * 2)
+    expect(exponentialBackoffWithJitter(4)).toEqual(INITIAL_DELAY_MS * 4)
+    expect(exponentialBackoffWithJitter(5)).toEqual(INITIAL_DELAY_MS * 8)
   })
 })
--- packages/backend/src/helpers/backoff.ts
+++ packages/backend/src/helpers/backoff.ts
@@ -8,13 +8,20 @@ export const exponentialBackoffWithJitter: BackoffStrategy = function (
   _err: Error,
   _job: Job,
 ): number {
-  // This implements (uncapped) FullJitter; we want to minimize outgoing calls
-  // to be a nice API consumer. Capping should not matter here since we cap
-  // total attempts.
+  // This implements FullJitter-like jitter, with the following changes:
+  //
+  // * We wait the full delay on the 1st retry.
+  // * We wait at least the full duration of the previous delay.
+  // * No cap on wait time; capping should not matter here since we cap total
+  //   attempts.
   //
   // Reference:
   // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
 
-  const baseDelay = Math.round(Math.pow(2, attemptsMade - 1) * INITIAL_DELAY_MS)
-  return Math.round(Math.random() * baseDelay)
+  if (attemptsMade === 1) {
+    return INITIAL_DELAY_MS
+  }
+
+  const prevFullDelay = Math.pow(2, attemptsMade - 2) * INITIAL_DELAY_MS
+  return prevFullDelay + Math.round(Math.random() * prevFullDelay)
 }
