e409fb660bc7d8f5547803edfd8b94d1113d3e6d
Ian Chen <ian-@gmx.com>
2023-10-29 19:02:36 +0800

    feat: frontend POC

---
+++ packages/frontend/src/graphql/queries/get-all-rows.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client'
+
+export const GET_ALL_ROWS = gql`
+  query GetAllRows($tableId: String!) {
+    getAllRows(tableId: $tableId) {
+      rowId
+      data
+    }
+  }
+`
--- packages/frontend/src/pages/Tile/index.tsx
+++ packages/frontend/src/pages/Tile/index.tsx
@@ -1,9 +1,50 @@
+import { ITableMetadata, ITableRow } from '@plumber/types'
+
 import { useParams } from 'react-router-dom'
-import { Flex, Text } from '@chakra-ui/react'
+import { useQuery } from '@apollo/client'
+import { Center, Flex, Spinner, Text } from '@chakra-ui/react'
 import Container from 'components/Container'
+import { GET_ALL_ROWS } from 'graphql/queries/get-all-rows'
+import { GET_TABLE } from 'graphql/queries/get-table'
+
+import Table from './components/Table'
 
 export default function Tile(): JSX.Element {
   const { tableId } = useParams<{ tableId: string }>()
+
+  const { data: getTableData, loading: getTableLoading } = useQuery<{
+    getTable: ITableMetadata
+  }>(GET_TABLE, {
+    variables: {
+      tableId,
+    },
+  })
+
+  const { data: getAllRowsData, loading: getAllRowsLoading } = useQuery<{
+    getAllRows: ITableRow[]
+  }>(GET_ALL_ROWS, {
+    variables: {
+      tableId,
+    },
+  })
+
+  if (
+    !getTableData?.getTable ||
+    !getAllRowsData?.getAllRows ||
+    getTableLoading ||
+    getAllRowsLoading
+  ) {
+    return (
+      <Center height="100vh">
+        <Spinner size="xl" thickness="4px" color="primary.500" margin="auto" />
+      </Center>
+    )
+  }
+
+  const {
+    getTable: { id, name, columns },
+  } = getTableData
+  const { getAllRows: rows } = getAllRowsData
 
   return (
     <Container py={7}>
@@ -13,7 +54,8 @@ export default function Tile(): JSX.Element {
         alignItems="stretch"
         gap={4}
       >
-        <Text textStyle="h4">table id: {tableId}</Text>
+        <Text textStyle="h4">{name}</Text>
+        <Table tableId={id} tableColumns={columns} tableRows={rows} />
       </Flex>
     </Container>
   )
