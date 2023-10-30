b0cecae66118868ecc8cec2df2e2a0f376f8b5a2
Ian Chen <ian-@gmx.com>
2023-10-30 17:05:47 +0800

    PLU-70: [TILES-6] get table(s) query (#256)
    * feat: get all dynamodb rows query
    
    * chore: fix test
    
    * wip
    
    * chore: add tests
    
    * feat: list tables query

---
+++ packages/backend/src/graphql/__tests__/queries/tiles/get-tables.itest.ts
@@ -0,0 +1,39 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+
+import getTables from '@/graphql/queries/tiles/get-tables'
+import Context from '@/types/express/context'
+
+import {
+  generateMockContext,
+  generateMockTable,
+} from '../../mutations/tiles/table.mock'
+
+describe('get tables query', () => {
+  let context: Context
+
+  // cant use before all here since the data is re-seeded each time
+  beforeEach(async () => {
+    context = await generateMockContext()
+  })
+  it('should return array of tables belonging to suers', async () => {
+    const numTables = 5
+    for (let i = 0; i < numTables; i++) {
+      await generateMockTable({ userId: context.currentUser.id })
+    }
+    const tables = await getTables(null, null, context)
+    expect(tables).toHaveLength(numTables)
+  })
+
+  it('should not return columns', async () => {
+    await generateMockTable({
+      userId: context.currentUser.id,
+    })
+    const tables = await getTables(null, null, context)
+    expect(tables[0].columns).toBeUndefined()
+  })
+
+  it('should return empty array if no tables found', async () => {
+    const tables = await getTables(null, null, context)
+    expect(tables).toHaveLength(0)
+  })
+})
