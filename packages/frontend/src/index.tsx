13f6228f94ef227caf88b0101366c037298708e3
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2023-11-14 17:01:04 +0800

    chore(datadog): add datadog RUM (#316)

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,11 +2,13 @@ import '@fontsource/space-grotesk'
 
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { datadogRum } from '@datadog/browser-rum'
 import ApolloProvider from 'components/ApolloProvider'
 import IntlProvider from 'components/IntlProvider'
 import router from 'components/Router'
 import SnackbarProvider from 'components/SnackbarProvider'
 import ThemeProvider from 'components/ThemeProvider'
+import appConfig from 'config/app'
 import { AuthenticationProvider } from 'contexts/Authentication'
 import { LaunchDarklyProvider } from 'contexts/LaunchDarkly'
 
@@ -18,6 +20,21 @@ if (!container) {
 
 const root = createRoot(container)
 
+if (['prod', 'staging'].includes(appConfig.env)) {
+  datadogRum.init({
+    applicationId: '3bd09f62-dad8-4107-a907-1242dda9ee4d',
+    clientToken: 'pubdc3dfc4ab0145729ebc91a19e9fae167',
+    site: 'datadoghq.com',
+    service: 'plumber',
+    env: appConfig.env,
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  })
+}
 root.render(
   <SnackbarProvider>
     <ThemeProvider>
