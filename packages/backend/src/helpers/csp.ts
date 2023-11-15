e3fff4d22a62b644fd3f8e58f09c586fe3e9d810
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-11-15 10:42:02 +0800

    fix(dd-rum): add missing host for connect-src
    	modified:   packages/backend/src/helpers/csp.ts

--- packages/backend/src/helpers/csp.ts
+++ packages/backend/src/helpers/csp.ts
@@ -11,6 +11,7 @@ const helmetOptions: HelmetOptions = {
       connectSrc: [
         "'self'",
         // For Datadog RUM
+        'https://browser-intake-datadoghq.com',
         'https://*.browser-intake-datadoghq.com',
         // Launch Darkly feature flags
         'https://*.launchdarkly.com',
@@ -47,6 +48,7 @@ const helmetOptions: HelmetOptions = {
         !appConfig.isDev && 'https://*.apollographql.com',
       ].filter(Boolean),
       upgradeInsecureRequests: [],
+      workerSrc: ['blob:'],
     },
   },
   crossOriginOpenerPolicy: {
