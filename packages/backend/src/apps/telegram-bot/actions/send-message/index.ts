6cb2843b179710ffa2ecf3b9475cc91520289428
m0nggh <malcolmong27@gmail.com>
2023-11-16 18:12:20 +0800

    add initial draft for catching errors for telegram

--- packages/backend/src/apps/telegram-bot/actions/send-message/index.ts
+++ packages/backend/src/apps/telegram-bot/actions/send-message/index.ts
@@ -1,5 +1,12 @@
 import { IRawAction } from '@plumber/types'
 
+import get from 'lodash.get'
+
+import {
+  generateHttpStepError,
+  generateStepError,
+} from '@/helpers/generate-step-error'
+
 import { escapeMarkdown, sanitizeMarkdown } from '../../common/markdown-v1'
 
 const action: IRawAction = {
@@ -65,14 +72,66 @@ const action: IRawAction = {
   },
 
   async run($) {
-    const sanitizedMarkdown = sanitizeMarkdown($.step.parameters.text as string)
+    const messageText = ($.step.parameters.text as string).trim()
+    if (!messageText) {
+      const stepErrorName = 'Empty message text'
+      const stepErrorSolution =
+        'Click on set up action and check that the message text is not empty, especially if variables are used.'
+      throw generateStepError(
+        stepErrorName,
+        stepErrorSolution,
+        $.step.position,
+        $.app.name,
+      )
+    }
+
+    const sanitizedMarkdown = sanitizeMarkdown(messageText)
     const payload = {
       chat_id: $.step.parameters.chatId,
       text: sanitizedMarkdown,
       disable_notification: $.step.parameters.disableNotification,
       parse_mode: 'markdown', // legacy markdown to allow only a small set of modifiers
     }
-    const response = await $.http.post('/sendMessage', payload)
+    const response = await $.http
+      .post('/sendMessage', payload)
+      .catch((err): never => {
+        // different error format for ETIMEDOUT and ECONNRESET
+
+        const status = err.response.status
+        let stepErrorSolution = ''
+        if (status === 400) {
+          // many sub errors caught by telegram for this status
+          const errorString = JSON.stringify(
+            get(err, 'details.description', ''),
+          )
+          if (errorString.includes('not enough rights')) {
+            stepErrorSolution =
+              'Please give permission for your bot in telegram to send messages in the group chat used in your action.'
+          } else if (errorString.includes('supergroup')) {
+            stepErrorSolution =
+              'Please re-connect the chat ID because chat ID changes when your group gets upgraded to a supergroup chat.'
+          } else if (errorString.includes('chat not found')) {
+            stepErrorSolution =
+              'Click on set up action and check that a valid chat ID is used. Otherwise, remove and re-add the bot into the group. Make sure that you send a message to the bot before the bot can send messages to you.'
+          }
+        } else if (status === 403) {
+          stepErrorSolution =
+            'Check that the bot is added in the group chat used in your action.'
+        } else if (status === 429) {
+          stepErrorSolution =
+            'Too many messages are being sent. Please wait for a minute before retrying and avoid sending more than 20 messages per minute to the same group.'
+        } else {
+          // return original error since uncaught
+          throw err
+        }
+        throw generateHttpStepError(
+          err,
+          stepErrorSolution,
+          $.step.position,
+          $.app.name,
+        )
+      })
+    console.log({ response: response.data.result })
 
     $.setActionItem({
       raw: response.data,
