2305e80e1e7cd06c4cf7c279da80e57952ea9363
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-11-16 16:52:10 +0800

    fix(file-processing): handle edge cases for file processing

--- packages/backend/src/helpers/actions.ts
+++ packages/backend/src/helpers/actions.ts
@@ -10,7 +10,14 @@ import StepError from '@/errors/step'
 import Step from '@/models/step'
 
 export function doesActionProcessFiles(step: Step): boolean {
+  if (!apps[step.appKey]) {
+    return false
+  }
   const action = apps[step.appKey].actions.find((a) => a.key === step.key)
+  if (!action || !action.doesFileProcessing) {
+    return false
+  }
+
   return action.doesFileProcessing(step)
 }
 
