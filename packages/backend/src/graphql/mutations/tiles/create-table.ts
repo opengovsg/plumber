74915497511b97bddc849308e9af3b00d4fba30a
Ian Chen <ian-@gmx.com>
2023-11-22 16:03:07 +0800

    chore: placeholder rows and columns

--- packages/backend/src/graphql/mutations/tiles/create-table.ts
+++ packages/backend/src/graphql/mutations/tiles/create-table.ts
@@ -1,3 +1,4 @@
+import { createTableRows } from '@/models/dynamodb/table-row'
 import Context from '@/types/express/context'
 
 type Params = {
@@ -17,9 +18,24 @@ const createTable = async (
     name: tableName,
   })
 
-  await table.$relatedQuery('columns').insert({
-    name: 'Column 1',
-  })
+  await table.$relatedQuery('columns').insert([
+    {
+      name: 'Column 1',
+      position: 0,
+    },
+    {
+      name: 'Column 2',
+      position: 1,
+    },
+    {
+      name: 'Column 3',
+      position: 2,
+    },
+  ])
+
+  const emptyDataArray = new Array(5).fill({})
+
+  await createTableRows({ tableId: table.id, dataArray: emptyDataArray })
 
   return table
 }
