1799be40f05e80084aaca4fbf2b45bdf26a1b9dc
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-11-28 11:04:52 +0800

    fix(rich-text-input): remove hardbreak from rich text inputs

--- packages/frontend/src/components/RichTextEditor/index.tsx
+++ packages/frontend/src/components/RichTextEditor/index.tsx
@@ -11,7 +11,6 @@ import {
 import { Controller, useFormContext } from 'react-hook-form'
 import { ClickAwayListener, FormControl } from '@mui/material'
 import { FormLabel } from '@opengovsg/design-system-react'
-import Hardbreak from '@tiptap/extension-hard-break'
 import Link from '@tiptap/extension-link'
 import Placeholder from '@tiptap/extension-placeholder'
 import Table from '@tiptap/extension-table'
