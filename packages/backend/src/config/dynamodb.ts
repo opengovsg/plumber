b918191ca91ce2132a94aab5cb172e320288e050
Ian Chen <ian-@gmx.com>
2023-11-30 03:06:47 +0800

    chore: move to electrodb to handle unprocessed items

--- packages/backend/src/config/dynamodb.ts
+++ packages/backend/src/config/dynamodb.ts
@@ -1,32 +1,49 @@
-import dynamoose from 'dynamoose'
-
-import { TableRow } from '@/models/dynamodb/table-row'
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
 
 import appConfig from './app'
 
-const ddb = new dynamoose.aws.ddb.DynamoDB({
+export const tableName = `plumber-${appConfig.appEnv}-tiles`
+
+const dynamodbEndpoint = appConfig.isDev
+  ? `http://localhost:${process.env.LOCAL_DYNAMODB_PORT ?? 8000}`
+  : undefined
+
+const client = new DynamoDB({
   region: 'ap-southeast-1',
+  endpoint: dynamodbEndpoint,
 })
 
-if (appConfig.isDev) {
-  // Use local dynamodb in development env
-  // LOCAL_DYNAMODB_PORT is set in packages/backend/test/ddb-global-setup.ts
-  // and is only specified when running integration tests.
-  dynamoose.aws.ddb.local(
-    `http://localhost:${process.env.LOCAL_DYNAMODB_PORT ?? 8000}`,
-  )
-} else {
-  dynamoose.aws.ddb.set(ddb)
+export async function createDynamoDBTable() {
+  const data = await client.listTables({})
+  const tableExists = data.TableNames?.includes(tableName)
+  if (!tableExists) {
+    if (!appConfig.isDev) {
+      throw new Error(`Table ${tableName} does not exist`)
+    }
+    await client.createTable({
+      TableName: tableName,
+      AttributeDefinitions: [
+        { AttributeName: 'tableId', AttributeType: 'S' },
+        { AttributeName: 'rowId', AttributeType: 'S' },
+        { AttributeName: 'createdAt', AttributeType: 'N' },
+      ],
+      BillingMode: 'PAY_PER_REQUEST',
+      LocalSecondaryIndexes: [
+        {
+          IndexName: 'createdAtIndex',
+          KeySchema: [
+            { AttributeName: 'tableId', KeyType: 'HASH' },
+            { AttributeName: 'createdAt', KeyType: 'RANGE' },
+          ],
+          Projection: { ProjectionType: 'ALL' },
+        },
+      ],
+      KeySchema: [
+        { AttributeName: 'tableId', KeyType: 'HASH' },
+        { AttributeName: 'rowId', KeyType: 'RANGE' },
+      ],
+    })
+  }
 }
 
-dynamoose.Table.defaults.set({
-  prefix: `${appConfig.appEnv}-plumber-`,
-  // If Dynamoose should attempt to create the table on DynamoDB. True only in dev
-  create: appConfig.isDev,
-  // If Dynamoose should update the capacity of the existing table to match the model throughput.
-  update: false,
-  // If Dynamoose should run it's initialization flow (creating the table, updating the throughput, etc) automatically.
-  initialize: appConfig.isDev,
-})
-
-new dynamoose.Table('table', [TableRow])
+export default client
--- packages/backend/src/models/dynamodb/helpers.ts
+++ packages/backend/src/models/dynamodb/helpers.ts
@@ -1,14 +1,27 @@
-import { Condition } from 'dynamoose'
+import { ElectroError } from 'electrodb'
 
-export function wrapDynamoDBError(error: unknown): never {
-  if (
-    error instanceof Error &&
-    error.name === 'ConditionalCheckFailedException'
-  ) {
-    throw new Error('No row found for given table and row id')
+export function handleDynamoDBError(error: unknown): never {
+  console.error(error)
+  if (error instanceof ElectroError) {
+    // ref: https://electrodb.dev/en/reference/error/#error-classifications
+    if (error.code < 2000) {
+      throw new Error('DynamoDB Configuration Error: ' + error.cause.message)
+    }
+    if (error.code < 3000) {
+      throw new Error('DynamoDB Invalid Query Error: ' + error.cause.message)
+    }
+    if (error.code < 4000) {
+      throw new Error('DynamoDB Validation Error: ' + error.cause.message)
+    }
+    if (error.code < 5000) {
+      throw new Error('DynamoDB Internal Error: ' + error.cause.message)
+    }
+    if (error.code < 6000) {
+      throw new Error('DynamoDB Unexpected Error: ' + error.cause.message)
+    }
   }
-  throw error
+  if (error instanceof Error) {
+    throw new Error('DynamoDB Error: ' + error.message)
+  }
+  throw new Error('DynamoDB Unknown Error')
 }
-
-export const rowExistCondition = (tableId: string, rowId: string) =>
-  new Condition().where('tableId').eq(tableId).and().where('rowId').eq(rowId)
