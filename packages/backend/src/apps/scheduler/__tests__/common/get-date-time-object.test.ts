4253f3bc8a0baf4b6a3227ff75bcc9e1d1b49b2f
m0nggh <malcolmong27@gmail.com>
2023-11-29 15:01:41 +0800

    add fix for scheduler date format

---
+++ packages/backend/src/apps/scheduler/__tests__/common/get-date-time-object.test.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { describe, expect, it } from 'vitest'
+
+import getDateTimeObjectRepresentation from '../../common/get-date-time-object'
+
+describe('Test get date time object format', () => {
+  describe('Test pretty_date', () => {
+    it('Single digit date should be double padded', () => {
+      const dateTime = DateTime.local(2023, 11, 1)
+      expect(getDateTimeObjectRepresentation(dateTime).pretty_date).toEqual(
+        '01 Nov 2023',
+      )
+    })
+
+    it('Double digit date should be double padded', () => {
+      const dateTime = DateTime.local(2023, 10, 21)
+      expect(getDateTimeObjectRepresentation(dateTime).pretty_date).toEqual(
+        '21 Oct 2023',
+      )
+    })
+  })
+})
