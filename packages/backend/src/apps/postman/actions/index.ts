58f0c7faeaaa77ddaa962b223268829b2ac6eeef
Stanley Nguyen <stanleynguyen@users.noreply.github.com>
2023-12-01 18:14:02 +0800

    fix(action): feature flag back the old editor (#349)

--- packages/backend/src/apps/postman/actions/index.ts
+++ packages/backend/src/apps/postman/actions/index.ts
@@ -1,4 +1,9 @@
+import sendEmailWithOldEditor from './send-email-raw-html'
 import sendEmailWithAttachments from './send-email-with-attachments'
 import sendTransactionalEmail from './send-transactional-email'
 
-export default [sendTransactionalEmail, sendEmailWithAttachments]
+export default [
+  sendTransactionalEmail,
+  sendEmailWithAttachments,
+  sendEmailWithOldEditor,
+]
