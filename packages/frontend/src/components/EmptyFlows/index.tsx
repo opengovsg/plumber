75d7e9008d782dbfa6a6022235cea70682a486a8
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2023-12-08 10:55:54 +0800

    PLU-162: Add template for users with no flows (#334)

---
+++ packages/frontend/src/components/EmptyFlows/index.tsx
@@ -0,0 +1,83 @@
+import { BiWinkSmile } from 'react-icons/bi'
+import {
+  AbsoluteCenter,
+  As,
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  Hide,
+  Icon,
+  Text,
+} from '@chakra-ui/react'
+import { Button } from '@opengovsg/design-system-react'
+import NavigationDrawer from 'components/Layout/NavigationDrawer'
+
+import FlowTemplate, { FlowTemplateProps } from './FlowTemplate'
+
+const flowTemplates: FlowTemplateProps[] = [
+  {
+    title: 'Notifications for form submission',
+    description: 'Set up notifications when a new form submission comes in',
+    link: 'https://guide.plumber.gov.sg/use-cases/notifications-for-formsg',
+  },
+  {
+    title: 'Customised acknowledgements',
+    description: `Send respondents’ customised acknowledgements based on their form responses`,
+    link: 'https://guide.plumber.gov.sg/use-cases/email-acknowledgements-to-respondent',
+  },
+  {
+    title: 'Attendance tracking',
+    description: 'Track turnout for events',
+    link: 'https://guide.plumber.gov.sg/use-cases/attendance-taking',
+  },
+]
+
+interface EmptyFlowsProps {
+  CreateFlowLink: As
+}
+
+export default function EmptyFlows(props: EmptyFlowsProps) {
+  const { CreateFlowLink } = props
+  return (
+    <Box px="10vw" py="10vh">
+      <Flex>
+        <Hide above="sm">
+          <Box mt={-1.5}>
+            <NavigationDrawer />
+          </Box>
+        </Hide>
+        <Text textStyle="h4" display="inline">
+          {`You don't have any pipes, see what others are creating`}{' '}
+          <Icon
+            as={BiWinkSmile}
+            verticalAlign="middle"
+            color="primary.600"
+            boxSize={8}
+          />
+        </Text>
+      </Flex>
+      <Grid mt={4} gridTemplateColumns="repeat(3, 1fr)" gap={4}>
+        {flowTemplates.map(({ title, description, link }, index) => (
+          <FlowTemplate
+            key={index}
+            title={title}
+            description={description}
+            link={link}
+          />
+        ))}
+      </Grid>
+      <Box position="relative" my={8}>
+        <Divider />
+        <AbsoluteCenter>
+          <Box bg="white" p={3}>
+            <Text textStyle="subhead-1">OR</Text>
+          </Box>
+        </AbsoluteCenter>
+      </Box>
+      <Button w="100%" as={CreateFlowLink}>
+        Create my own pipe
+      </Button>
+    </Box>
+  )
+}
