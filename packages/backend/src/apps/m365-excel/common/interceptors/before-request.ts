c0565d90434d64a718c5de010a6c0be220817335
Kuan Wee Loong <weeloong@open.gov.sg>
2023-12-13 10:18:05 +0800

    feat: rate limiting

--- packages/backend/src/apps/m365-excel/common/interceptors/before-request.ts
+++ packages/backend/src/apps/m365-excel/common/interceptors/before-request.ts
@@ -1,7 +1,13 @@
 import type { TBeforeRequest } from '@plumber/types'
 
+import { RateLimiterRes } from 'rate-limiter-flexible'
+
+import { isM365TenantKey } from '@/config/app-env-vars/m365'
+import RetriableError from '@/errors/retriable-error'
+
 import { MS_GRAPH_OAUTH_BASE_URL } from '../constants'
 import { getAccessToken } from '../oauth/token-cache'
+import { consumeOrThrowLimiterWithLongestDelay } from '../rate-limiter'
 
 // TODO in later PR: usage tracker
 
@@ -27,8 +33,32 @@ const addAuthToken: TBeforeRequest = async function ($, requestConfig) {
   return requestConfig
 }
 
-// TODO in later PR: rate limiting request interceptor (this is temporary
-// until we implement the plumber-wide rate limiting system; building this
-// first to unblock M365 pilot).
+// This rate limiting request interceptor is slightly different from the planned
+// plumber-wide rate limiting system; we need to limit per request instead of
+// per action (that's also why this is placed in beforeRequest instead of in the
+// `run` function; an action may need multiple requests).
+const rateLimitCheck: TBeforeRequest = async function ($, requestConfig) {
+  const tenantKey = $.auth.data?.tenantKey as string
+  if (!isM365TenantKey(tenantKey)) {
+    throw new Error(`'${tenantKey}' is not a valid M365 tenant.`)
+  }
 
-export default [addAuthToken]
+  try {
+    await consumeOrThrowLimiterWithLongestDelay($, tenantKey, 1)
+  } catch (error) {
+    if (!(error instanceof RateLimiterRes)) {
+      return
+    }
+
+    throw new RetriableError({
+      error: 'Reached M365 rate limit',
+      delayInMs: error.msBeforeNext,
+    })
+  }
+
+  return requestConfig
+}
+
+// rateLimitCheck is explicitly the 1st interceptor so that the others are not
+// called if it throws an error.
+export default [rateLimitCheck, addAuthToken]
