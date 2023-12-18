9061afb1705663c2fe66c69629fec929f7a57ef7
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-12-18 14:17:15 +0800

    PLU-125: [EXCEL-1] Support MS Graph OAuth (#352)

---
+++ packages/backend/src/apps/m365-excel/index.ts
@@ -0,0 +1,17 @@
+import type { IApp } from '@plumber/types'
+
+import actions from './actions'
+
+const app: IApp = {
+  name: 'M365 Excel',
+  key: 'm365-excel',
+  iconUrl: '{BASE_URL}/apps/m365-excel/assets/favicon.svg',
+  authDocUrl: 'https://guide.plumber.gov.sg/user-guides/actions/m365-excel',
+  baseUrl: 'https://www.office.com',
+  apiBaseUrl: 'https://graph.microsoft.com',
+  supportsConnections: true,
+  primaryColor: '000000',
+  actions,
+}
+
+export default app
