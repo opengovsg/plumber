a89636f21edcd37812d3b33866a5d204263fd13e
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2023-12-28 23:36:38 +0800

    PLU-125: [EXCEL-6] Dynamic data for excel (#367)

--- packages/backend/src/apps/m365-excel/actions/index.ts
+++ packages/backend/src/apps/m365-excel/actions/index.ts
@@ -6,12 +6,66 @@ const testAction: IRawAction = {
   description: 'To make excel show up in app list; real actions in later PR.',
   arguments: [
     {
-      label: 'Placeholder',
-      key: 'data',
-      type: 'string' as const,
+      key: 'fileId',
+      label: 'Excel File',
       required: true,
-      description: 'Beep boop',
-      variables: true,
+      description: 'File to edit',
+      type: 'dropdown' as const,
+      variables: false,
+      source: {
+        type: 'query' as const,
+        name: 'getDynamicData' as const,
+        arguments: [
+          {
+            name: 'key',
+            value: 'listFiles',
+          },
+        ],
+      },
+    },
+    {
+      key: 'worksheetId',
+      label: 'Worksheet',
+      required: true,
+      description: 'Worksheet to edit',
+      type: 'dropdown' as const,
+      variables: false,
+      source: {
+        type: 'query' as const,
+        name: 'getDynamicData' as const,
+        arguments: [
+          {
+            name: 'key',
+            value: 'listWorksheets',
+          },
+          {
+            name: 'parameters.fileId',
+            value: '{parameters.fileId}',
+          },
+        ],
+      },
+    },
+    {
+      key: 'tableId',
+      label: 'Table',
+      required: true,
+      description: 'Table to edit',
+      type: 'dropdown' as const,
+      variables: false,
+      source: {
+        type: 'query' as const,
+        name: 'getDynamicData' as const,
+        arguments: [
+          {
+            name: 'key',
+            value: 'listTables',
+          },
+          {
+            name: 'parameters.fileId',
+            value: '{parameters.fileId}',
+          },
+        ],
+      },
     },
   ],
 
---
+++ packages/backend/src/apps/m365-excel/dynamic-data/index.ts
@@ -0,0 +1,5 @@
+import listFiles from './list-files'
+import listTables from './list-tables'
+import listWorksheets from './list-worksheets'
+
+export default [listFiles, listTables, listWorksheets]
