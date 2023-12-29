52e02451a9e065c1d06fdc0851a4aedba2b6f12c
Ian Chen <ian-@gmx.com>
2023-12-29 16:28:08 +0800

    feat: dynamic data for tiles related actions

---
+++ packages/backend/src/apps/tiles/dynamic-data/list-tables.ts
@@ -0,0 +1,37 @@
+import {
+  DynamicDataOutput,
+  IDynamicData,
+  IGlobalVariable,
+} from '@plumber/types'
+
+import logger from '@/helpers/logger'
+import User from '@/models/user'
+
+const dynamicData: IDynamicData = {
+  name: 'List Tables',
+  key: 'listTables',
+
+  async run($: IGlobalVariable): Promise<DynamicDataOutput> {
+    if (!$.user?.id) {
+      throw new Error('No user found')
+    }
+    try {
+      const currentUser = await User.query().findById($.user.id)
+      const tiles = await currentUser
+        .$relatedQuery('tables')
+        .orderBy('created_at', 'desc')
+
+      return {
+        data: tiles.map((tile) => ({
+          value: tile.id,
+          name: tile.name,
+        })),
+      }
+    } catch (e) {
+      logger.error(e)
+      throw new Error('Unable to fetch list of tiles')
+    }
+  },
+}
+
+export default dynamicData
