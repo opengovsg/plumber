a99469973844f48de965ed017b6e3da7210fdc22
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2024-01-04 18:07:01 +0800

    PLU-125: [EXCEL-9] Pre-request checks (#373)

--- packages/backend/src/apps/m365-excel/common/interceptors/before-request.ts
+++ packages/backend/src/apps/m365-excel/common/interceptors/before-request.ts
@@ -1,9 +1,33 @@
 import type { TBeforeRequest } from '@plumber/types'
 
+import { RateLimiterRes } from 'rate-limiter-flexible'
+
+import { isM365TenantKey } from '@/config/app-env-vars/m365'
+import RetriableError from '@/errors/retriable-error'
+import logger from '@/helpers/logger'
+
 import { MS_GRAPH_OAUTH_BASE_URL } from '../constants'
 import { getAccessToken } from '../oauth/token-cache'
+import { consumeOrThrowLimiterWithLongestDelay } from '../rate-limiter'
 
-// TODO in later PR: usage tracker
+// This explicitly overcounts - e.g we will log if the request times out, even
+// we can't confirm that it reached Microsoft. The intent is to assume the worst
+// case scenario and not miss cases such as:
+// 1. We sent a request and it reached Microsoft.
+// 2. Microsoft responds; response is routed by various routers on the net.
+// 3. One of the routers in the response trip crashes and we get a timeout.
+const usageTracker: TBeforeRequest = async function ($, requestConfig) {
+  logger.info('Making request to MS Graph', {
+    event: 'm365-ms-graph-request',
+    baseUrl: requestConfig.baseURL, // base URL is different for auth requests
+    urlPath: requestConfig.url,
+    flowId: $.flow?.id,
+    stepId: $.step?.id,
+    executionId: $.execution?.id,
+  })
+
+  return requestConfig
+}
 
 const addAuthToken: TBeforeRequest = async function ($, requestConfig) {
   // Don't add token if we're trying to request a token.
@@ -27,8 +51,32 @@ const addAuthToken: TBeforeRequest = async function ($, requestConfig) {
   return requestConfig
 }
 
-// TODO in later PR: rate limiting request interceptor (this is temporary
-// until we implement the plumber-wide rate limiting system; building this
-// first to unblock M365 pilot).
+// This rate limiting request interceptor is slightly different from the planned
+// plumber-wide rate limiting system; we need to limit per request instead of
+// per action (that's also why this is placed in beforeRequest instead of in the
+// `run` function; an action may need multiple requests).
+const rateLimitCheck: TBeforeRequest = async function ($, requestConfig) {
+  const tenantKey = $.auth.data?.tenantKey as string
+  if (!isM365TenantKey(tenantKey)) {
+    throw new Error(`'${tenantKey}' is not a valid M365 tenant.`)
+  }
 
-export default [addAuthToken]
+  try {
+    await consumeOrThrowLimiterWithLongestDelay($, tenantKey, 1)
+  } catch (error) {
+    if (!(error instanceof RateLimiterRes)) {
+      return
+    }
+
+    throw new RetriableError({
+      error: 'Reached M365 rate limit',
+      delayInMs: error.msBeforeNext,
+    })
+  }
+
+  return requestConfig
+}
+
+// rateLimitCheck are explicitly the earliest interceptors so that the others
+// are not called if it throws an error.
+export default [rateLimitCheck, usageTracker, addAuthToken]
--- packages/backend/src/helpers/http-client/index.ts
+++ packages/backend/src/helpers/http-client/index.ts
@@ -6,6 +6,7 @@ import { IHttpClientParams } from '@plumber/types'
 import { URL } from 'url'
 
 import HttpError from '@/errors/http'
+import RetriableError from '@/errors/retriable-error'
 
 const removeBaseUrlForAbsoluteUrls = (
   requestConfig: InternalAxiosRequestConfig,
@@ -48,6 +49,20 @@ export default function createHttpClient({
   instance.interceptors.response.use(
     (response) => response,
     async (error) => {
+      // EDGE CASE: We allow actions / triggers to throw RetriableError, and
+      // some of them may choose to throw from beforeRequest. If this happens,
+      // Axios will still process the error through this response interceptor.
+      //
+      // Since RetriableError is supposed to be passed directly through to
+      // BullMQ, we special case it here instead of converting it to HttpError.
+      //
+      // Note that we still want to convert _other_ error types (even if
+      // they're not axios errors!) to HttpError; this helps prevent accidental
+      // sensitive data leakage via error objects in our logs.
+      if (error instanceof RetriableError) {
+        throw error
+      }
+
       // This handles system errors like ECONNREFUSED, which don't have a
       // response body.
       if (!error.response) {
