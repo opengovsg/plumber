438a8524e7971f686cc1650b180cacf6ef313b7e
Kuan Wee Loong <135598754+ogp-weeloong@users.noreply.github.com>
2024-01-06 22:07:23 +0800

    PLU-125: [EXCEL-10] Data classification checks (#374)

--- packages/backend/src/apps/m365-excel/dynamic-data/list-tables/index.ts
+++ packages/backend/src/apps/m365-excel/dynamic-data/list-tables/index.ts
@@ -3,6 +3,7 @@ import type { IDynamicData } from '@plumber/types'
 import { getM365TenantInfo } from '@/config/app-env-vars/m365'
 
 import { getRegisteredAuthData } from '../../common/auth-data'
+import { isFileTooSensitive } from '../../common/data-classification'
 
 const dynamicData: IDynamicData = {
   name: 'List Tables',
@@ -18,6 +19,15 @@ const dynamicData: IDynamicData = {
 
     const authData = getRegisteredAuthData($)
     const tenant = getM365TenantInfo(authData.tenantKey)
+
+    // Did not want to open a workbook session as user could just be casually
+    // browsing through files.
+    // FIXME (ogp-weeloong): move to a central file metadata cache to remove
+    // need for this check
+    if (await isFileTooSensitive(tenant, fileId as string, $.http)) {
+      throw new Error(`File is too sensitive!`)
+    }
+
     const results = await $.http.get<{
       value: Array<{ id: string; name: string }>
     }>(
