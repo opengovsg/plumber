315c64d04520a8e56e43279461d49c8038796234
Ian Chen <ian@open.gov.sg>
2024-01-24 14:37:29 +0800

    chore: refactor ld flag keys (#408)
    ## Problem
    
    Currently, all the launch darkly flags are scattered around the codebase
    as arbitrary strings.
    
    ## Solution
    
    1. Consolidate all these flag keys into a single config file for easier
    maintenance.
    2. Show `✨ Coming soon` badge for tiles with LD flag for internal
    testing
    
    <img width="285" alt="image"
    src="https://github.com/opengovsg/plumber/assets/10072985/53d8f1c7-0dad-49ae-936a-a91e56d5acae">

--- packages/frontend/src/pages/Tile/index.tsx
+++ packages/frontend/src/pages/Tile/index.tsx
@@ -1,8 +1,11 @@
 import { ITableMetadata, ITableRow } from '@plumber/types'
 
-import { useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { Center, Flex, Spinner } from '@chakra-ui/react'
+import { TILES_FEATURE_FLAG } from 'config/flags'
+import { LaunchDarklyContext } from 'contexts/LaunchDarkly'
 import { GET_ALL_ROWS } from 'graphql/queries/get-all-rows'
 import { GET_TABLE } from 'graphql/queries/get-table'
 
@@ -11,6 +14,9 @@ import TableBanner from './components/TableBanner'
 import { TableContextProvider } from './contexts/TableContext'
 
 export default function Tile(): JSX.Element {
+  const { flags, isLoading } = useContext(LaunchDarklyContext)
+  const navigate = useNavigate()
+
   const { tileId: tableId } = useParams<{ tileId: string }>()
 
   const { data: getTableData } = useQuery<{
@@ -29,6 +35,13 @@ export default function Tile(): JSX.Element {
     },
     fetchPolicy: 'network-only',
   })
+
+  /**
+   * Check if feature flag is enabled, otherwise redirect to 404
+   */
+  if (!isLoading && !flags?.[TILES_FEATURE_FLAG]) {
+    navigate('/404')
+  }
 
   if (!getTableData?.getTable || !getAllRowsData?.getAllRows) {
     return (
--- packages/frontend/src/pages/Tiles/index.tsx
+++ packages/frontend/src/pages/Tiles/index.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Flex } from '@chakra-ui/react'
 import Container from 'components/Container'
 import PageTitle from 'components/PageTitle'
+import { TILES_FEATURE_FLAG } from 'config/flags'
+import { LaunchDarklyContext } from 'contexts/LaunchDarkly'
 
 import CreateTileButton from './components/CreateTileButton'
 import TileList from './components/TileList'
@@ -8,6 +12,16 @@ import TileList from './components/TileList'
 const TILES_TITLE = 'Tiles'
 
 export default function Tiles(): JSX.Element {
+  /**
+   * Check if feature flag is enabled, otherwise redirect to 404
+   */
+  const { flags, isLoading } = useContext(LaunchDarklyContext)
+  const navigate = useNavigate()
+
+  if (!isLoading && !flags?.[TILES_FEATURE_FLAG]) {
+    navigate('/404')
+  }
+
   return (
     <Container py={7}>
       <Flex
