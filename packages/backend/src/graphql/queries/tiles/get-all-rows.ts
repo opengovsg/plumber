1062dae3b560bddd7ec5e533135a750220be80f0
Ian Chen <ian-@gmx.com>
2024-01-30 21:11:22 +0800

    feat: implement public auth for getTable and rows

--- packages/backend/src/graphql/queries/tiles/get-all-rows.ts
+++ packages/backend/src/graphql/queries/tiles/get-all-rows.ts
@@ -1,4 +1,8 @@
+import { NotFoundError } from 'objection'
+
+import InvalidTileViewKeyError from '@/errors/invalid-tile-view-key'
 import { getTableRows, TableRowItem } from '@/models/dynamodb/table-row'
+import TableMetadata from '@/models/table-metadata'
 import Context from '@/types/express/context'
 
 type Params = {
@@ -11,17 +15,40 @@ const getAllRows = async (
   context: Context,
 ): Promise<Pick<TableRowItem, 'rowId' | 'data'>[]> => {
   const { tableId } = params
-  const table = await context.currentUser
-    .$relatedQuery('tables')
-    .withGraphJoined('columns')
-    .findById(tableId)
-    .throwIfNotFound()
-  // update last accessed at for collaborator/table
-  await table.$relatedQuery('collaborators').patch({
-    lastAccessedAt: new Date().toISOString(),
-  })
-  const columnIds = table.columns.map((column) => column.id)
-  return getTableRows({ tableId, columnIds })
+
+  try {
+    const table = context.tilesViewKey
+      ? await TableMetadata.query()
+          .withGraphFetched('columns')
+          .findOne({
+            id: tableId,
+            view_only_key: context.tilesViewKey,
+          })
+          .throwIfNotFound()
+      : await context.currentUser
+          .$relatedQuery('tables')
+          .withGraphFetched('columns')
+          .findById(tableId)
+          .throwIfNotFound()
+
+    // update last accessed at for collaborator/table
+    if (!context.tilesViewKey) {
+      await table.$relatedQuery('collaborators').patch({
+        lastAccessedAt: new Date().toISOString(),
+      })
+    }
+
+    const columnIds = table.columns.map((column) => column.id)
+    return getTableRows({ tableId, columnIds })
+  } catch (e) {
+    if (e instanceof NotFoundError) {
+      if (context.tilesViewKey) {
+        throw new InvalidTileViewKeyError(tableId, context.tilesViewKey)
+      }
+      throw new Error('Table not found')
+    }
+    throw new Error('Error fetching rows')
+  }
 }
 
 export default getAllRows
--- packages/backend/src/graphql/queries/tiles/get-table.ts
+++ packages/backend/src/graphql/queries/tiles/get-table.ts
@@ -1,6 +1,6 @@
 import { NotFoundError } from 'objection'
-import { SetRequired } from 'type-fest'
 
+import InvalidTileViewKeyError from '@/errors/invalid-tile-view-key'
 import TableMetadata from '@/models/table-metadata'
 import Context from '@/types/express/context'
 
@@ -12,18 +12,28 @@ const getTable = async (
   _parent: unknown,
   params: Params,
   context: Context,
-): Promise<SetRequired<TableMetadata, 'columns'>> => {
+): Promise<TableMetadata> => {
   const { tableId } = params
 
   try {
-    const table = await context.currentUser
-      .$relatedQuery('tables')
-      .findById(tableId)
-      .throwIfNotFound()
+    const table = context.tilesViewKey
+      ? await TableMetadata.query()
+          .findOne({
+            id: tableId,
+            view_only_key: context.tilesViewKey,
+          })
+          .throwIfNotFound()
+      : await context.currentUser
+          .$relatedQuery('tables')
+          .findById(tableId)
+          .throwIfNotFound()
 
     return table
   } catch (e) {
     if (e instanceof NotFoundError) {
+      if (context.tilesViewKey) {
+        throw new InvalidTileViewKeyError(tableId, context.tilesViewKey)
+      }
       throw new Error('Table not found')
     }
     throw new Error('Error fetching table')
