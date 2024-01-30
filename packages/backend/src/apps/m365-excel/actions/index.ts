ef1069a244fe7db3eb30988ed9773335513ffb23
Kuan Wee Loong <weeloong@open.gov.sg>
2024-01-30 21:17:17 +0800

    feat: get table row action

--- packages/backend/src/apps/m365-excel/actions/index.ts
+++ packages/backend/src/apps/m365-excel/actions/index.ts
@@ -1,5 +1,6 @@
 import createTableRow from './create-table-row'
 import getCellValues from './get-cell-values'
+import getTableRow from './get-table-row'
 import writeCellValues from './write-cell-values'
 
-export default [createTableRow, getCellValues, writeCellValues]
+export default [createTableRow, getTableRow, getCellValues, writeCellValues]
