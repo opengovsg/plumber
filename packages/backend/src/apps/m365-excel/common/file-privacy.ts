657b525a75a3cbc09919da92b184fd0a45e4fbca
Kuan Wee Loong <weeloong@open.gov.sg>
2024-02-05 20:04:47 +0800

    fix: support other grantedTo fields

--- packages/backend/src/apps/m365-excel/common/file-privacy.ts
+++ packages/backend/src/apps/m365-excel/common/file-privacy.ts
@@ -9,6 +9,50 @@ import {
 
 import { AuthData } from './auth-data'
 
+// https://learn.microsoft.com/en-us/graph/api/resources/permission?view=graph-rest-1.0
+//
+// NOTE: We're very loose with parsing this as we're _only_ interested in users,
+// but MS Graph may return other identities like app identities. We parse what
+// we can and then collate all user emails into an array via a transform (hence
+// the variable name).
+const permissionUserSchema = z.object({
+  user: z
+    .object({
+      email: z.string().email().optional(),
+    })
+    .optional(),
+})
+const permissionSchemaWithCollatedEmails = z
+  .object({
+    roles: z.array(z.enum(['read', 'write', 'owner'])),
+
+    // https://learn.microsoft.com/en-us/graph/api/resources/identityset?view=graph-rest-1.0
+    grantedTo: permissionUserSchema.optional(),
+    grantedToIdentities: z.array(permissionUserSchema).optional(),
+
+    // https://learn.microsoft.com/en-us/graph/api/resources/sharepointidentityset?view=graph-rest-1.0
+    grantedToV2: permissionUserSchema.optional(),
+    grantedToIdentitiesV2: z.array(permissionUserSchema).optional(),
+  })
+  .transform((permission) => {
+    // Potentially empty
+    const emails = new Set<string>()
+
+    emails.add(permission.grantedTo?.user?.email ?? '')
+    emails.add(permission.grantedToV2?.user?.email ?? '')
+    for (const identity of permission.grantedToIdentities ?? []) {
+      emails.add(identity.user?.email ?? '')
+    }
+    for (const identity of permission.grantedToIdentitiesV2 ?? []) {
+      emails.add(identity.user?.email ?? '')
+    }
+
+    return {
+      roles: permission.roles,
+      emails: Array.from(emails).filter((email) => email.length > 0),
+    }
+  })
+
 const fileInfoResponseSchema = z
   .object({
     sensitivityLabel: z.object({
@@ -20,27 +64,14 @@ const fileInfoResponseSchema = z
       id: z.string().toUpperCase(),
       siteId: z.string().toUpperCase().nullish(),
     }),
-    // https://learn.microsoft.com/en-us/graph/api/resources/permission?view=graph-rest-1.0
-    permissions: z.array(
-      z.object({
-        roles: z.array(z.enum(['read', 'write', 'owner'])),
-        // Documented as deprecated in favor of grantedToV2, but have been
-        // unable to query V2 in my own testing.
-        // FIXME (ogp-weeloong): migrate to V2 when property is available.
-        grantedTo: z.object({
-          user: z.object({
-            email: z.string().email(),
-          }),
-        }),
-      }),
-    ),
+    permissions: z.array(permissionSchemaWithCollatedEmails),
   })
   .transform((response) => ({
     sensitivityLabelGuid: response.sensitivityLabel.id ?? null,
     usersWithWriteAccess: new Set(
       response.permissions
         .filter((p) => p.roles.includes('write') || p.roles.includes('owner'))
-        .map((p) => p.grantedTo.user.email),
+        .flatMap((p) => p.emails),
     ),
     parentDriveType: response.parentReference.driveType,
     parentDirectoryId: response.parentReference.id,
@@ -52,13 +83,10 @@ async function queryFilePrivacyInfo(
   fileId: string,
   http: IHttpClient,
 ): Promise<z.infer<typeof fileInfoResponseSchema>> {
-  return fileInfoResponseSchema.parse(
-    (
-      await http.get(
-        `/v1.0/sites/${tenant.sharePointSiteId}/drive/items/${fileId}/?$select=sensitivityLabel,parentReference&$expand=permissions`,
-      )
-    ).data,
+  const response = await http.get(
+    `/v1.0/sites/${tenant.sharePointSiteId}/drive/items/${fileId}/?$select=sensitivityLabel,parentReference&$expand=permissions`,
   )
+  return fileInfoResponseSchema.parse(response.data)
 }
 
 export async function validateCanAccessFile(
