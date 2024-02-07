d0389cf23c959e305331b0b14824cf87adb50088
Ian Chen <ian@open.gov.sg>
2024-02-07 14:24:02 +0800

    PLU-179: slack channel dropdown not showing all public channels (#426)

--- packages/backend/src/apps/slack/dynamic-data/list-channels/index.ts
+++ packages/backend/src/apps/slack/dynamic-data/list-channels/index.ts
@@ -2,39 +2,78 @@ import {
   DynamicDataOutput,
   IDynamicData,
   IGlobalVariable,
-  IJSONObject,
 } from '@plumber/types'
+
+import logger from '@/helpers/logger'
+
+interface SlackConversation {
+  id: string
+  name: string
+  is_channel: boolean
+  is_archived: boolean
+}
+
+interface ListChannelResponse {
+  ok: boolean
+  channels: Array<SlackConversation>
+  response_metadata?: {
+    next_cursor: string
+  }
+}
+
+// we want to limit the number of pages the API traverses to prevent rate limits or timeouts
+const MAX_PAGES = 4
 
 const dynamicData: IDynamicData = {
   name: 'List channels',
   key: 'listChannels',
 
   async run($: IGlobalVariable): Promise<DynamicDataOutput> {
-    const channels: DynamicDataOutput = {
+    const result: DynamicDataOutput = {
       data: [],
       error: null,
     }
 
-    const response = await $.http.get('/conversations.list', {
-      params: {
-        exclude_archived: true,
-        types: 'public_channel',
-        limit: 1000,
-      },
-    })
+    let cursor: string | undefined
+    let pages = 0
 
-    if (response.data.ok === false) {
-      throw new Error(response.data)
-    }
+    do {
+      const { data } = await $.http
+        .get<ListChannelResponse>('/conversations.list', {
+          params: {
+            exclude_archived: true,
+            // do not add private_channel to this list as it will return a lot of channels
+            // and cause this dynamic data api to timeout. instead, we allow the user to
+            // manually enter the private channel ID
+            types: 'public_channel',
+            limit: 1000,
+            cursor,
+          },
+        })
+        .catch((error) => {
+          logger.error(error)
+          throw new Error('Failed to list channels')
+        })
 
-    channels.data = response.data.channels.map((channel: IJSONObject) => {
-      return {
-        value: channel.id,
-        name: channel.name,
+      if (data.ok === false) {
+        logger.error(data)
+        throw new Error('Failed to list channels')
       }
-    })
 
-    return channels
+      cursor = data.response_metadata?.next_cursor
+
+      const channels = data.channels.map((channel) => {
+        return {
+          value: channel.id,
+          name: channel.name,
+        }
+      })
+
+      result.data.push(...channels)
+      pages++
+    } while (cursor && pages < MAX_PAGES)
+
+    return result
   },
 }
 
