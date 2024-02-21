5ae05e010825ff467359e1b11d03220d0fb8010d
Ian Chen <ian-@gmx.com>
2024-02-21 17:19:15 +0800

    chore: change css for empty tiles page

--- packages/frontend/src/pages/Tiles/components/EmptyTileList.tsx
+++ packages/frontend/src/pages/Tiles/components/EmptyTileList.tsx
@@ -1,40 +1,33 @@
-import { Image, Stack, Text, VStack } from '@chakra-ui/react'
+import { Image, Text, VStack } from '@chakra-ui/react'
 import spreadsheetImg from 'assets/spreadsheet.png'
 
 import CreateTileButton from './CreateTileButton'
 
 const EmptyTileList = (): JSX.Element => {
   return (
-    <Stack
-      direction={{ base: 'column', md: 'row' }}
-      maxW="1000px"
+    <VStack
+      maxW="600px"
       margin="auto"
       gap={8}
       px={8}
       alignItems="center"
       justifyContent="center"
     >
-      <Image src={spreadsheetImg} alt="Spreadsheet" w="400px" maxW="50%" />
-      <VStack alignItems={{ base: 'center', md: 'start' }} gap={8}>
+      <Image src={spreadsheetImg} alt="Spreadsheet" w="400px" maxW="50vw" />
+      <Text textStyle="h4" textAlign="center" fontWeight="normal">
         <Text
-          textStyle="h4"
-          textAlign={{ base: 'center', md: 'left' }}
-          fontWeight="normal"
+          bgGradient="linear(to-r, primary.400, primary.600)"
+          backgroundClip="text"
+          as="span"
+          fontWeight="bold"
         >
-          <Text
-            bgGradient="linear(to-r, primary.400, primary.600)"
-            backgroundClip="text"
-            as="span"
-            fontWeight="bold"
-          >
-            Tiles
-          </Text>{' '}
-          is a simple database to view, store and automate your data — all in
-          one place.
-        </Text>
-        <CreateTileButton />
-      </VStack>
-    </Stack>
+          Tiles
+        </Text>{' '}
+        is a simple database to view, store and automate your data — all in one
+        place.
+      </Text>
+      <CreateTileButton />
+    </VStack>
   )
 }
 
