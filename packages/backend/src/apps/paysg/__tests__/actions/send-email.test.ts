b0b938757a14bf154b95163bd6b8b86cfbbe6a5d
zsoh97 <41435427+zsoh97@users.noreply.github.com>
2024-02-28 14:20:50 +0800

    feat: paysg/send email (#447)
    Co-authored-by: Kuan Wee Loong <weeloong@open.gov.sg>

---
+++ packages/backend/src/apps/paysg/__tests__/actions/send-email.test.ts
@@ -0,0 +1,66 @@
+import type { IGlobalVariable } from '@plumber/types'
+
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import app from '../..'
+import sendEmailAction from '../../actions/send-email'
+import { getApiBaseUrl } from '../../common/api'
+
+const mocks = vi.hoisted(() => ({
+  httpPost: vi.fn(() => ({})),
+}))
+
+describe('send payment email', () => {
+  let $: IGlobalVariable
+
+  beforeEach(() => {
+    $ = {
+      auth: {
+        set: vi.fn(),
+        data: {
+          apiKey: 'paysg_stag_abcd',
+          paymentServiceId: 'sample-payment-service-id',
+        },
+      },
+      step: {
+        id: 'herp-derp',
+        appKey: 'paysg',
+        position: 2,
+        parameters: {
+          // Pre-fill some required fields
+          paymentId: 'sample-payment-id',
+        },
+      },
+      http: {
+        post: mocks.httpPost,
+      } as unknown as IGlobalVariable['http'],
+      setActionItem: vi.fn(),
+      app,
+    }
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it.each(['paysg_stag_abcd, paysg_live_abcd'])(
+    'invokes the correct URL based on the API key and payment ID',
+    async (apiKey) => {
+      $.auth.data.apiKey = apiKey
+      const expectedBaseUrl = getApiBaseUrl($.auth.data.apiKey)
+
+      await sendEmailAction.run($)
+
+      expect(mocks.httpPost).toHaveBeenCalledWith(
+        '/v1/payment-services/sample-payment-service-id/payments/sample-payment-id/send-email',
+        {},
+        expect.objectContaining({
+          baseURL: expectedBaseUrl,
+          headers: {
+            'x-api-key': $.auth.data.apiKey,
+          },
+        }),
+      )
+    },
+  )
+})
--- packages/backend/src/apps/paysg/actions/index.ts
+++ packages/backend/src/apps/paysg/actions/index.ts
@@ -1,4 +1,5 @@
 import createPayment from './create-payment'
 import getPayment from './get-payment'
+import sendEmail from './send-email'
 
-export default [createPayment, getPayment]
+export default [createPayment, getPayment, sendEmail]
