89f7a073eb2c101d46bf13597c73a70b4a6931a1
Kuan Wee Loong <weeloong@open.gov.sg>
2024-03-15 17:25:13 +0800

    fix: enable automatic retries for 503

--- packages/backend/src/apps/m365-excel/common/interceptors/request-error-handler.ts
+++ packages/backend/src/apps/m365-excel/common/interceptors/request-error-handler.ts
@@ -1,12 +1,16 @@
 import type { IApp } from '@plumber/types'
 
+import RetriableError from '@/errors/retriable-error'
 import logger from '@/helpers/logger'
 
-const http429Handler: IApp['requestErrorHandler'] = async function ($, error) {
-  if (error.response.status !== 429) {
-    return
-  }
+type ThrowingHandler = (
+  ...args: Parameters<IApp['requestErrorHandler']>
+) => never
 
+//
+// Handle MS rate limiting us
+//
+const handle429: ThrowingHandler = ($, error) => {
   // A 429 response is considered a SEV-2+ incident for some tenants; log it
   // explicitly so that we can easily trigger incident creation from DD.
   logger.error('Received HTTP 429 from MS Graph', {
@@ -23,4 +27,39 @@ const http429Handler: IApp['requestErrorHandler'] = async function ($, error) {
   throw new Error('Rate limited by Microsoft Graph.')
 }
 
-export default http429Handler
+//
+// Retry failures due to flakey M365 servers
+//
+const handle503: ThrowingHandler = function ($, error) {
+  // Log to monitor spikes, just in case
+  logger.error('Received HTTP 503 from MS Graph', {
+    event: 'm365-http-503',
+    tenant: $.auth?.data?.tenantKey as string,
+    baseUrl: error.response.config.baseURL,
+    url: error.response.config.url,
+    flowId: $.flow?.id,
+    stepId: $.step?.id,
+    executionId: $.execution?.id,
+  })
+
+  // Microsoft _sometimes_ specifies a Retry-After when it returns 503.
+  const retryAfterMs = Number(error.response?.headers?.['retry-after'] ?? null)
+  throw new RetriableError({
+    error: 'Encountered HTTP 503 from MS',
+    delayInMs:
+      isNaN(retryAfterMs) || retryAfterMs === 0
+        ? 'default'
+        : retryAfterMs * 1000,
+  })
+}
+
+const errorHandler: IApp['requestErrorHandler'] = async function ($, error) {
+  switch (error.response.status) {
+    case 429:
+      return handle429($, error)
+    case 503:
+      return handle503($, error)
+  }
+}
+
+export default errorHandler
