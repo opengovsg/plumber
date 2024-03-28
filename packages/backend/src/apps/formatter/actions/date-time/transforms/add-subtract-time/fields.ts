ef22c78b5f8ce9833ea6a7a80489830ca57e2b87
Kuan Wee Loong <weeloong@open.gov.sg>
2024-03-29 00:18:08 +0800

    chore: simplify zod

--- packages/backend/src/apps/formatter/actions/date-time/transforms/add-subtract-time/fields.ts
+++ packages/backend/src/apps/formatter/actions/date-time/transforms/add-subtract-time/fields.ts
@@ -2,38 +2,36 @@ import { type DurationLikeObject } from 'luxon'
 import z from 'zod'
 
 import { type TransformSpec } from '@/apps/formatter/common/transform-spec'
+import { ensureZodObjectKey } from '@/helpers/zod-utils'
+
+const opTypeEnum = z.enum(['add', 'subtract'])
+
+const timeUnitEnum = z.enum([
+  'seconds',
+  'minutes',
+  'hours',
+  'days',
+  'months',
+  // To simplify date math code, values have the same keys as luxton
+  // duration objects.
+] as const satisfies ReadonlyArray<keyof DurationLikeObject>)
+
+const opsSchema = z.object({
+  opType: opTypeEnum,
+  timeUnit: timeUnitEnum,
+  timeAmount: z
+    .string()
+    .transform((amount) => Number(amount))
+    .pipe(z.number().int().positive()),
+})
 
 export const fieldSchema = z
   .object({
-    addSubtractTimeOps: z
-      .array(
-        z.object({
-          opType: z.enum(['add', 'subtract']),
-          timeUnit: z.enum([
-            'seconds',
-            'minutes',
-            'hours',
-            'days',
-            'months',
-            // To simplify date math code, values have the same keys as luxton
-            // duration objects.
-          ] as const satisfies ReadonlyArray<keyof DurationLikeObject>),
-          timeAmount: z
-            .string()
-            .transform((amount) => Number(amount))
-            .pipe(z.number().int().positive()),
-        }),
-      )
-      .min(1),
+    addSubtractTimeOps: z.array(opsSchema).min(1),
   })
   .transform((params) => ({
     ops: params.addSubtractTimeOps,
   }))
-
-// FIXME: Some jank zod-fu here as I am trying to prevent duplicating data across
-// zod schema + IFields[]. :/
-const subFieldsSchema =
-  fieldSchema.sourceType().shape.addSubtractTimeOps.element
 
 function sentenceCase(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1)
@@ -42,37 +40,37 @@ function sentenceCase(s: string): string {
 export const fields: TransformSpec['fields'] = [
   {
     label: 'Specify the amount of time you want to add or subtract',
-    key: fieldSchema.sourceType().keyof().enum.addSubtractTimeOps,
+    key: ensureZodObjectKey(fieldSchema.sourceType(), 'addSubtractTimeOps'),
     type: 'multirow' as const,
     required: true,
     subFields: [
       {
         placeholder: 'Add or subtract?',
-        key: subFieldsSchema.keyof().enum.opType,
+        key: ensureZodObjectKey(opsSchema, 'opType'),
         type: 'dropdown' as const,
         required: true,
         variables: false,
         showOptionValue: false,
-        options: subFieldsSchema.shape.opType.options.map((opName) => ({
-          label: sentenceCase(opName),
-          value: opName,
+        options: opTypeEnum.options.map((op) => ({
+          label: sentenceCase(op),
+          value: op,
         })),
       },
       {
         placeholder: 'Amount of time to add or subtract (number)',
-        key: subFieldsSchema.keyof().enum.timeAmount,
+        key: ensureZodObjectKey(opsSchema, 'timeAmount'),
         type: 'string' as const,
         required: true,
         variables: true,
       },
       {
         placeholder: 'Unit of time to add or subtract',
-        key: subFieldsSchema.keyof().enum.timeUnit,
+        key: ensureZodObjectKey(opsSchema, 'timeUnit'),
         type: 'dropdown' as const,
         required: true,
         variables: false,
         showOptionValue: false,
-        options: subFieldsSchema.shape.timeUnit.options.map((unit) => ({
+        options: timeUnitEnum.options.map((unit) => ({
           label: sentenceCase(unit),
           value: unit,
         })),
