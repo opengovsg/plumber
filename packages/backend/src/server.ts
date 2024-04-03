2afc79ac007555c87755e9ce00d242d8195990bb
Ian Chen <ian@open.gov.sg>
2024-04-03 19:01:13 +0800

    PLU-255 Increase server timeout to hopefully eliminate random 5xx (#508)
    ref: https://github.com/opengovsg/askgov/pull/743

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -9,6 +9,8 @@ import logger from '@/helpers/logger'
 
 const port = appConfig.port
 
+const ALB_IDLE_TIMEOUT_SECONDS = 60
+
 const server: Server = app.listen(port, () => {
   logger.info(`Server is listening on ${port}`)
 })
@@ -20,3 +22,8 @@ function shutdown(server: Server) {
 process.on('SIGTERM', () => {
   shutdown(server)
 })
+
+// As per AWS guidelines, we should set the server timeout to be longer than ALB idle timeout
+// https://repost.aws/knowledge-center/elb-alb-troubleshoot-502-errors
+server.keepAliveTimeout = (ALB_IDLE_TIMEOUT_SECONDS + 1) * 1000
+server.headersTimeout = (ALB_IDLE_TIMEOUT_SECONDS + 2) * 1000
