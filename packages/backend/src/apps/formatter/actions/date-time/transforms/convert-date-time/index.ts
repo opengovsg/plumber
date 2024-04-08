07b3f4ba254b98f78fc149425b2e6396506fa2ce
Kuan Wee Loong <weeloong@open.gov.sg>
2024-04-08 10:50:01 +0800

    feat: better error copy for convert date time

--- packages/backend/src/apps/formatter/actions/date-time/transforms/convert-date-time/index.ts
+++ packages/backend/src/apps/formatter/actions/date-time/transforms/convert-date-time/index.ts
@@ -1,3 +1,10 @@
+import type { IGlobalVariable } from '@plumber/types'
+
+import { ZodError } from 'zod'
+
+import StepError, { GenericSolution } from '@/errors/step'
+import { firstZodParseError } from '@/helpers/zod-utils'
+
 import type { TransformSpec } from '../../../../common/transform-spec'
 import {
   fieldSchema as dateTimeFormatSchema,
@@ -5,6 +12,28 @@ import {
 } from '../../common/date-time-format'
 
 import { field, fieldSchema } from './fields'
+
+// Helper function to provide a nice StepError
+function getParams($: IGlobalVariable) /* inferred return type */ {
+  try {
+    const { dateTimeFormat } = dateTimeFormatSchema.parse($.step.parameters)
+    const { formatDateTimeToFormat: formatString } = fieldSchema.parse(
+      $.step.parameters,
+    )
+    return { dateTimeFormat, formatString }
+  } catch (error) {
+    if (!(error instanceof ZodError)) {
+      throw error
+    }
+
+    throw new StepError(
+      `Configuration problem: '${firstZodParseError(error)}'`,
+      GenericSolution.ReconfigureInvalidField,
+      $.step.position,
+      $.app.name,
+    )
+  }
+}
 
 export const spec = {
   id: 'formatDateTime',
@@ -17,12 +46,26 @@ export const spec = {
   fields: [field],
 
   transformData: async ($, valueToTransform) => {
-    const { dateTimeFormat } = dateTimeFormatSchema.parse($.step.parameters)
-    const { formatDateTimeToFormat: formatString } = fieldSchema.parse(
-      $.step.parameters,
-    )
+    try {
+      const { dateTimeFormat, formatString } = getParams($)
+      const dateTime = parseDateTime(dateTimeFormat, valueToTransform)
 
-    const dateTime = parseDateTime(dateTimeFormat, valueToTransform)
-    $.setActionItem({ raw: { result: dateTime.toFormat(formatString) } })
+      $.setActionItem({
+        raw: {
+          result: dateTime.toFormat(formatString),
+        },
+      })
+    } catch (error) {
+      if (error instanceof StepError) {
+        throw error
+      }
+
+      throw new StepError(
+        `Error with the original value: '${error.message}'`,
+        'Ensure that you have selected the correct date format for your original value.',
+        $.step.position,
+        $.app.name,
+      )
+    }
   },
 } satisfies TransformSpec
