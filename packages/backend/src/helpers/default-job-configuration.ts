4469366f63f3793d391ffb55a336ab3fff50d136
Kuan Wee Loong <weeloong@open.gov.sg>
2024-04-18 10:48:28 +0800

    chore: bump retry count v2

--- packages/backend/src/helpers/default-job-configuration.ts
+++ packages/backend/src/helpers/default-job-configuration.ts
@@ -10,7 +10,18 @@ export const REMOVE_AFTER_7_DAYS_OR_50_JOBS = {
 }
 
 export const DEFAULT_JOB_DELAY_DURATION = 0
-export const MAXIMUM_JOB_ATTEMPTS = 20 // FIXME (ogp-weeloong): high number to handle expected increased retries from M465. Revert back to 6 once BullMQ Pro is in.
+
+// FIXME (ogp-weeloong): high number to handle expected increased retries from
+// M465. Revert back to 6 once BullMQ Pro is in.
+//
+// Number chosen as follows:
+// - Excel rate limit is 6 per 3 seconds
+// - Excel pipes tend to spike at ~100 submissions per 3 second window
+// - Each excel step takes 2 queries = ~3 excel steps progresses per window,
+//   others get retried. Under high concurrency, we may half this number as all
+//   steps share the same limiter.
+// - So in the worst case, a step may need to be retried 100 / 1.5 ~= 60 times.
+export const MAXIMUM_JOB_ATTEMPTS = 60
 
 export const DEFAULT_JOB_OPTIONS: JobsOptions = {
   removeOnComplete: REMOVE_AFTER_7_DAYS_OR_50_JOBS,
