74349b3d008884cc102af9c66be021de660d40ba
m0nggh <malcolmong27@gmail.com>
2024-05-02 18:43:15 +0800

    add model query instead of raw query

--- packages/backend/src/graphql/queries/get-plumber-stats.ts
+++ packages/backend/src/graphql/queries/get-plumber-stats.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 
 import { client } from '@/config/database'
 import { createRedisClient, REDIS_DB_INDEX } from '@/config/redis'
+import Execution from '@/models/execution'
 
 import type { QueryResolvers } from '../__generated__/types.generated'
 
@@ -13,20 +14,19 @@ const getPlumberStats: QueryResolvers['getPlumberStats'] = async () => {
     const { userCount, executionCount } = await redisClient.hgetall(REDIS_KEY)
 
     return {
-      userCount,
-      executionCount,
+      userCount: +userCount,
+      executionCount: +executionCount,
     }
   }
 
-  const [userCountQuery, executionCountQuery] = await Promise.all([
+  const [userCountQuery, executionCount] = await Promise.all([
     client.raw(
       `SELECT reltuples AS estimate FROM pg_class WHERE relname = 'users';`,
     ),
-    client.raw(`select count(*) from executions where test_run = false;`),
+    Execution.query().withSoftDeleted().where('test_run', false).resultSize(),
   ])
 
   const userCount = userCountQuery.rows[0].estimate
-  const executionCount = executionCountQuery.rows[0].count
 
   await redisClient
     .multi()
