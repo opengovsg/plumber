9b77109dd277ed4757b371dead970d8ceaee259a
m0nggh <malcolmong27@gmail.com>
2024-05-16 10:44:24 +0800

    add single select component from ogp ds

---
+++ packages/frontend/src/components/SingleSelect/components/SelectCombobox/SelectCombobox.tsx
@@ -0,0 +1,109 @@
+import { forwardRef, useCallback, useMemo } from 'react'
+import {
+  Flex,
+  Icon,
+  InputGroup,
+  Stack,
+  Text,
+  useMergeRefs,
+} from '@chakra-ui/react'
+import { Input } from '@opengovsg/design-system-react'
+
+import { useSelectContext } from '../../SelectContext'
+import { itemToIcon, itemToLabelString } from '../../utils/itemUtils'
+
+import { ComboboxClearButton } from './ComboboxClearButton'
+import { ToggleChevron } from './ToggleChevron'
+
+export const SelectCombobox = forwardRef<HTMLInputElement>(
+  (_props, ref): JSX.Element => {
+    const {
+      toggleMenu,
+      selectedItem,
+      getInputProps,
+      styles,
+      isDisabled,
+      isSearchable,
+      isReadOnly,
+      isInvalid,
+      inputValue,
+      isRequired,
+      placeholder,
+      isOpen,
+      resetInputValue,
+      inputRef,
+      isClearable,
+      size,
+    } = useSelectContext()
+
+    const mergedInputRef = useMergeRefs(inputRef, ref)
+
+    const selectedItemMeta = useMemo(
+      () => ({
+        icon: itemToIcon(selectedItem),
+        label: itemToLabelString(selectedItem),
+      }),
+      [selectedItem],
+    )
+
+    const handleToggleMenu = useCallback(() => {
+      if (isReadOnly || isDisabled) {
+        return
+      }
+      return toggleMenu()
+    }, [isDisabled, isReadOnly, toggleMenu])
+
+    return (
+      <Flex>
+        <InputGroup
+          size={size}
+          pos="relative"
+          display="grid"
+          marginInlineEnd={isClearable ? '-1px' : undefined}
+          _focusWithin={{
+            zIndex: 1,
+          }}
+          gridTemplateColumns="1fr"
+        >
+          <Stack
+            visibility={inputValue ? 'hidden' : 'initial'}
+            direction="row"
+            spacing="1rem"
+            aria-disabled={isDisabled}
+            sx={styles.selected}
+            aria-hidden
+          >
+            {selectedItemMeta.icon ? (
+              <Icon
+                sx={styles.icon}
+                as={selectedItemMeta.icon}
+                aria-disabled={isDisabled}
+              />
+            ) : null}
+            <Text noOfLines={1}>{selectedItemMeta.label}</Text>
+          </Stack>
+          <Input
+            isReadOnly={!isSearchable || isReadOnly}
+            isInvalid={isInvalid}
+            isDisabled={isDisabled}
+            placeholder={selectedItem ? undefined : placeholder}
+            sx={styles.field}
+            {...getInputProps({
+              onClick: handleToggleMenu,
+              onBlur: () => !isOpen && resetInputValue(),
+              ref: mergedInputRef,
+              disabled: isDisabled,
+              readOnly: isReadOnly,
+              required: isRequired,
+              'aria-expanded': !!isOpen,
+            })}
+          />
+          <ToggleChevron />
+        </InputGroup>
+        <ComboboxClearButton />
+      </Flex>
+    )
+  },
+)
+
+SelectCombobox.displayName = 'SelectCombobox'
