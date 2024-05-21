a3a28b2fc808d8e87462564c158d2dff34830ed4
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2024-05-21 15:33:20 +0800

    PLU-242: feat: duplicate flow (#551)

---
+++ packages/frontend/src/components/FlowRow/MenuAlertDialog.tsx
@@ -0,0 +1,81 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+} from '@chakra-ui/react'
+import { Button } from '@opengovsg/design-system-react'
+
+export type AlertDialogType = 'delete' | 'duplicate'
+
+interface MenuAlertDialogProps {
+  isDialogOpen: boolean
+  cancelRef: React.RefObject<HTMLButtonElement>
+  onDialogClose: () => void
+  type: AlertDialogType
+  onClick: () => void
+  isLoading: boolean
+}
+
+interface AlertDialogContent {
+  header: string
+  body: string
+  buttonText: string
+}
+
+function getAlertDialogContent(type: AlertDialogType): AlertDialogContent {
+  switch (type) {
+    case 'delete':
+      return {
+        header: 'Delete Pipe',
+        body: "Are you sure you want to delete this pipe? You can't undo this action afterwards.",
+        buttonText: 'Delete',
+      }
+    case 'duplicate':
+      return {
+        header: 'Duplicate Pipe',
+        body: `You'll need to test all your steps before publishing.`,
+        buttonText: 'Duplicate',
+      }
+  }
+}
+
+export default function MenuAlertDialog(props: MenuAlertDialogProps) {
+  const { isDialogOpen, cancelRef, onDialogClose, type, onClick, isLoading } =
+    props
+  const { header, body, buttonText } = getAlertDialogContent(type)
+
+  return (
+    <AlertDialog
+      isOpen={isDialogOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onDialogClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            {header}
+          </AlertDialogHeader>
+
+          <AlertDialogBody>{body}</AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button
+              variant="clear"
+              colorScheme="secondary"
+              ref={cancelRef}
+              onClick={onDialogClose}
+            >
+              Cancel
+            </Button>
+            <Button onClick={onClick} ml={3} isLoading={isLoading}>
+              {buttonText}
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
