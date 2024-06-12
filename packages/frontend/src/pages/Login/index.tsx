d89e2846b0adb7780c12d9d35a70f05514458f41
m0nggh <malcolmong27@gmail.com>
2024-06-12 12:22:20 +0800

    change login page layout

--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -1,99 +1,49 @@
-import * as React from 'react'
-import { FormattedMessage } from 'react-intl'
-import { useNavigate } from 'react-router-dom'
-import { Box, Typography, useMediaQuery } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
-import mainLogo from 'assets/logo.svg'
+import { ReactElement } from 'react'
+import { Box, Center, Flex, Image, Show, Text } from '@chakra-ui/react'
 import landingImg from 'assets/plumber-landing.svg'
-import Container from 'components/Container'
+import mainLogo from 'assets/plumber-logo.svg'
 import LoginForm from 'components/LoginForm'
-import * as URLS from 'config/urls'
 
-export default function Login(): React.ReactElement {
-  const theme = useTheme()
-  const navigate = useNavigate()
-
-  const matchSmallScreens = useMediaQuery(theme.breakpoints.down('md'), {
-    noSsr: true,
-  })
-
+export default function Login(): ReactElement {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flex: 1,
-        alignItems: 'stretch',
-        flexDirection: matchSmallScreens ? 'column' : 'row',
-      }}
-    >
-      <Box
+    <Flex flex={1} alignItems="stretch" flexDir={{ base: 'column', md: 'row' }}>
+      {/* Left half of the page: Login graphic */}
+      <Flex
         flex={1}
-        maxWidth="700px"
-        display={matchSmallScreens ? 'none' : 'flex'}
-        color="white"
-        sx={{ backgroundColor: 'primary.light' }}
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
+        flexDir="column"
+        display={{ base: 'none', md: 'flex' }}
+        bg="primary.50"
+        color="primary.600"
+        pt="3.75rem"
+        gap={{ base: '3rem', lg: '4.5rem' }}
       >
-        <Typography
-          variant="h2"
-          component="h3"
-          color="primary.main"
-          marginX={10}
-          marginBottom={5}
-        >
-          Build and automate your work<i>flows</i>
-        </Typography>
-        <Box
-          component="img"
-          src={landingImg}
-          width="100%"
-          left="0"
-          paddingRight={10}
-        />
-      </Box>
-      <Box
-        flex={matchSmallScreens ? 2 : 1}
-        width="100%"
-        paddingTop={matchSmallScreens ? '5vh' : 0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <Container maxW="container.sm">
-          <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems={matchSmallScreens ? 'center' : 'start'}
-          >
-            {matchSmallScreens && (
-              <Box component="img" src={landingImg} width="35vw" mb={3} />
-            )}
-            <Typography
-              variant={'h2'}
-              fontSize={'1.5rem'}
-              noWrap
-              fontWeight="bold"
-              fontFamily={'Space Grotesk'}
-              marginBottom={3}
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                cursor: 'pointer',
-                userSelect: 'none',
-              }}
-              onClick={() => navigate(URLS.ROOT)}
-            >
-              <Box component="img" src={mainLogo} height="100%" mr={1} />
-              <FormattedMessage id="brandText" />
-            </Typography>
-          </Box>
-          <LoginForm />
-        </Container>
-      </Box>
-    </Box>
+        <Flex flexDir="column" gap={2} pl={11}>
+          <Text textStyle="heavy" maxW="60%">
+            Automate your manual and repititive tasks
+          </Text>
+          <Text textStyle="body-1" maxW="80%">
+            A no-code tool that connects multiple applications together to
+            automate your workflows.
+          </Text>
+        </Flex>
+
+        <Show above="md">
+          <Center pr={8}>
+            <Image
+              src={landingImg}
+              w={{ base: '80%', lg: '60%', '2xl': '65%' }}
+            />
+          </Center>
+        </Show>
+      </Flex>
+
+      {/* Right half of the page: Login input */}
+      <Flex flexDir="column" flex={1} gap={12} justifyContent="center" px="5vw">
+        <Box>
+          <Image h={10} src={mainLogo} alt="plumber-logo" />
+        </Box>
+        <LoginForm />
+      </Flex>
+    </Flex>
   )
 }
