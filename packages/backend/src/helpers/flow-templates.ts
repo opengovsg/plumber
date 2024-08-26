f4f938c33c95404deabbbe75604eac7768a9e34e
m0nggh <malcolmong27@gmail.com>
2024-08-26 10:32:02 +0800

    add pre template automation

--- packages/backend/src/helpers/flow-templates.ts
+++ packages/backend/src/helpers/flow-templates.ts
@@ -1,4 +1,4 @@
-import type { IApp } from '@plumber/types'
+import type { IApp, IJSONObject } from '@plumber/types'
 
 import apps from '@/apps'
 import { TEMPLATES } from '@/db/storage'
@@ -6,6 +6,7 @@ import type {
   FlowConfig,
   TemplateStep,
 } from '@/graphql/__generated__/types.generated'
+import { createTableRows } from '@/models/dynamodb/table-row'
 import Flow from '@/models/flow'
 import User from '@/models/user'
 
@@ -35,6 +36,65 @@ function validateAppAndEventKey(step: TemplateStep, templateName: string) {
   }
 }
 
+// This replaces the keys of columns which are names to the column ids for row data
+// e.g. { Rating: 5 } --> { column_id: 5 }
+function replaceRowData(
+  rowData: IJSONObject[],
+  columnNameToIdMap: Record<string, string>,
+) {
+  const replacedRowData: IJSONObject[] = []
+  for (const row of rowData) {
+    const newRow: IJSONObject = {}
+    for (const key in row) {
+      if (!columnNameToIdMap[key]) {
+        throw new Error(
+          'Creation of tile ran into an issue, please contact support@plumber.gov.sg',
+        )
+      }
+      const newKey = columnNameToIdMap[key]
+      newRow[newKey] = row[key]
+    }
+    replacedRowData.push(newRow)
+  }
+  return replacedRowData
+}
+
+/**
+ * This replaces the column name to the column id in each tile event data.
+ * This is because tiles apis uses column ids instead of the names
+ * e.g. { columnName: <Name> } --> { columnId: <id> }
+ * Then, generates parameters based on the format for each tile action
+ */
+function generateParametersForTilesAction(
+  tileEventData: IJSONObject[],
+  columnNameToIdMap: Record<string, string>,
+  eventKey: string,
+) {
+  const updatedTileEventData: IJSONObject[] = JSON.parse(
+    JSON.stringify(tileEventData),
+  )
+
+  for (const row of updatedTileEventData) {
+    row['columnId'] = columnNameToIdMap[String(row['columnName'])]
+    delete row['columnName']
+  }
+
+  // This follows the parameters format for each tile action
+  switch (eventKey) {
+    case 'createTileRow':
+    case 'updateSingleRow':
+      return {
+        rowData: updatedTileEventData,
+      } as IJSONObject
+    case 'findSingleRow':
+      return {
+        filters: updatedTileEventData,
+        returnLastRow: true,
+      } as IJSONObject
+  }
+  return {} as IJSONObject
+}
+
 export async function createFlowFromTemplate(
   templateId: string,
   user: User,
@@ -47,7 +107,7 @@ export async function createFlowFromTemplate(
   }
 
   // check if the template is a demo template first: affects name, config and logging only
-  const { name: flowName, steps, tag } = template
+  const { name: flowName, steps, tag, tileTemplateData } = template
   const isDemoTemplate = tag === 'demo'
   const flowConfig: FlowConfig = isDemoTemplate
     ? {
@@ -72,6 +132,29 @@ export async function createFlowFromTemplate(
     })
 
     validateAppAndEventKey(steps[0], flowName)
+    // create a tile with its columns and rows first if template uses tiles
+    let tableId: string | undefined
+    const columnNameToIdMap: Record<string, string> = {}
+    if (tileTemplateData) {
+      const { name, columns, rowData } = tileTemplateData
+      const table = await user.$relatedQuery('tables').insertGraph({
+        name,
+        role: 'owner',
+        columns,
+      })
+      tableId = table.id
+
+      // obtain a map of column name to the column id for easy replacement
+      for (const tableColumn of table.columns) {
+        columnNameToIdMap[tableColumn.name] = tableColumn.id
+      }
+
+      // replace row data and create table rows
+      const newRowData = replaceRowData(rowData, columnNameToIdMap)
+      await createTableRows({ tableId, dataArray: newRowData })
+    }
+
+    // account for trigger/action step having null for app or event key
     // insert trigger step
     await flow.$relatedQuery('steps', trx).insert({
       type: 'trigger',
@@ -87,7 +170,8 @@ export async function createFlowFromTemplate(
       const step: TemplateStep = steps[i]
       validateAppAndEventKey(step, flowName)
       // replace all parameters with {{user_email}} to the current user email
-      const updatedParameters = structuredClone(step?.parameters ?? {})
+      let updatedParameters = structuredClone(step.parameters ?? {})
+
       for (const [key, value] of Object.entries(updatedParameters)) {
         // ignore objects e.g. conditions because nothing to replace inside for now
         if (typeof value === 'string') {
@@ -99,6 +183,21 @@ export async function createFlowFromTemplate(
         }
       }
 
+      // perform tiles parameter update with tile template step data
+      if (step.appKey === 'tiles') {
+        // update all the parameters and map the column name to id
+        updatedParameters['tableId'] = tableId
+        updatedParameters = {
+          ...generateParametersForTilesAction(
+            step.tileTemplateStepData,
+            columnNameToIdMap,
+            step.eventKey,
+          ),
+          ...updatedParameters,
+        }
+      }
+
+      // insert action step
       await flow.$relatedQuery('steps', trx).insert({
         type: 'action',
         position: step.position,
