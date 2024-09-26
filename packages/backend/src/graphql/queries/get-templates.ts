bae1a6ea8e912fc519265be7adfe2bae2ac477d1
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2024-09-26 14:19:07 +0800

    PLU-318: [TEMPLATES-3]: Link flow with template in empty flows page (#716)
    ## Problem
    Users are unaware of what they can do with Plumber.
    
    ## Solution
    Introduce templates to speed up their pipe creation and provide new
    ideas for them to automate.
    
    ## Details
    - Combined templates and demos into one structure `template` because a
    demo template is just a subset of a template
    - Moved the demo-templates (only 1) data from the frontend to the
    backend
    - `createTemplatedFlow` now can either create a flow using a template or
    demo template by the id, so users cannot anyhow introduce a new template
    because checks are enforced on the backend query
    - `getTemplates` query now also takes in `names` which filters and
    returns template results with those names
    
    ## Tests
    - [x] Empty flows page will show 3 templates and we can create a flow
    with the template id
    - [x] Can still create an empty pipe from the empty flows page

--- packages/backend/src/graphql/queries/get-templates.ts
+++ packages/backend/src/graphql/queries/get-templates.ts
@@ -2,8 +2,13 @@ import { TEMPLATES } from '@/db/storage'
 
 import type { QueryResolvers } from '../__generated__/types.generated'
 
-const getTemplates: QueryResolvers['getTemplates'] = () => {
-  return TEMPLATES
+const getTemplates: QueryResolvers['getTemplates'] = (_parent, params) => {
+  const tag = params?.tag
+  // retrieve all non-demo templates if tag is not present
+  if (!tag) {
+    return TEMPLATES.filter((template) => template.tag !== 'demo')
+  }
+  return TEMPLATES.filter((template) => template.tag === tag)
 }
 
 export default getTemplates
