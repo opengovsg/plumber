5478148f116f6139cbd5ecb0ae63ff13c5c79259
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2024-09-26 14:20:52 +0800

    PLU-318: [TEMPLATES-4]: Add template subpage (#717)
    ## Problem
    Users are unaware of what they can do with Plumber.
    
    ## Solution
    Introduce templates to speed up their pipe creation and provide new
    ideas for them to automate.
    
    ## Details
    - Add `getTemplate` graphQL query to fetch template or demo template
    based on the id, blocks the query if the id is invalid
    - Add template subpage which is a modal
    - Any other template will have a normal step graphic (step 1, step 2...)
      - Check the route support enquiries for ifThen step graphic
    - Add a `sampleUrlDescription` to each template step that is `formsg` or
    `tiles`, this opens up a URL to a form or tile for that template
    
    ## Tests
    - [x] Can access each template by clicking on the tile and it should
    open a modal with the route `/template/:templateId`
    - [x] Invalid template appears if wrong id is input e.g. `/template/123`

--- packages/backend/src/db/storage/attendance-taking.ts
+++ packages/backend/src/db/storage/attendance-taking.ts
@@ -1,5 +1,10 @@
 import type { Template } from '@/graphql/__generated__/types.generated'
 
+import {
+  FORMSG_SAMPLE_URL_DESCRIPTION,
+  TILES_SAMPLE_URL_DESCRIPTION,
+} from './constants'
+
 const ATTENDANCE_TAKING_ID = '04f95a37-46fe-455b-aa96-28c421379e1a'
 
 export const ATTENDANCE_TAKING_TEMPLATE: Template = {
@@ -15,6 +20,7 @@ export const ATTENDANCE_TAKING_TEMPLATE: Template = {
       appKey: 'formsg',
       eventKey: 'newSubmission',
       sampleUrl: 'https://form.gov.sg/66c2c58c0ebf8abcb0ad4c76',
+      sampleUrlDescription: FORMSG_SAMPLE_URL_DESCRIPTION,
     },
     {
       position: 2,
@@ -22,6 +28,7 @@ export const ATTENDANCE_TAKING_TEMPLATE: Template = {
       eventKey: 'findSingleRow',
       sampleUrl:
         'https://plumber.gov.sg/tiles/c77bc8fc-e1ca-4300-a50d-7f2933b9e5b4/a4ca3902-f0ef-41e1-9f5d-45c602c04d50',
+      sampleUrlDescription: TILES_SAMPLE_URL_DESCRIPTION,
     },
     {
       position: 3,
