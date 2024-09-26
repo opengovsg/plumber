d250b5b90e720d00e802400dd8b30a5e98f4c94f
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2024-09-26 14:12:13 +0800

    PLU-318: [TEMPLATES-1]: add templates data and backend query (#714)
    ## Problem
    Users are unaware of what they can do with Plumber.
    
    ## Solution
    Introduce templates to speed up their pipe creation and provide new
    ideas for them to automate.
    
    ## Details
    - Add templates data on the backend in a storage folder (freeze it to
    prevent accidental modification)
    - Add backend query to retrieve the templates
    
    ## Tests
    - [x] Can retrieve the templates using the graphQL query

---
+++ packages/backend/src/db/storage/schedule-reminders.ts
@@ -0,0 +1,30 @@
+import type { Template } from '@/graphql/__generated__/types.generated'
+
+const SCHEDULE_REMINDERS_ID = '65e90f41-b605-4e83-bcd7-e4d2e349299d'
+
+export const SCHEDULE_REMINDERS_TEMPLATE: Template = {
+  id: SCHEDULE_REMINDERS_ID,
+  name: 'Schedule reminders',
+  description:
+    'Schedule a recurring reminder to yourself to complete a task everyday',
+  // Steps: scheduler --> postman
+  steps: [
+    {
+      position: 1,
+      appKey: 'scheduler',
+      eventKey: 'everyWeek',
+      parameters: { hour: '9', weekday: '1' },
+    },
+    {
+      position: 2,
+      appKey: 'postman',
+      eventKey: 'sendTransactionalEmail',
+      parameters: {
+        body: '<p style="margin: 0">This is a scheduled reminder to do you best this week! </p>',
+        subject: "It's a new week!",
+        senderName: 'Weekly motivation',
+        destinationEmail: '{{user_email}}',
+      },
+    },
+  ],
+}
