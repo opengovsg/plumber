380293f9969cd0004eaab6dc13c30c8de14f9ebb
Ian Chen <ian@open.gov.sg>
2024-10-17 15:59:33 +0800

    feat: add clear button to search input (#774)
    ## Changes
    - add clear button in search input
    - refactor executions search input with filter into separate component since it's getting too big
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/7271fe09-fa8f-4676-9f7f-13a858ab97c7.png)
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/0f5460f7-c08f-4c53-b87e-866e4c88f354.png)

--- packages/frontend/src/components/DebouncedSearchInput/index.tsx
+++ packages/frontend/src/components/DebouncedSearchInput/index.tsx
@@ -1,6 +1,11 @@
-import { useCallback, useMemo } from 'react'
-import { BiSearch } from 'react-icons/bi'
-import { Icon, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { useCallback, useMemo, useState } from 'react'
+import { BiSearch, BiSolidXCircle } from 'react-icons/bi'
+import {
+  Icon,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react'
 import { Input } from '@opengovsg/design-system-react'
 import debounce from 'lodash/debounce'
 
@@ -13,6 +18,8 @@ export default function DebouncedSearchInput({
   onChange,
   searchValue,
 }: DebouncedSearchInputProps): React.ReactElement {
+  const [tempSearchValue, setTempSearchValue] = useState(searchValue)
+
   const handleSearchInputChangeDebounced = useMemo(
     () => debounce(onChange, 500),
     [onChange],
@@ -20,24 +27,44 @@ export default function DebouncedSearchInput({
 
   const onSearchInputChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTempSearchValue(event.target.value)
       handleSearchInputChangeDebounced(event.target.value)
     },
     [handleSearchInputChangeDebounced],
   )
 
+  const clearSearch = useCallback(() => {
+    setTempSearchValue('')
+    onChange('')
+    handleSearchInputChangeDebounced.cancel()
+  }, [onChange, handleSearchInputChangeDebounced])
+
   return (
     <InputGroup>
       <InputLeftElement h="100%">
-        <Icon as={BiSearch} style={{ height: 20, width: 20 }} />
+        <Icon as={BiSearch} h={5} w={5} />
       </InputLeftElement>
       <Input
         type="text"
-        w="full"
+        minW="20rem"
+        maxW="100%"
         fontSize="md"
         onChange={onSearchInputChange}
         defaultValue={searchValue}
+        value={tempSearchValue}
         placeholder="Search"
       />
+      {tempSearchValue && (
+        <InputRightElement h="100%">
+          <Icon
+            as={BiSolidXCircle}
+            cursor="pointer"
+            opacity={0.6}
+            _hover={{ opacity: 1 }}
+            onClick={clearSearch}
+          />
+        </InputRightElement>
+      )}
     </InputGroup>
   )
 }
---
+++ packages/frontend/src/pages/Executions/components/SearchWithFilterInput.tsx
@@ -0,0 +1,101 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { BiSearch, BiSolidXCircle } from 'react-icons/bi'
+import {
+  Divider,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react'
+import { debounce } from 'lodash'
+
+import ExecutionStatusMenu from '@/components/ExecutionStatusMenu'
+
+interface SearchWithFilterInputProps {
+  status: string
+  searchValue: string
+  onChange: (val: string) => void
+  onStatusChange: (newStatus: string) => void
+}
+
+export default function SearchWithFilterInput({
+  searchValue,
+  status,
+  onChange,
+  onStatusChange,
+}: SearchWithFilterInputProps) {
+  const filterRef = useRef<HTMLDivElement>(null)
+  const [inputPadding, setInputPadding] = useState<number>(0)
+  const [tempSearchValue, setTempSearchValue] = useState(searchValue)
+
+  const handleSearchInputChangeDebounced = useMemo(
+    () => debounce(onChange, 500),
+    [onChange],
+  )
+
+  const onSearchInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTempSearchValue(event.target.value)
+      handleSearchInputChangeDebounced(event.target.value)
+    },
+    [handleSearchInputChangeDebounced],
+  )
+
+  const clearSearch = useCallback(() => {
+    setTempSearchValue('')
+    onChange('')
+    handleSearchInputChangeDebounced.cancel()
+  }, [onChange, handleSearchInputChangeDebounced])
+
+  // update padding of input element when filter element width changes.
+  useEffect(() => {
+    if (!filterRef.current) {
+      return
+    }
+    setInputPadding(filterRef.current.offsetWidth + 8)
+  }, [filterRef.current?.offsetWidth, status])
+
+  return (
+    <InputGroup>
+      <InputLeftElement>
+        <Icon as={BiSearch} boxSize={5} />
+      </InputLeftElement>
+      <Input
+        textStyle="body-1"
+        minW="25rem"
+        maxW="100%"
+        pr={inputPadding}
+        placeholder="Search by pipe name"
+        defaultValue={searchValue}
+        value={tempSearchValue}
+        onChange={onSearchInputChange}
+      />
+      <InputRightElement w="fit-content" p={1} ref={filterRef}>
+        {tempSearchValue && (
+          <Icon
+            as={BiSolidXCircle}
+            cursor="pointer"
+            opacity={0.6}
+            _hover={{ opacity: 1 }}
+            display="block"
+            w={4}
+            ml={-1}
+            mr={2}
+            onClick={clearSearch}
+          />
+        )}
+        <Divider
+          borderColor="base.divider.medium"
+          h={5}
+          mx={1}
+          orientation="vertical"
+        />
+        <ExecutionStatusMenu
+          filterStatus={status}
+          onFilterChange={onStatusChange}
+        />
+      </InputRightElement>
+    </InputGroup>
+  )
+}
