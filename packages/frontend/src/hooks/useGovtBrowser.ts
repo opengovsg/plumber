6969f1b6195052b2b35c3992d7e62498d055f083
Ian Chen <ian@open.gov.sg>
2024-10-18 17:52:26 +0800

    chore: proxy url if gsib browser

---
+++ packages/frontend/src/hooks/useGovtBrowser.ts
@@ -0,0 +1,24 @@
+/**
+ * Use this hook to check if user is proxied via Menlo Security, if so, prepend the proxy url
+ * When proxied, the window.name property is set to "JSON:{\"safe_rid\":\"4vnqcfBf\",\"mpa_id\":\"\",\"cid\":\"******\",
+ * \"cluster_id\":\"od_menlo_2b\",\"tab_id\":1,\"protocol_domain\":\"xhr-asia-southeast1-menlo-view.menlosecurity.com\",
+ * \"cluster_domain\":\"asia-southeast1-menlo-view.menlosecurity.com\",\"inspect_domain\":\"asia-southeast1-menlo-view.menlosecurity.com\"}"
+ * We check for existence of "menlo-view.menlosecurity.com" in window.name to determine if we should prepend the proxy url
+ */
+
+import { useCallback } from 'react'
+
+const isGovtBrowser = window.name?.includes('menlo-view.menlosecurity.com')
+
+const useProxyUrl = () => {
+  const createProxiedUrl = useCallback((url: string) => {
+    if (isGovtBrowser) {
+      return `https://safe.menlosecurity.com/${url}`
+    }
+    return url
+  }, [])
+
+  return { createProxiedUrl }
+}
+
+export { useProxyUrl }
