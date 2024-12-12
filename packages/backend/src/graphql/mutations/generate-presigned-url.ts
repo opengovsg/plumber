5abf78110c217e122e3a37928859434359001d8f
Kevin <kevinkim@open.gov.sg>
2024-12-12 18:06:49 +0800

    test custom domain for s3 upload

--- packages/backend/src/graphql/mutations/generate-presigned-url.ts
+++ packages/backend/src/graphql/mutations/generate-presigned-url.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'crypto'
 
+import appConfig from '@/config/app'
 import { COMMON_S3_BUCKET, getPresignedUrl } from '@/helpers/s3'
 import Flow from '@/models/flow'
 
@@ -24,7 +25,14 @@ const generatePresignedUrl: MutationResolvers['generatePresignedUrl'] = async (
     manualUpload: manualUpload.toString(),
   })
 
-  return { url, s3Id: `s3:${COMMON_S3_BUCKET}:${filePath}` }
+  const customUrl = url.replace(
+    new RegExp(
+      `https://${COMMON_S3_BUCKET}\\.s3\\.[a-z0-9-]+\\.amazonaws\\.com`,
+    ),
+    `https://upload.${appConfig.appEnv}.plumber.gov.sg`,
+  )
+
+  return { url: customUrl, s3Id: `s3:${COMMON_S3_BUCKET}:${filePath}` }
 }
 
 export default generatePresignedUrl
