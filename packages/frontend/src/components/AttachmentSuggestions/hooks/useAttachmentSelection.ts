e2b381e47d518022ceff488761698a6aa4583193
Kevin <kevinkim@open.gov.sg>
2024-12-16 14:51:46 +0800

    fix: variables not selected on initial upload

--- packages/frontend/src/components/AttachmentSuggestions/hooks/useAttachmentSelection.ts
+++ packages/frontend/src/components/AttachmentSuggestions/hooks/useAttachmentSelection.ts
@@ -23,9 +23,11 @@ export function useAttachmentSelection(setError: UseFormSetError<FieldValues>) {
       // We also add curly braces to check for attachments that are variables
       const { name, uploaded } = variable
       const nameToCheck = uploaded ? name : `{{${name}}}`
+      const currentValues = values ?? []
+
       if (!checked) {
         setSelectedOptions((prev) => prev.filter((p) => p.name !== name))
-        onChange?.(values.filter((v: string) => v !== nameToCheck))
+        onChange?.(currentValues.filter((v: string) => v !== nameToCheck))
       } else {
         const { isValid, error } = validateFiles(variable, selectedOptions)
         if (!isValid) {
@@ -35,7 +37,7 @@ export function useAttachmentSelection(setError: UseFormSetError<FieldValues>) {
           })
         } else {
           setSelectedOptions((prevOptions) => [...prevOptions, variable])
-          onChange?.(values.concat(nameToCheck))
+          onChange?.(currentValues.concat(nameToCheck))
         }
       }
     },
