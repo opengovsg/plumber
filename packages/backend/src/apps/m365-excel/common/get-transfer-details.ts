6d8cf30b1817955bc10301a585ef6b082fe6f2b4
m0nggh <malcolmong27@gmail.com>
2025-02-10 21:58:48 +0800

    add fix for missing file for m365 after pipe transfer

--- packages/backend/src/apps/m365-excel/common/get-transfer-details.ts
+++ packages/backend/src/apps/m365-excel/common/get-transfer-details.ts
@@ -2,6 +2,7 @@ import { IGlobalVariable, ITransferDetails } from '@plumber/types'
 
 import { getM365TenantInfo } from '@/config/app-env-vars/m365'
 import { getEmptyConnectionDetails } from '@/helpers/get-default-transfer-details'
+import logger from '@/helpers/logger'
 
 import { AuthData } from './auth-data'
 
@@ -41,22 +42,28 @@ async function getTransferDetails(
 
   // TODO (mal/WL): query takes around 0.5s, build cache if necessary
   const tenant = getM365TenantInfo(authData.tenantKey)
-  const results = await $.http.get<{ name: string }>(
-    `/v1.0/sites/${tenant.sharePointSiteId}/drive/items/${fileId}?$select=name`,
-  )
-
-  if (!results.data.name) {
+  try {
+    const results = await $.http.get<{ name: string }>(
+      `/v1.0/sites/${tenant.sharePointSiteId}/drive/items/${fileId}?$select=name`,
+    )
     return {
       position: $.step.position,
       appName: $.app.name,
-      instructions: 'No M365 file is found',
+      connectionName: results.data.name,
     }
-  }
+  } catch (error) {
+    logger.warn('M365 excel file cannot be found after pipe transfer', {
+      event: 'm365-excel-pipe-transfer',
+      error,
+      flowId: $.flow.id,
+      stepId: $.step.id,
+    })
 
-  return {
-    position: $.step.position,
-    appName: $.app.name,
-    connectionName: results.data.name,
+    return {
+      position: $.step.position,
+      appName: $.app.name,
+      instructions: 'M365 file cannot be found in your folder',
+    }
   }
 }
 
