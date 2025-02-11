add81e5a2929d54cd137bd941a9c7341bfb8ac66
Ian Chen <ian@open.gov.sg>
2025-02-11 18:31:10 +0800

    fix: prevent infinite loop if position goes out of order (#856)
    alter logic to use index instead of position
    log with prefix BUG: to trigger alert to oncall

--- packages/backend/src/apps/toolbox/common/get-branch-step-id-to-skip-to.ts
+++ packages/backend/src/apps/toolbox/common/get-branch-step-id-to-skip-to.ts
@@ -8,14 +8,32 @@ export async function getBranchStepIdToSkipTo(
   // PERF-FIXME: Objectionjs does no caching, so this will almost always be
   // queried multiple times by the same worker during a test run. If it does
   // turn out to impact perf, we can LRU memoize this by executionId.
+
+  /**
+   * We do this because this could be executed by only continue if, we
+   * need to find the preceding if-then
+   */
+
   const flowSteps = await Step.query()
     .where('flow_id', $.flow.id)
     .orderBy('position', 'asc')
     .throwIfNotFound()
 
+  // This should be position - 1 but we check in case position goes out of order
+  const indexOfCurrentStep = flowSteps.findIndex(
+    (step) => step.id === $.step.id,
+  )
+
+  if (indexOfCurrentStep !== $.step.position - 1) {
+    // this is a sanity check to ensure the step is at the expected position
+    console.error(
+      `Bug: Current step ${$.step.id} is not at the expected position. The positioning of steps for flow ${$.flow.id} may be out of order.`,
+    )
+  }
+
   // search for immediate branch before current step
   const currBranchStep = flowSteps
-    .slice(0, $.step.position) // position is 1-indexed so if-then takes current step by default
+    .slice(0, indexOfCurrentStep + 1)
     .reverse()
     .find((step) => step.appKey === 'toolbox' && step.key === 'ifThen')
 
@@ -33,19 +51,21 @@ export async function getBranchStepIdToSkipTo(
   }
 
   // search for next branch after current step
-  const nextBranchStep = flowSteps.slice($.step.position).find((step) => {
-    if (!(step.appKey === 'toolbox' && step.key === 'ifThen')) {
-      return false
-    }
+  const nextBranchStep = flowSteps
+    .slice(indexOfCurrentStep + 1)
+    .find((step) => {
+      if (!(step.appKey === 'toolbox' && step.key === 'ifThen')) {
+        return false
+      }
 
-    const nextBranchDepth = parseInt(step.parameters?.depth as string)
-    if (isNaN(nextBranchDepth)) {
-      throw new Error(
-        `Branch depth for future branch step ${$.step.id} is not defined.`,
-      )
-    }
+      const nextBranchDepth = parseInt(step.parameters?.depth as string)
+      if (isNaN(nextBranchDepth)) {
+        throw new Error(
+          `Branch depth for future branch step ${$.step.id} is not defined.`,
+        )
+      }
 
-    return nextBranchDepth <= currDepth
-  })
+      return nextBranchDepth <= currDepth
+    })
   return nextBranchStep?.id
 }
