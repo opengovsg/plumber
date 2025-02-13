02a3f651a9fcf3dbd4495b900389c7f92ac42263
kevin <kevinkim@open.gov.sg>
2025-02-13 13:53:52 +0800

    PLU-408: FormSG new local address field (#859)
    ## Feature
    FormSG is releasing a new local address field
    <img width="858" alt="Screenshot 2025-02-11 at 10 16 02 PM"
    src="https://github.com/user-attachments/assets/35bd2954-0c61-48a1-98bf-bcb64b5e930a"
    />
    
    **Features**:
    
    - Handle address field inputs and create the respective variables
    - FormSG local address inputs are received as answer arrays in Plumber
    in the following order:
      1. Block number
      2. Street name
      3. Building name (optional)
      4. Level number (optional)
      5. Unit number (optional)
      6. Postal code
    - Level number and unit number are combined into a single variable 'Unit
    number'
    - FormSG has validation that both Level number and Unit number must be
    filled if either field is filled
    
    ## Before & After Screenshots
    
    **BEFORE**:
    NA.
    
    **AFTER**:
    ![Screenshot 2025-02-11 at 10 23
    21 PM](https://github.com/user-attachments/assets/f06e347c-0aed-4e36-91ae-eefe1a0fc2f5)
    
    Variables are available for use even if data was not provided in test
    submission
    <img width="862" alt="Screenshot 2025-02-12 at 6 52 55 PM"
    src="https://github.com/user-attachments/assets/a0a8878d-5491-4ded-af41-257caea0cba2"
    />
    <img width="835" alt="Screenshot 2025-02-12 at 6 54 35 PM"
    src="https://github.com/user-attachments/assets/48d678a3-6a32-4e2f-b706-53c73acac974"
    />
    
    
    Mock data is generated for all fields
    <img width="855" alt="Screenshot 2025-02-12 at 6 56 27 PM"
    src="https://github.com/user-attachments/assets/44f142b9-b8a9-4e20-99b6-2fb5676d23d8"
    />
    <img width="854" alt="Screenshot 2025-02-12 at 6 56 41 PM"
    src="https://github.com/user-attachments/assets/3c160bd0-b8f2-4a2e-b650-6495ae892294"
    />
    
    
    
    ## Tests
    
    - [x] All other FormSG fields are parsed correctly, just like before
    this new field was introduced
    - [x] Address fields are parsed in the correct order, with variables
    available for use even if field was empty during test submission
    - [x] All FormSG functionalities are working after formsg-sdk upgrade
    (connecting new form, receiving new form submissions on existing forms)
    
    ## Deploy Notes
    - FormSG is targeting to release the local address field on Thu,
    13-Feb-2025. Plumber can release this feature anytime as it is not
    dependent on their release.

--- packages/backend/src/apps/formsg/common/constants.ts
+++ packages/backend/src/apps/formsg/common/constants.ts
@@ -1,1 +1,22 @@
 export const FORM_ID_LENGTH = 24
+
+/**
+ * NOTE: this must be updated if there are any changes to the address fields
+ * Currently following the same field names as FormSG
+ */
+export const ADDRESS_LABELS = [
+  'Block number',
+  'Street name',
+  'Building name',
+  // this combines level number and unit number
+  'Unit number',
+  'Postal code',
+]
+
+// Reference form response for local address
+// ---
+// {
+//   question: 'Local address',
+//   fieldType: 'address',
+//   answerArray: [ '51', 'BRAS BASAH ROAD', 'Lazada One', '8', '8888', '189554' ]
+// }
