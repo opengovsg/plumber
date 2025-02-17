3901904e8b6212c8ef3e0a0c5c5dcdf6715cf1f4
kevin <kevinkim@open.gov.sg>
2025-02-17 16:33:02 +0800

    fix: create queue for Tiles Find Single Row action (#865)
    ## Problem
    * The findSingleRow action in Tiles encounters ‘Throughput Limit
    Exceeded’ errors when there are a high number of executions attempting
    to find rows.
    
    ## Solution
    * Create a queue to rate limit the number of findSingleRow actions
    
    **BEFORE**:
    <img width="1512" alt="Screenshot 2025-02-17 at 3 27 40 PM"
    src="https://github.com/user-attachments/assets/f53ff49c-1cde-44c8-a7a1-faad17ddaf8f"
    />
    
    **AFTER**:
    <img width="1496" alt="Screenshot 2025-02-17 at 3 29 49 PM"
    src="https://github.com/user-attachments/assets/bfef0b2e-2b27-4799-af16-8d2026435994"
    />
    
    ## Tests
    - [x] Test that existing executions with Tile actions execute as
    execpted
    - [x] Verify from admin dashboard that multiple concurrent findSingleRow
    actions on the same Tile are rate limited, and that only
    
    ## Environment Variables
    Note that rate limit defaults to 1 findSingleRow action per second.
    If a different rate limit is desired, create a new environment variable
    in the parameter store named
    `plumber-<ENVIRONMENT>-tiles-interval-between-find-single-row-ms`

---
+++ packages/backend/src/apps/tiles/__tests__/queue.test.ts
@@ -0,0 +1,75 @@
+// Avoid cyclic imports when importing m365ExcelApp
+import '@/apps'
+
+import { afterEach, describe, expect, it, vi } from 'vitest'
+
+import tilesApp from '..'
+
+const mocks = vi.hoisted(() => ({
+  stepQueryResult: vi.fn(),
+}))
+
+vi.mock('@/models/step', () => ({
+  default: {
+    query: vi.fn(() => ({
+      findById: vi.fn(() => ({
+        throwIfNotFound: mocks.stepQueryResult,
+      })),
+    })),
+  },
+}))
+
+describe('Queue config', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('configures a delayable queue', () => {
+    expect(tilesApp.queue.isQueueDelayable).toEqual(true)
+  })
+
+  it('sets group ID to the file ID', async () => {
+    mocks.stepQueryResult.mockResolvedValueOnce({
+      parameters: {
+        tableId: 'mock-table-id',
+      },
+      key: 'findSingleRow',
+      appKey: 'tiles',
+    })
+    const groupConfig = await tilesApp.queue.getGroupConfigForJob({
+      flowId: 'test-flow-id',
+      stepId: 'test-step-id',
+      executionId: 'test-step-id',
+    })
+    expect(groupConfig).toEqual({
+      id: 'mock-table-id-findSingleRow',
+    })
+  })
+
+  it('sets group ID to null', async () => {
+    mocks.stepQueryResult.mockResolvedValueOnce({
+      parameters: {
+        tableId: 'mock-table-id',
+      },
+      key: 'createRow',
+      appKey: 'tiles',
+    })
+    const groupConfig = await tilesApp.queue.getGroupConfigForJob({
+      flowId: 'test-flow-id',
+      stepId: 'test-step-id',
+      executionId: 'test-step-id',
+    })
+    expect(groupConfig).toEqual(null)
+  })
+
+  it('sets group concurrency to 1', () => {
+    expect(tilesApp.queue.groupLimits).toEqual({
+      type: 'concurrency',
+      concurrency: 1,
+    })
+  })
+
+  it('avoids bursting via a leaky bucket approach', () => {
+    expect(tilesApp.queue.queueRateLimit.max).toEqual(1)
+  })
+})
