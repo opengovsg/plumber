4728a96c153b3c8d3c228248e64b04b63d865489
m0nggh <malcolmong27@gmail.com>
2025-02-24 16:47:30 +0800

    add copy changes

--- packages/backend/src/apps/formatter/index.ts
+++ packages/backend/src/apps/formatter/index.ts
@@ -5,14 +5,14 @@ import actions from './actions'
 const app: IApp = {
   name: 'Formatter',
   key: 'formatter',
+  description:
+    'Manipulate your data, such as changing date formats or adding days to a date',
   iconUrl: '{BASE_URL}/apps/formatter/assets/favicon.svg',
   authDocUrl: 'https://guide.plumber.gov.sg/user-guides/actions/formatter',
   baseUrl: '',
   apiBaseUrl: '',
   primaryColor: '000000',
   actions,
-  description:
-    'Manipulate your data, such as changing date formats or adding days to a date',
   substepLabels: {
     settingsStepLabel: 'Set up formatter',
   },
