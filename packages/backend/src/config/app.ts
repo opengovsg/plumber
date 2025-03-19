4c6fa07c09d4cf42a7ece3a7dbc4a4f3bacac352
kevinkim-ogp <kevinkim@open.gov.sg>
2025-03-19 15:59:19 +0800

    chore: remove unused var from app config

--- packages/backend/src/config/app.ts
+++ packages/backend/src/config/app.ts
@@ -47,7 +47,6 @@ type AppConfig = {
   launchDarklySdkKey: string
   maxJobAttempts: number
   onboardingEmailWebhookUrl: string
-  s3UploadUrl: string
 }
 
 const port = process.env.PORT || '3000'
@@ -108,7 +107,6 @@ const appConfig: AppConfig = {
   launchDarklySdkKey: process.env.LAUNCH_DARKLY_SDK_KEY,
   maxJobAttempts: Number(process.env.MAX_JOB_ATTEMPTS ?? '10'),
   onboardingEmailWebhookUrl: process.env.ONBOARDING_EMAIL_WEBHOOK_URL || '',
-  s3UploadUrl: process.env.S3_UPLOAD_URL,
 }
 
 if (!appConfig.encryptionKey) {
