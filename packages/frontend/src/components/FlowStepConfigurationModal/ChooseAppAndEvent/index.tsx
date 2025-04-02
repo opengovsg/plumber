5121eb30642b45931f6b182915c772902db717ac
Ian Chen <ian@open.gov.sg>
2025-04-02 17:52:51 +0800

    refactor: prevent duplicate GET_APPS calls (#920)
    ## Problem
    There are multiple repeated calls to GET_APPS which is quite a large query.
    
    ## Solution
    Move the GET_APPS query into EditorContext, so that it gets called only once.

--- packages/frontend/src/components/FlowStepConfigurationModal/ChooseAppAndEvent/index.tsx
+++ packages/frontend/src/components/FlowStepConfigurationModal/ChooseAppAndEvent/index.tsx
@@ -1,10 +1,8 @@
 import { type IAction, IApp, ITrigger } from '@plumber/types'
 
 import { useCallback, useContext } from 'react'
-import { useQuery } from '@apollo/client'
 
 import { EditorContext } from '@/contexts/Editor'
-import { GET_APPS } from '@/graphql/queries/get-apps'
 import {
   TOOLBOX_ACTIONS,
   TOOLBOX_APP_KEY,
@@ -25,21 +23,14 @@ type ChooseAppAndEventProps = {
 export default function ChooseAppAndEvent(props: ChooseAppAndEventProps) {
   const { onClose } = props
 
-  const { onUpdateStep, onCreateStep } = useContext(EditorContext)
+  const { onUpdateStep, onCreateStep, allApps } = useContext(EditorContext)
 
-  const {
-    modalState,
-    patchModalState,
-    prevStepId,
-    isLastStep,
-    isTrigger,
-    step,
-  } = useContext(FlowStepConfigurationContext)
+  const { modalState, patchModalState, prevStepId, isTrigger, step } =
+    useContext(FlowStepConfigurationContext)
 
   const { currentScreen, selectedApp } = modalState
 
-  const { data } = useQuery(GET_APPS)
-  const apps: IApp[] = data?.getApps?.filter((app: IApp) =>
+  const apps: IApp[] = allApps.filter((app: IApp) =>
     isTrigger ? !!app.triggers?.length : !!app.actions?.length,
   )
 
@@ -74,10 +65,8 @@ export default function ChooseAppAndEvent(props: ChooseAppAndEventProps) {
         }
       }
       // For a better visual experience, delay the closing of the modal
-      setTimeout(() => {
-        patchModalState({ isLoading: false })
-        onClose()
-      }, 500)
+      patchModalState({ isLoading: false })
+      onClose()
     },
     [
       patchModalState,
@@ -91,36 +80,9 @@ export default function ChooseAppAndEvent(props: ChooseAppAndEventProps) {
   )
 
   if (currentScreen === 'choose-app') {
-    return (
-      <ChooseApp
-        apps={apps}
-        isTrigger={isTrigger}
-        onSelectApp={(app: IApp) => {
-          patchModalState({
-            selectedApp: app,
-            currentScreen: 'choose-event',
-          })
-        }}
-        onSelectAppEvent={onSelectAppEvent}
-      />
-    )
+    return <ChooseApp apps={apps} onSelectAppEvent={onSelectAppEvent} />
   } else if (selectedApp && currentScreen === 'choose-event') {
-    return (
-      <ChooseEvent
-        selectedApp={selectedApp}
-        isTrigger={isTrigger}
-        isLastStep={isLastStep}
-        onSelectAppEvent={onSelectAppEvent}
-        onBack={() => {
-          patchModalState({
-            selectedApp: null,
-            selectedEvent: null,
-            selectedConnectionId: '',
-            currentScreen: 'choose-app',
-          })
-        }}
-      />
-    )
+    return <ChooseEvent onSelectAppEvent={onSelectAppEvent} />
   } else {
     return <InvalidModalScreen />
   }
