e4ded894d7c8b228a3316297bebf7c196bb70f83
Ong Guan Hong Malcolm <65110268+m0nggh@users.noreply.github.com>
2025-04-03 13:43:24 +0800

    PLU-375: [UI-REVAMP-APP-SELECT-7] Allow deletion of trigger (#913)
    ### TL;DR
    
    Allow trigger steps to be reset instead of deleted, and enable the delete button for trigger steps.
    
    ### What changed?
    
    - Modified the `deleteStep` mutation to handle trigger steps differently:
      - When deleting a trigger step, it now resets the step properties (key, appKey, connectionId, parameters) and sets status to 'incomplete' instead of actually deleting it
      - For non-trigger steps, the existing deletion logic is maintained
    - Updated the frontend to:
      - Enable the delete button for trigger steps by removing the `!isTrigger` condition
      - Reset the current substep to 0 after deleting a step
    
    ### Screenshots
    
    https://github.com/user-attachments/assets/6dc94f2d-0466-4014-a8e1-278063f9a014
    
    ### Changes (ian)
    - Show empty action if last action is delete
    
    #### Screenshots
    If all actions are deleted OR only one empty action left
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/f655ba13-ddea-49e5-a58f-f36c483fde2d.png)
    
    If IF-THEN exists
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/8121c4f3-fb8f-40ce-a9d9-8bf3d49b317b.png)
    
    If at least one non-empty action exists
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/a428b5c5-7c30-4758-8239-05fe6ff2086c.png)
    
    Inside an empty IF-THEN branch
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/ycT2Xb5nBEjN2kf2gy95/4b9d1b9b-f4d1-4d57-8bae-cd2a793ac908.png)
    
    ### How to test?
    
    1. Create a flow with a trigger step
    2. Click the delete button on the trigger step
    3. Verify that the trigger step is reset (not deleted) and shows as incomplete
    4. Create a flow with multiple steps
    5. Delete a non-trigger step
    6. Verify that the step is completely removed and subsequent steps' positions are updated
    
    ### Why make this change?
    
    Flows require a trigger step to function properly. This change allows users to reset a trigger step when they want to change it, rather than preventing deletion entirely. This improves the user experience by providing a way to start over with trigger configuration without having to recreate the entire flow.

---
+++ packages/frontend/src/components/Editor/AddStepButton.tsx
@@ -0,0 +1,100 @@
+import { BiPlus } from 'react-icons/bi'
+import { Box, Divider, useDisclosure } from '@chakra-ui/react'
+import { IconButton, TouchableTooltip } from '@opengovsg/design-system-react'
+
+import EmptyFlowStepHeader from '../EmptyFlowStepHeader'
+import FlowStepConfigurationModal from '../FlowStepConfigurationModal'
+
+interface AddStepButtonProps {
+  isHidden: boolean
+  isDisabled: boolean
+  isLastStep: boolean
+  showEmptyAction: boolean
+  stepId: string
+}
+
+export function AddStepButton(props: AddStepButtonProps): JSX.Element {
+  const { isHidden, isLastStep, stepId, isDisabled, showEmptyAction } = props
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  // If in between add button is disabled, we hide it
+  if (isHidden || (isDisabled && !isLastStep)) {
+    return (
+      <Box pos="relative" h={24} py={2}>
+        {/* dont show line if last step, leave box for padding */}
+        {!isLastStep && (
+          <Divider orientation="vertical" borderColor="base.divider.strong" />
+        )}
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      <Box
+        pos="relative"
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        alignSelf="stretch"
+      >
+        {/* Show empty action instead of add button */}
+        {showEmptyAction && (
+          <>
+            {/* Top vertical line */}
+            <Divider
+              orientation="vertical"
+              borderColor="base.divider.strong"
+              h={20}
+              my={2}
+            />
+            <EmptyFlowStepHeader isTrigger={false} onModalOpen={onOpen} />
+          </>
+        )}
+        {/* Top vertical line */}
+        <Box h={5} mt={2}>
+          <Divider orientation="vertical" borderColor="base.divider.strong" />
+        </Box>
+        <TouchableTooltip
+          label={isDisabled ? 'Add your first action first' : 'Add step'}
+          placement="right"
+          display={isDisabled && !isLastStep ? 'none' : 'flex'}
+          marginX="auto"
+        >
+          <IconButton
+            onClick={onOpen}
+            aria-label="Add Step"
+            isDisabled={isDisabled || showEmptyAction}
+            icon={<BiPlus />}
+            variant={isLastStep ? 'outline' : 'clear'}
+            size="xs"
+            color="interaction.sub.default"
+            borderRadius="full"
+            _hover={{
+              bg: 'interaction.muted.neutral.hover',
+            }}
+            _active={{
+              bg: 'interaction.muted.neutral.active',
+            }}
+            borderColor={isLastStep ? 'interaction.sub.default' : undefined}
+          />
+        </TouchableTooltip>
+        {/* Bottom vertical line */}
+        {!isLastStep && (
+          <Box h={5} mb={2}>
+            <Divider orientation="vertical" borderColor="base.divider.strong" />
+          </Box>
+        )}
+      </Box>
+
+      {isOpen && (
+        <FlowStepConfigurationModal
+          onClose={onClose}
+          isTrigger={false} // Can only add an action all the time
+          isLastStep={isLastStep}
+          prevStepId={stepId}
+        />
+      )}
+    </>
+  )
+}
