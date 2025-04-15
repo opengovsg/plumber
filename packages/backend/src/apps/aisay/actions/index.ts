35efb7902fca6b6fc5c260887f8bf6457fd6ba84
kevinkim-ogp <kevinkim@open.gov.sg>
2025-04-15 10:25:51 +0800

    chore: hide specific model from actions

--- packages/backend/src/apps/aisay/actions/index.ts
+++ packages/backend/src/apps/aisay/actions/index.ts
@@ -1,4 +1,6 @@
 import useGeneralisedModel from './use-generalised-model'
-import useSpecificModel from './use-specific-model'
+// TODO (kevinkim-ogp): specific model will be rolled out in the future
+// once we work out how to handle array fields with unknown number of items
+// import useSpecificModel from './use-specific-model'
 
-export default [useSpecificModel, useGeneralisedModel]
+export default [useGeneralisedModel]
