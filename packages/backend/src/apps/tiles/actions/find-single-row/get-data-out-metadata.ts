3aa97d30459b074ef850a2e70f55a572fd7d0edf
Ian Chen <ian@open.gov.sg>
2025-04-23 16:40:27 +0800

    feat (tiles-update-row): only show tile row id in variables

--- packages/backend/src/apps/tiles/actions/find-single-row/get-data-out-metadata.ts
+++ packages/backend/src/apps/tiles/actions/find-single-row/get-data-out-metadata.ts
@@ -18,6 +18,7 @@ async function getDataOutMetadata(
   const metadata: IDataOutMetadata = {
     rowId: {
       label: 'Row ID',
+      type: 'tile_row_id',
       order: 2,
     },
     rowsFound: {
