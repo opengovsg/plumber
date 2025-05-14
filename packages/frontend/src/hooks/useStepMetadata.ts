7a91e00b41684376b3d6def41dc7ecc9962a08dd
kevinkim-ogp <kevinkim@open.gov.sg>
2025-05-14 09:22:07 +0800

    chore: use helper to check if isIfThenStep

--- packages/frontend/src/hooks/useStepMetadata.ts
+++ packages/frontend/src/hooks/useStepMetadata.ts
@@ -2,7 +2,7 @@ import { IAction, IApp, IStep, ISubstep, ITrigger } from '@plumber/types'
 
 import { useMemo } from 'react'
 
-import { TOOLBOX_ACTIONS, TOOLBOX_APP_KEY } from '@/helpers/toolbox'
+import { isIfThenStep as checkIfThenStep } from '@/helpers/toolbox'
 
 interface UseStepMetadataResult {
   app: IApp | undefined
@@ -23,7 +23,7 @@ export function useStepMetadata(
 ): UseStepMetadataResult {
   const isCompleted = step?.status === 'completed'
   const isTrigger = step?.type === 'trigger'
-  const isIfThenStep = step?.appKey === 'toolbox' && step?.key === 'ifThen'
+  const isIfThenStep = step ? checkIfThenStep(step) : false
 
   const apps: IApp[] = allApps?.filter((app: IApp) =>
     isTrigger ? !!app.triggers?.length : !!app.actions?.length,
@@ -53,11 +53,8 @@ export function useStepMetadata(
       selectedActionOrTrigger?.name
     }`
 
-    if (
-      step?.key === TOOLBOX_ACTIONS.IfThen &&
-      step?.appKey === TOOLBOX_APP_KEY
-    ) {
-      caption = `${step.position}. Condition`
+    if (isIfThenStep) {
+      caption = `${step?.position}. Condition`
     }
   } else if (app?.name) {
     caption = `${step?.position ? `${step.position}. ` : ''}${app.name}`
