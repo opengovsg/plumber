a9dbafbf4c2d95d1f74f56376bf67d155761e2e7
kevinkim-ogp <kevinkim@open.gov.sg>
2025-05-19 13:20:04 +0800

    chore: update names based on pr review for clarity

--- packages/backend/src/helpers/check-step-parameters.ts
+++ packages/backend/src/helpers/check-step-parameters.ts
@@ -6,39 +6,42 @@ const GLOBAL_VARIABLE_REGEX = new RegExp(VARIABLE_REGEX, 'g')
 
 /**
  * NOTE: this function is used to check if a step references another step.
- * @param obj parameters of a step
- * @param stepMap map of step id(s) that are being deleted
+ * @param stepParams parameters of a step
+ * @param stepIdSet set of step id(s) that are being deleted
  * @returns this returns whether parameters are referencing a step that is being deleted
  */
-export function hasStepReference(obj: IJSONObject, stepMap: Set<string>) {
+export function hasStepReference(
+  stepParams: IJSONObject,
+  stepIdSet: Set<string>,
+) {
   const missing = new Set()
 
-  function traverse(value: any) {
-    if (!value) {
+  function traverse(obj: any) {
+    if (!obj) {
       return
     }
 
-    if (typeof value === 'string') {
+    if (typeof obj === 'string') {
       const regex = new RegExp(GLOBAL_VARIABLE_REGEX)
       let match
 
-      while ((match = regex.exec(value)) !== null) {
+      while ((match = regex.exec(obj)) !== null) {
         try {
           const stepId = match[1].split('.')[1]
-          if (stepMap.has(stepId)) {
+          if (stepIdSet.has(stepId)) {
             missing.add(stepId)
           }
         } catch (error) {
           continue
         }
       }
-    } else if (Array.isArray(value)) {
-      value.forEach(traverse)
-    } else if (value && typeof value === 'object') {
-      Object.values(value).forEach(traverse)
+    } else if (Array.isArray(obj)) {
+      obj.forEach(traverse)
+    } else if (obj && typeof obj === 'object') {
+      Object.values(obj).forEach(traverse)
     }
   }
 
-  traverse(obj)
+  traverse(stepParams)
   return missing.size > 0
 }
