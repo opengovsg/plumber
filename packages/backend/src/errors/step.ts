ac01ab84da7eade8be55b614fc8207c375ade31d
Ian Chen <ian@open.gov.sg>
2025-05-28 19:08:28 +0800

    fix: postman sms retriable error not properly thrown (#1017)
    ## Problem
    Postman SMS retriable error was re-wrapped with StepError resulting in
    no retries
    
    ## Solution
    Dont re-catch and wrap this error

--- packages/backend/src/errors/step.ts
+++ packages/backend/src/errors/step.ts
@@ -7,6 +7,7 @@ import HttpError from './http'
 //
 export enum GenericSolution {
   ReconfigureInvalidField = 'Click on set up action and reconfigure the invalid field. Error could also result from the variables used in the field.',
+  MalformedConnectionData = 'Please reconnect and try again.',
 }
 
 export default class StepError extends Error {
