6ee01a1454c170dcc97e46dca33820fe774473a9
kevinkim-ogp <kevinkim@open.gov.sg>
2025-06-02 09:42:59 +0800

    chore: get excel column values dynamically

--- packages/backend/src/apps/m365-excel/actions/get-table-rows/get-data-out-metadata.ts
+++ packages/backend/src/apps/m365-excel/actions/get-table-rows/get-data-out-metadata.ts
@@ -17,34 +17,111 @@ async function getDataOutMetadata(
       label: 'Number of rows found',
       order: 2,
     },
+    data: {
+      label: 'List of row(s) found',
+      displayedValue: `Preview ${dataOut.rowsFound} row(s)`,
+      type: 'multiple-row-object',
+      order: 1,
+    },
   }
 
-  metadata.rows = {
-    label: 'List of row(s) found',
-    displayedValue: `Preview ${dataOut.rowsFound} row(s)`,
-    type: 'array',
-    order: 1,
-  }
-
-  const columnMetadata: IDataOutMetadata = []
-  if ('columns' in dataOut) {
-    dataOut.columns.forEach(({ name }) => {
-      columnMetadata.push({
-        id: { isHidden: true },
-        name: { isHidden: true },
-        value: { label: name },
-      })
-    })
-  }
-
-  return { ...metadata, columns: columnMetadata }
+  return { ...metadata }
 }
 
 export default getDataOutMetadata
 
-// Example dataOut: {
-//   // rows is an array of objects, where each object is a row of data
-//   rows: [{"id": "0-2","tableRowIndex":0,"sheetRowNumber":2,"row":{"436f6c756D6E31":{"value":"abc","columnName":"Column1"},"436F6C756D6E32":{"value":"123","columnName":"Column2"}}},{"id": "3-5","tableRowIndex":3,"sheetRowNumber":5,"row":{"436F6C756D6E31":{"value":"abc","columnName":"Column1"},"436F6C756D6E32":{"value":"111","columnName":"Column2"}}}],
-//   columns: {"Column1": {"id": "Column1", "order":1, value: "Column1"}, "Column2": {"id": "Column2", "order":2, value: "Column2"}},
-//   rowsFound: 2
+// Example dataOut:
+// {
+//   "data": {
+//     "rows": [
+//       {
+//         "data": {
+//           "4e524943": "S7032796N",
+//           "41646472657373": "Block 161 Victoria Street #41-21",
+//           "525356502d6564": "Yes",
+//           "4164647265737332": "",
+//           "4c617374204e616d65": "Lee",
+//           "4669727374204e616d65": "Jane",
+//           "506f7374616c20436f6465": "753105",
+//           "4e657720436f6c756d6e2033": "2025-04-07T10:19:39.920Z",
+//           "4e657720436f6c756d6e2034": "400",
+//           "456d61696c2041646472657373": "meiling.lee179@gmail.com",
+//           "456d61696c204164647265737332": ""
+//         }
+//       },
+//       {
+//         "data": {
+//           "4e524943": "S2645644U",
+//           "41646472657373": "Block 535 Bedok North Road #38-325",
+//           "525356502d6564": "Yes",
+//           "4164647265737332": "",
+//           "4c617374204e616d65": "Abdullah",
+//           "4669727374204e616d65": "Ivan",
+//           "506f7374616c20436f6465": "557687",
+//           "4e657720436f6c756d6e2033": "",
+//           "4e657720436f6c756d6e2034": "",
+//           "456d61696c2041646472657373": "",
+//           "456d61696c204164647265737332": ""
+//         }
+//       }
+//     ],
+//     "columns": [
+//       {
+//         "id": "525356502d6564",
+//         "name": "RSVP-ed",
+//         "value": "data.rows.*.data.525356502d6564"
+//       },
+//       {
+//         "id": "4669727374204e616d65",
+//         "name": "First Name",
+//         "value": "data.rows.*.data.4669727374204e616d65"
+//       },
+//       {
+//         "id": "4c617374204e616d65",
+//         "name": "Last Name",
+//         "value": "data.rows.*.data.4c617374204e616d65"
+//       },
+//       {
+//         "id": "4e524943",
+//         "name": "NRIC",
+//         "value": "data.rows.*.data.4e524943"
+//       },
+//       {
+//         "id": "41646472657373",
+//         "name": "Address",
+//         "value": "data.rows.*.data.41646472657373"
+//       },
+//       {
+//         "id": "506f7374616c20436f6465",
+//         "name": "Postal Code",
+//         "value": "data.rows.*.data.506f7374616c20436f6465"
+//       },
+//       {
+//         "id": "456d61696c2041646472657373",
+//         "name": "Email Address",
+//         "value": "data.rows.*.data.456d61696c2041646472657373"
+//       },
+//       {
+//         "id": "456d61696c204164647265737332",
+//         "name": "Email Address2",
+//         "value": "data.rows.*.data.456d61696c204164647265737332"
+//       },
+//       {
+//         "id": "4164647265737332",
+//         "name": "Address2",
+//         "value": "data.rows.*.data.4164647265737332"
+//       },
+//       {
+//         "id": "4e657720436f6c756d6e2033",
+//         "name": "New Column 3",
+//         "value": "data.rows.*.data.4e657720436f6c756d6e2033"
+//       },
+//       {
+//         "id": "4e657720436f6c756d6e2034",
+//         "name": "New Column 4",
+//         "value": "data.rows.*.data.4e657720436f6c756d6e2034"
+//       }
+//     ]
+//   },
+//   "rowsFound": 6
 // }
