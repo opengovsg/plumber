c3bfaed56282581c1d8ff8794f743ae4612bda26
kevin <kevinkim@open.gov.sg>
2025-06-02 09:16:24 +0800

    PLU-429 [FIND-MULTIPLE-ROWS-2]: M365 Excel  (#900)
    ### TL;DR
    * Added a new "Get Multiple Rows" action to the M365 Excel app that allows users to retrieve multiple rows from a excel table based on the specified lookup column and value.
    * Cell values for each column will be combined into a single variable under that column
    
    This is a prerequisite for for-each.
    
    #### Note to reviewers
    * This is meant to be the backend implementation of getting rows, the data-out metadata will be updated again.
    * Feature flag has been added on LaunchDarkly to allow for progressive rollout.
    * There will be a PR created below this to display the find multiple row results in a table
    
    ### What changed?
    * Created a new getTableRows action that retrieves multiple rows from a M365 Excel table based on a specified lookup column and value
    *  Only the first 500 rows that meet the condition will be returned
    
    ### How to test?
    1. Create a new step with M365 Excel app and select the 'Get table rows' event.
    2. Select an excel file, table, and set the lookup column and value
    3. Test the step and verify that the correct rows are returned
    
    ### Screenshots
    
    ![Screenshot 2025-04-15 at 2.55.18 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Wrwhm8Mmhv1Z2GwlSb7V/568e961d-cb5d-4c20-9a9d-314af6320f56.png)
    
    ![Screenshot 2025-04-15 at 2.55.27 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Wrwhm8Mmhv1Z2GwlSb7V/4e89363b-bd52-4c9d-ab85-477c8bb2989c.png)

--- packages/backend/src/apps/m365-excel/actions/index.ts
+++ packages/backend/src/apps/m365-excel/actions/index.ts
@@ -1,12 +1,14 @@
 import createTableRow from './create-table-row'
 import getCellValues from './get-cell-values'
 import getTableRow from './get-table-row'
+import getTableRows from './get-table-rows'
 import updateTableRow from './update-table-row'
 import writeCellValues from './write-cell-values'
 
 export default [
   createTableRow,
   getTableRow,
+  getTableRows,
   getCellValues,
   updateTableRow,
   writeCellValues,
