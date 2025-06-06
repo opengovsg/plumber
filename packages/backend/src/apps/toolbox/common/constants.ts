a6cfbd6dbe68f495c181cad4b2d9fa71df0f522f
kevinkim-ogp <kevinkim@open.gov.sg>
2025-06-06 08:40:43 +0800

    chore: update for-each iteration key

--- packages/backend/src/apps/toolbox/common/constants.ts
+++ packages/backend/src/apps/toolbox/common/constants.ts
@@ -1,4 +1,4 @@
-export const FOR_EACH_ITERATION_KEY = '$$forEachIteration$$'
+export const FOR_EACH_ITERATION_KEY = '__ITERATION__'
 export enum FOR_EACH_INPUT_SOURCE {
   CHECKBOX = 'checkbox',
   M365_EXCEL = 'm365-excel',
