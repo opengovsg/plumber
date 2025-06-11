d2826b6b493421a701db620e237854f91c6c4653
kevin <kevinkim@open.gov.sg>
2025-06-11 10:55:59 +0800

    chore: add dropdown to select model type (#1043)
    ## TL;DR
    Adds dropdown to select the type of generalised model to use for AISAY
    action.
    Context: AISAY added a new model type called 'Vision' to their
    generalised model.
    
    AISAY's API doc:
    https://app.swaggerhub.com/apis-docs/DAMIENSKTWORK/bgp-aisay_document_extraction_api/1.0.11#/DocumentParserResponse
    
    ## Solution
    **Improvements**:
    - Add dropdown to select generalised model: `Standard` (default) or
    `Vision` (new)
    
    ## How to test?
    - [ ] Existing AISAY actions work as intended without making any changes
      - [ ] Show the model type as `Standard` by default 
    - [ ] Existing AISAY action can be updated to use new `Vision` model
    - [ ] Create new AISAY action with `Vision` model and runs as intended
    - [ ] Create new AISAY action with `Standard` model and runs as intended
    
    ## What changed?
    * Additional dropdown field called `Model type` with the following
    options: `Standard` (default), `Vision` (new)
    * Add tests to validate the model type selection
    * Add model type to api call as `additional_features`
    
    ## Before & After Screenshots
    
    **BEFORE**:
    <img width="975" alt="Screenshot 2025-06-06 at 2 49 19 PM"
    src="https://github.com/user-attachments/assets/9063c9ef-7770-4e96-af14-7fa6e0f750a8"
    />
    
    
    **AFTER**:
    <img width="968" alt="Screenshot 2025-06-06 at 2 52 50 PM"
    src="https://github.com/user-attachments/assets/24103909-31da-4b01-be8f-7f0c49cc4d2e"
    />
    
    
    https://github.com/user-attachments/assets/b0b0093f-75d6-4457-9552-02ffe80fa348

--- packages/backend/src/apps/aisay/common/schema.ts
+++ packages/backend/src/apps/aisay/common/schema.ts
@@ -2,7 +2,11 @@ import { z } from 'zod'
 
 import { parseS3Id } from '@/helpers/s3'
 
-import { DOCUMENT_TYPES } from './constants'
+import {
+  DEFAULT_GENERALISED_MODEL_TYPE,
+  DOCUMENT_TYPES,
+  GENERALISED_MODEL_OPTIONS,
+} from './constants'
 
 export const fileSchema = z.string().transform((value, context) => {
   if (!value) {
@@ -36,6 +40,29 @@ export const generalisedModelSchema = z.object({
     })
     return result
   }),
+  /**
+   * NOTE: the custom model type is indicated by the 'additonal_features' field in the request body
+   * 1. this function returns null when using the standard model as it is not required by default.
+   * 2. it is only included when using the vision generalised model.
+   * API doc: https://app.swaggerhub.com/apis-docs/DAMIENSKTWORK/bgp-aisay_document_extraction_api/1.0.11#/DocumentParserRequestS3GenericV2
+   */
+  modelType: z
+    .string()
+    .optional()
+    .transform((value) => {
+      if (!value || value === DEFAULT_GENERALISED_MODEL_TYPE) {
+        return null
+      }
+
+      const allowedValues = GENERALISED_MODEL_OPTIONS.map(
+        (option) => option.value,
+      )
+
+      if (!allowedValues.includes(value)) {
+        return null
+      }
+      return { [value]: {} }
+    }),
 })
 
 export const documentTypeEnum = z.enum(DOCUMENT_TYPES as [string, ...string[]])
