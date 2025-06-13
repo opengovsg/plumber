971adb6908bf5d37f4fbabef316512959af91821
kevinkim-ogp <kevinkim@open.gov.sg>
2025-06-13 11:27:24 +0800

    test refactored query

--- packages/backend/src/models/execution-step.ts
+++ packages/backend/src/models/execution-step.ts
@@ -85,38 +85,27 @@ class ExecutionStep extends Base {
   }
 
   static async getForEachExecutionSteps(executionId: string) {
+    // TESTING
     return ExecutionStep.query()
+      .select('execution_steps.*', 'latest_steps.min_created_at')
       .with('latest_steps', (builder) => {
-        /**
-         * NOTE: there is a known issue with knex where 'groupBy' are placed at the end of the 'unionAll' query.
-         * the workaround is to unionAll both queries with 'true' to wrap the subequery.
-         */
         builder
-          .unionAll((qb) => {
-            qb.select(
-              'step_id',
-              raw('max(created_at) as max_created_at'),
-              raw('min(created_at) as min_created_at'),
-            )
-              .from('execution_steps')
-              .groupBy('step_id')
-              .where('execution_id', '=', executionId)
-              .where(raw("metadata = '{}'::jsonb"))
-              .withSoftDeleted()
-          }, true)
-          .unionAll((qb) => {
-            qb.select(
-              'step_id',
-              raw('max(created_at) as max_created_at'),
-              raw('min(created_at) as min_created_at'),
-            )
-              .from('execution_steps')
-              .groupBy('step_id', raw("metadata->>'iteration'"))
-              .where('execution_id', '=', executionId)
-              .where(raw("metadata != '{}'::jsonb"))
-              .withSoftDeleted()
-          }, true)
-          .withSoftDeleted()
+          .select(
+            'step_id',
+            raw('MAX(created_at) as max_created_at'),
+            raw('MIN(created_at) as min_created_at'),
+          )
+          .from('execution_steps')
+          .where('execution_id', executionId)
+          .groupBy('step_id')
+          .groupBy(
+            raw(`
+              CASE
+                WHEN metadata = '{}'::jsonb THEN NULL
+                ELSE metadata ->> 'iteration'
+              END
+            `),
+          )
       })
       .join('latest_steps', (builder) => {
         builder
@@ -127,9 +116,53 @@ class ExecutionStep extends Base {
             'latest_steps.max_created_at',
           )
       })
-      .select('execution_steps.*', 'min_created_at')
-      .withSoftDeleted()
-      .orderBy('min_created_at', 'asc')
+      .where('execution_steps.execution_id', executionId)
+      .orderBy('latest_steps.min_created_at', 'asc')
+    // return ExecutionStep.query()
+    //   .with('latest_steps', (builder) => {
+    //     /**
+    //      * NOTE: there is a known issue with knex where 'groupBy' are placed at the end of the 'unionAll' query.
+    //      * the workaround is to unionAll both queries with 'true' to wrap the subequery.
+    //      */
+    //     builder
+    //       .unionAll((qb) => {
+    //         qb.select(
+    //           'step_id',
+    //           raw('max(created_at) as max_created_at'),
+    //           raw('min(created_at) as min_created_at'),
+    //         )
+    //           .from('execution_steps')
+    //           .groupBy('step_id')
+    //           .where('execution_id', '=', executionId)
+    //           .where(raw("metadata = '{}'::jsonb"))
+    //           .withSoftDeleted()
+    //       }, true)
+    //       .unionAll((qb) => {
+    //         qb.select(
+    //           'step_id',
+    //           raw('max(created_at) as max_created_at'),
+    //           raw('min(created_at) as min_created_at'),
+    //         )
+    //           .from('execution_steps')
+    //           .groupBy('step_id', raw("metadata->>'iteration'"))
+    //           .where('execution_id', '=', executionId)
+    //           .where(raw("metadata != '{}'::jsonb"))
+    //           .withSoftDeleted()
+    //       }, true)
+    //       .withSoftDeleted()
+    //   })
+    //   .join('latest_steps', (builder) => {
+    //     builder
+    //       .on('execution_steps.step_id', '=', 'latest_steps.step_id')
+    //       .andOn(
+    //         'execution_steps.created_at',
+    //         '=',
+    //         'latest_steps.max_created_at',
+    //       )
+    //   })
+    //   .select('execution_steps.*', 'min_created_at')
+    //   .withSoftDeleted()
+    //   .orderBy('min_created_at', 'asc')
   }
 
   static async getForEachExecutionState(executionId: string) {
