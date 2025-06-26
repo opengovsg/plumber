38767b930dc165b0b948cb1c8f51a6507672fb3c
kevinkim-ogp <kevinkim@open.gov.sg>
2025-06-26 17:27:41 +0800

    feat: add SortableList components

---
+++ packages/frontend/src/components/SortableList/index.tsx
@@ -0,0 +1,94 @@
+import './SortableList.css'
+
+import type { ReactNode } from 'react'
+import React, { useMemo, useState } from 'react'
+import type { Active, UniqueIdentifier } from '@dnd-kit/core'
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core'
+import {
+  restrictToParentElement,
+  restrictToVerticalAxis,
+  restrictToWindowEdges,
+} from '@dnd-kit/modifiers'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+} from '@dnd-kit/sortable'
+
+import { DragHandle, SortableItem, SortableOverlay } from './components'
+
+interface BaseItem {
+  id: UniqueIdentifier
+}
+
+interface Props<T extends BaseItem> {
+  items: T[]
+  onChange(items: T[]): void
+  renderItem(item: T, isDragging: boolean): ReactNode
+}
+
+export function SortableList<T extends BaseItem>({
+  items,
+  onChange,
+  renderItem,
+}: Props<T>) {
+  const [active, setActive] = useState<Active | null>(null)
+  const activeItem = useMemo(
+    () => items.find((item) => item.id === active?.id),
+    [active, items],
+  )
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  )
+
+  return (
+    <DndContext
+      sensors={sensors}
+      onDragStart={({ active }) => {
+        setActive(active)
+      }}
+      onDragEnd={({ active, over }) => {
+        if (over && active.id !== over?.id) {
+          const activeIndex = items.findIndex(({ id }) => id === active.id)
+          const overIndex = items.findIndex(({ id }) => id === over.id)
+
+          onChange(arrayMove(items, activeIndex, overIndex))
+        }
+        setActive(null)
+      }}
+      onDragCancel={() => {
+        setActive(null)
+      }}
+      modifiers={[
+        restrictToVerticalAxis,
+        restrictToParentElement,
+        restrictToWindowEdges,
+      ]}
+    >
+      <SortableContext items={items}>
+        <ul className="SortableList" role="application">
+          {items.map((item) => (
+            <React.Fragment key={item.id}>
+              {renderItem(item, active?.id === item.id)}
+            </React.Fragment>
+          ))}
+        </ul>
+      </SortableContext>
+      <SortableOverlay>
+        {activeItem ? renderItem(activeItem, true) : null}
+      </SortableOverlay>
+    </DndContext>
+  )
+}
+
+SortableList.Item = SortableItem
+SortableList.DragHandle = DragHandle
