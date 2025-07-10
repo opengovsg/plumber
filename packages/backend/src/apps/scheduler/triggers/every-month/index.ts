8d6ee329815e3a11539e59592fb5e0666474010a
kevinkim-ogp <kevinkim@open.gov.sg>
2025-07-10 14:02:36 +0800

    chore: extract time of day options to constant

--- packages/backend/src/apps/scheduler/triggers/every-month/index.ts
+++ packages/backend/src/apps/scheduler/triggers/every-month/index.ts
@@ -2,6 +2,10 @@ import { IGlobalVariable, IRawTrigger } from '@plumber/types'
 
 import { DateTime } from 'luxon'
 
+import {
+  TIME_OF_DAY_DESCRIPTION,
+  TIME_OF_DAY_OPTIONS,
+} from '../../common/constants'
 import cronTimes from '../../common/cron-times'
 import getDateTimeObjectRepresentation from '../../common/get-date-time-object'
 import getNextCronDateTime from '../../common/get-next-cron-date-time'
@@ -153,109 +157,12 @@ const trigger: IRawTrigger = {
       label: 'Time of day',
       key: 'hour',
       type: 'dropdown' as const,
-      description: 'What time of day should this flow trigger at?',
+      description: TIME_OF_DAY_DESCRIPTION,
       required: true,
       value: null,
       variables: false,
       showOptionValue: false,
-      options: [
-        {
-          label: '00:00',
-          value: 0,
-        },
-        {
-          label: '01:00',
-          value: 1,
-        },
-        {
-          label: '02:00',
-          value: 2,
-        },
-        {
-          label: '03:00',
-          value: 3,
-        },
-        {
-          label: '04:00',
-          value: 4,
-        },
-        {
-          label: '05:00',
-          value: 5,
-        },
-        {
-          label: '06:00',
-          value: 6,
-        },
-        {
-          label: '07:00',
-          value: 7,
-        },
-        {
-          label: '08:00',
-          value: 8,
-        },
-        {
-          label: '09:00',
-          value: 9,
-        },
-        {
-          label: '10:00',
-          value: 10,
-        },
-        {
-          label: '11:00',
-          value: 11,
-        },
-        {
-          label: '12:00',
-          value: 12,
-        },
-        {
-          label: '13:00',
-          value: 13,
-        },
-        {
-          label: '14:00',
-          value: 14,
-        },
-        {
-          label: '15:00',
-          value: 15,
-        },
-        {
-          label: '16:00',
-          value: 16,
-        },
-        {
-          label: '17:00',
-          value: 17,
-        },
-        {
-          label: '18:00',
-          value: 18,
-        },
-        {
-          label: '19:00',
-          value: 19,
-        },
-        {
-          label: '20:00',
-          value: 20,
-        },
-        {
-          label: '21:00',
-          value: 21,
-        },
-        {
-          label: '22:00',
-          value: 22,
-        },
-        {
-          label: '23:00',
-          value: 23,
-        },
-      ],
+      options: TIME_OF_DAY_OPTIONS,
     },
   ],
   getDataOutMetadata,
