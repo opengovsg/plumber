4c69ce5224b73606039a9abe3f9b1581215b3708
kevinkim-ogp <kevinkim@open.gov.sg>
2025-07-16 17:14:22 +0800

    chore: improve ui for reorder step

--- packages/frontend/src/components/FlowStep/index.tsx
+++ packages/frontend/src/components/FlowStep/index.tsx
@@ -18,7 +18,7 @@ import UnsavedChangesAlert from '../Editor/UnsavedChangesAlert'
 import EmptyFlowStepHeader from '../EmptyFlowStepHeader'
 import FlowStepConfigurationModal from '../FlowStepConfigurationModal'
 import { infoboxMdComponents } from '../MarkdownRenderer/CustomMarkdownComponents'
-import { SortableList } from '../SortableList'
+import { DragHandle } from '../SortableList/components'
 
 import DeleteStepButton from './components/DeleteStepButton'
 import DuplicateStepButton from './components/DuplicateStepButton'
@@ -139,7 +139,6 @@ export default function FlowStep(
     }
   }
 
-  const headerWidth = getFlowStepHeaderWidth(isDrawerOpen, isMobile, isNested)
   /**
    * NOTE: there are various conditions that determine whether the drag handle
    * should be shown.
@@ -152,6 +151,13 @@ export default function FlowStep(
    */
   const shouldShowDragHandle =
     !readOnly && !isTrigger && !isMobile && !isIfThenStep && allowReorder
+
+  const headerWidth = getFlowStepHeaderWidth(
+    isDrawerOpen,
+    isMobile,
+    isNested,
+    shouldShowDragHandle,
+  )
 
   // generate help message only if template config exists
   const stepAppEventKey = `${step?.appKey}_${step?.key}`
@@ -186,7 +192,9 @@ export default function FlowStep(
       ) : (
         <Flex flexDir="row" w="100%">
           <Flex
-            alignItems="center"
+            alignItems={
+              isNested && !shouldShowDragHandle ? 'flex-start' : 'center'
+            }
             justifyContent="center"
             flexDir="column"
             flex="1"
@@ -262,10 +270,10 @@ export default function FlowStep(
           </Flex>
           {shouldShowDragHandle &&
             (isNested ? (
-              <SortableList.DragHandle onWarningOpen={onWarningOpen} />
+              <DragHandle isNested={isNested} onWarningOpen={onWarningOpen} />
             ) : (
               <Box position="absolute" left="100%" alignSelf="center">
-                <SortableList.DragHandle onWarningOpen={onWarningOpen} />
+                <DragHandle onWarningOpen={onWarningOpen} />
               </Box>
             ))}
         </Flex>
