b6941242eab442c0e588839f6e86e3e1fecb5cff
kevin <kevinkim@open.gov.sg>
2025-07-17 15:32:32 +0800

    fix: missing default metadata at processAction (#1100)
    ## Problem
    Pipe does not execute properly when for-each step is the second step.
    Trigger step does not have metadata, which causes for-each step to fail if it is the second step in the Pipe.
    
    _This was not caught previously as we tested with additional find multiple row step, which are action steps and have metadata by default._
    
    ## Solution
    Add a default metadata at `processAction`
    
    ## How to test
    Create a Pipe where:
    1. FormSG with checkbox
    2. For-each step using checkboxes
    3. Perform some action
    
    - [ ] Each checkbox option is correctly iterated over
    - [ ] Executions page shows success with the correct number of checkbox options

--- packages/backend/src/services/action.ts
+++ packages/backend/src/services/action.ts
@@ -88,7 +88,7 @@ export const processAction = async (options: ProcessActionOptions) => {
     executionId,
     jobId,
     testRun = false,
-    metadata,
+    metadata = {},
   } = options
 
   const step = await Step.query().findById(stepId).throwIfNotFound()
@@ -149,7 +149,7 @@ export const processAction = async (options: ProcessActionOptions) => {
     const result =
       testRun && actionCommand.testRun
         ? await actionCommand.testRun($, metadata)
-        : await actionCommand.run($)
+        : await actionCommand.run($, metadata)
     if (result) {
       runResult = result
     }
