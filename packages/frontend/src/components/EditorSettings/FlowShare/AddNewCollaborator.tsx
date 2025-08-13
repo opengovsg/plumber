ad02b819050a23c95615a6b48a0d69f36d17b571
kevinkim-ogp <kevinkim@open.gov.sg>
2025-08-13 09:51:15 +0800

    chore: alert for adding editor

--- packages/frontend/src/components/EditorSettings/FlowShare/AddNewCollaborator.tsx
+++ packages/frontend/src/components/EditorSettings/FlowShare/AddNewCollaborator.tsx
@@ -1,12 +1,19 @@
 import { IFlow, IFlowCollabRole } from '@plumber/types'
 
-import { BaseSyntheticEvent, useCallback, useEffect, useState } from 'react'
+import {
+  BaseSyntheticEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import {
   Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  useDisclosure,
   VStack,
 } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -14,6 +21,7 @@ import { Button, Input } from '@opengovsg/design-system-react'
 import * as yup from 'yup'
 
 import CollaboratorRoleSelect from '@/components/CollaboratorRoleSelect'
+import MenuAlertDialog from '@/components/MenuAlertDialog'
 
 import SharedConnections from './SharedConnections'
 
@@ -33,10 +41,13 @@ const AddNewCollaborator = ({
   flow: IFlow
   onAdd: (email: string, role: string) => Promise<void>
 }) => {
+  const cancelRef = useRef<HTMLButtonElement>(null)
   const [email, setEmail] = useState('')
   const [role, setRole] = useState<IFlowCollabRole>('editor')
   const [isAdding, setIsAdding] = useState(false)
   const [debouncedIsValid, setDebouncedIsValid] = useState(false)
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const {
     register,
@@ -59,66 +70,96 @@ const AddNewCollaborator = ({
     async (data: FieldValues, event?: BaseSyntheticEvent) => {
       try {
         event?.preventDefault()
-        setIsAdding(true)
-        await onAdd(data.email, role)
-        setEmail('')
-      } finally {
-        setIsAdding(false)
+        if (role === 'editor') {
+          onOpen()
+        } else {
+          setIsAdding(true)
+          await onAdd(data.email, role)
+          setEmail('')
+          setIsAdding(false)
+        }
+      } catch (error) {
+        console.error(error)
       }
     },
-    [onAdd, role],
+    [onAdd, onOpen, role],
   )
 
+  const onConfirm = useCallback(async () => {
+    setIsAdding(true)
+    await onAdd(email, role)
+    setEmail('')
+    onClose()
+    setIsAdding(false)
+  }, [onAdd, email, role, onClose])
+
   return (
-    <form
-      style={{
-        width: '100%',
-      }}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <FormControl isInvalid={!isValid}>
-        <FormLabel>Add collaborator</FormLabel>
-        <VStack spacing={2} alignItems="flex-start">
-          <Flex alignSelf="stretch" gap={2}>
-            <Input
-              type="email"
-              value={email}
-              isRequired
-              {...register('email', {
-                onChange: (e) => {
-                  setEmail(e.target.value.toLowerCase())
-                },
-              })}
-            />
-            <CollaboratorRoleSelect
-              userRole={flow.role as IFlowCollabRole}
-              value={role}
-              onChange={setRole}
-              isEditable={true}
-              showOwnerOption={false}
-            />
-          </Flex>
-          {isDirty && !isValid && (
-            <FormErrorMessage>
-              Please enter a valid email address.
-            </FormErrorMessage>
-          )}
+    <>
+      <form
+        style={{
+          width: '100%',
+        }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <FormControl isInvalid={!isValid}>
+          <FormLabel>Add collaborator</FormLabel>
+          <VStack spacing={2} alignItems="flex-start">
+            <Flex alignSelf="stretch" gap={2}>
+              <Input
+                type="email"
+                value={email}
+                isRequired
+                {...register('email', {
+                  onChange: (e) => {
+                    setEmail(e.target.value.toLowerCase())
+                  },
+                })}
+              />
+              <CollaboratorRoleSelect
+                userRole={flow.role as IFlowCollabRole}
+                value={role}
+                onChange={setRole}
+                isEditable={true}
+                showOwnerOption={false}
+              />
+            </Flex>
+            {isDirty && !isValid && (
+              <FormErrorMessage>
+                Please enter a valid email address.
+              </FormErrorMessage>
+            )}
 
-          {/* Connections appear if pipe is unpublished */}
-          {role === 'editor' && debouncedIsValid && <SharedConnections />}
+            {/* Connections appear if pipe is unpublished */}
+            {role === 'editor' && debouncedIsValid && <SharedConnections />}
 
-          <Button
-            variant={role === 'owner' ? 'solid' : 'outline'}
-            colorScheme="red"
-            type="submit"
-            isLoading={isAdding}
-            isDisabled={!isValid}
-          >
-            Add collaborator
-          </Button>
-        </VStack>
-      </FormControl>
-    </form>
+            <Button
+              variant={role === 'owner' ? 'solid' : 'outline'}
+              colorScheme="red"
+              type="submit"
+              isLoading={isAdding}
+              isDisabled={!isValid}
+            >
+              Add collaborator
+            </Button>
+          </VStack>
+        </FormControl>
+      </form>
+
+      {/* dialog to warn user that connections will be shared with editors */}
+      <MenuAlertDialog
+        cancelRef={cancelRef}
+        isLoading={isAdding}
+        isDialogOpen={isOpen}
+        onDialogClose={onClose}
+        dialogType="share-connections"
+        dialogHeader="Share connections"
+        onClick={onConfirm}
+        customBody={`You are adding **${email.replace(
+          '@',
+          '@\u200B', // add zero-width space after @ to prevent email address rendering as an external link
+        )}** as an editor. They will have access to your connections and will be able to use them in this Pipe.`}
+      />
+    </>
   )
 }
 
