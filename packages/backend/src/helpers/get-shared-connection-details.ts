7694c0267c56121e26b59895d515b33125d0f72b
kevinkim-ogp <kevinkim@open.gov.sg>
2025-08-25 22:08:46 +0800

    chore: add m365-excel and add tests

--- packages/backend/src/helpers/get-shared-connection-details.ts
+++ packages/backend/src/helpers/get-shared-connection-details.ts
@@ -2,14 +2,21 @@ import { IStep } from '@plumber/types'
 
 /**
  * THIS MUST BE UPDATED WHEN A NEW APP OR NEW DYNAMIC FIELD IS ADDED
+ * we only need to app apps that have fields that behave like connections.
+ * apps that only have connections such as formsg or postman-sms
+ * do not need to be specified here
  */
 export const APP_CONNECTION_FIELDS: Record<
   IStep['appKey'],
   {
-    parameterKey: string
-    dynamicDataKey: string
+    parameterKey?: string
+    dynamicDataKey?: string
   }
 > = {
+  'm365-excel': {
+    parameterKey: 'fileId',
+    dynamicDataKey: 'listFiles',
+  },
   lettersg: {
     parameterKey: 'templateId',
     dynamicDataKey: 'getTemplateIds',
@@ -43,19 +50,22 @@ export function getConnectionDetails(steps: IStep[]): {
 
   steps.map((step) => {
     if (step.connectionId) {
-      const paramKey = APP_CONNECTION_FIELDS[step.appKey].parameterKey
+      connections[step.connectionId] ??= {}
+
+      const paramKey = APP_CONNECTION_FIELDS[step.appKey]?.parameterKey
       const paramValue = step.parameters[paramKey] as string
 
-      // create nested objects if missing
-      connections[step.connectionId] ??= {}
-      connections[step.connectionId][paramKey] ??= []
+      // only some apps need the metadata, so we only add it if the app has a parameter key
+      if (paramKey) {
+        connections[step.connectionId][paramKey] ??= []
 
-      // push only if not already present
-      if (
-        paramValue &&
-        !connections[step.connectionId][paramKey].includes(paramValue)
-      ) {
-        connections[step.connectionId][paramKey].push(paramValue)
+        // push only if not already present
+        if (
+          paramValue &&
+          !connections[step.connectionId][paramKey].includes(paramValue)
+        ) {
+          connections[step.connectionId][paramKey].push(paramValue)
+        }
       }
     }
 
