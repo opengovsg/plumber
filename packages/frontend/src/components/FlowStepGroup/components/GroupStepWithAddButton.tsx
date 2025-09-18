7589f9b7b021bfa0e457cb6c54f2c973da7374d1
kevinkim-ogp <kevinkim@open.gov.sg>
2025-09-18 12:05:37 +0800

    fix: allow deleting of onlyContinueIf

--- packages/frontend/src/components/FlowStepGroup/components/GroupStepWithAddButton.tsx
+++ packages/frontend/src/components/FlowStepGroup/components/GroupStepWithAddButton.tsx
@@ -4,7 +4,7 @@ import { useContext } from 'react'
 
 import { EditorContext } from '@/contexts/Editor'
 import { FlowStep } from '@/exports/components'
-import { TOOLBOX_ACTIONS, TOOLBOX_APP_KEY } from '@/helpers/toolbox'
+import { TOOLBOX_ACTIONS } from '@/helpers/toolbox'
 
 import { HoverAddStepButton } from '../Content/IfThen/HoverAddStepButton'
 
@@ -32,9 +32,7 @@ export default function GroupStepWithAddButton(
 
   // cannot delete the condition step
   const isDeletable =
-    step.appKey !== TOOLBOX_APP_KEY &&
-    step.key !== TOOLBOX_ACTIONS.IfThen &&
-    step.key !== TOOLBOX_ACTIONS.ForEach
+    step.key !== TOOLBOX_ACTIONS.IfThen && step.key !== TOOLBOX_ACTIONS.ForEach
 
   return (
     <>
