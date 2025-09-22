a219fd80229c3c2b4473dae4f68bced7b3001e9d
kevinkim-ogp <kevinkim@open.gov.sg>
2025-09-22 18:11:14 +0800

    chore: add composite primary key flow_id, user_id

--- packages/backend/src/db/migrations/20250725070831_flow_collaborators.ts
+++ packages/backend/src/db/migrations/20250725070831_flow_collaborators.ts
@@ -9,6 +9,8 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('deleted_at').nullable()
     table.timestamp('last_accessed_at').notNullable().defaultTo(knex.fn.now())
     table.uuid('updated_by').references('id').inTable('users').notNullable()
+
+    table.primary(['flow_id', 'user_id'])
   })
 }
 
