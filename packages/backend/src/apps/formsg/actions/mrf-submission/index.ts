afa8ed6ff79e9d6c81750cf01a867b7f0f650ebe
Ian Chen <ian@open.gov.sg>
2025-10-16 17:16:29 +0800

    feat: testStep for mrf actions should test multiple steps

--- packages/backend/src/apps/formsg/actions/mrf-submission/index.ts
+++ packages/backend/src/apps/formsg/actions/mrf-submission/index.ts
@@ -1,6 +1,14 @@
-import { IGlobalVariable, IRawAction } from '@plumber/types'
+import type { IGlobalVariable, IRawAction } from '@plumber/types'
 
-import getDataOutMetadata from '../../triggers/new-submission/get-data-out-metadata'
+import StepError from '@/errors/step'
+import ExecutionStep from '@/models/execution-step'
+import Step from '@/models/step'
+
+import getDataOutMetadata from '../../common/get-data-out-metadata'
+import {
+  type ParsedMrfWorkflowStep,
+  parsedMrfWorkflowStepSchema,
+} from '../../common/types'
 
 const action: IRawAction = {
   name: 'New form response',
@@ -10,8 +18,45 @@ const action: IRawAction = {
     'This is a hidden action that signifies a subsequent MRF submission',
   getDataOutMetadata,
 
-  async testRun(_$: IGlobalVariable) {
-    // TODO: this should run testRun for the trigger execution step
+  async testRun($: IGlobalVariable) {
+    // Gets the execution step of the trigger
+    const { mrf } = $.step.parameters as unknown as {
+      mrf: ParsedMrfWorkflowStep
+    }
+
+    if (!mrf || parsedMrfWorkflowStepSchema.safeParse(mrf).success === false) {
+      throw new StepError(
+        'Misconfigured MRF step',
+        'Reconnect your MRF form and try again.',
+        $.step.position,
+        $.app.name,
+      )
+    }
+
+    const triggerStep = await Step.query()
+      .findOne({
+        flow_id: $.flow.id,
+        type: 'trigger',
+        key: 'newSubmission',
+        app_key: 'formsg',
+      })
+      .throwIfNotFound()
+    const triggerExecutionStep = await ExecutionStep.query()
+      .where('step_id', triggerStep.id)
+      .andWhere('execution_id', $.execution.id)
+      .first()
+
+    if (!triggerExecutionStep) {
+      $.setActionItem({
+        raw: null,
+      })
+      return
+    }
+
+    $.setActionItem({
+      raw: triggerExecutionStep.dataOut,
+      meta: triggerExecutionStep.metadata,
+    })
   },
 }
 
