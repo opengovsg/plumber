2c82fdafa6d769e2c164eee5b2432bbda6432eb6
Ian Chen <ian@open.gov.sg>
2025-10-21 20:26:29 +0800

    fix: since formsg remove some fields

--- packages/backend/src/apps/formsg/common/types.ts
+++ packages/backend/src/apps/formsg/common/types.ts
@@ -1,37 +1,13 @@
 import { z } from 'zod'
 
-/**
- * This determines the kind of routing for this step
- * Static: routes to a fixed list of emails
- * Dynamic: routes based on a a specified email field
- * Conditional: routes based on a dropdown field which corresponds to a list of emails
- */
-const workflowTypeSpecificFields = z.discriminatedUnion('workflow_type', [
-  z.object({
-    workflow_type: z.literal('static'),
-    emails: z.array(z.string()),
-  }),
-  z.object({
-    workflow_type: z.literal('dynamic'),
-    // the email field to use for routing
-    field: z.string(),
-  }),
-  z.object({
-    workflow_type: z.literal('conditional'),
-    // the dropdown field to use for routing
-    conditional_field: z.string(),
-  }),
-])
-
 export const mrfWorkflowDataSchema = z.array(
-  z
-    .object({
-      _id: z.string(),
-      edit: z.array(z.string()),
-      step_name: z.string().optional(),
-      approval_field: z.string().optional(),
-    })
-    .and(workflowTypeSpecificFields),
+  z.object({
+    _id: z.string(),
+    edit: z.array(z.string()),
+    step_name: z.string().optional(),
+    approval_field: z.string().optional(),
+    workflow_type: z.enum(['static', 'dynamic', 'conditional']),
+  }),
 )
 
 export interface FormSchema {
