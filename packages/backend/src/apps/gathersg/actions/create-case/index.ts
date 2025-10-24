ff49dc10fad114ac0ad8fe62d8ad363aa06c4ffc
kevin <kevinkim@open.gov.sg>
2025-10-24 14:13:07 +0800

    [OG-2] Create case (#1262)
    ## TL;DR
    This PR adds a new "Create Case" action to the GatherSG app, allowing users to create cases with specified fields and status.
    
    ## What's new
    - New schema builder utilities for validating different field types (text, date, number, etc.)
    - Validation for date/time formats and logical correctness, validation for phone numbers
    - Reusable field schema generation for both create and update operations
    - New dynamic data endpoint to fetch available case types
    
    ## How to test?
    On GatherSG:
    - Create some case types
    On Plumber
    - [ ] Can select the create case action and connect successfully
    - [ ] Should see the various case types in the dropdown
    - [ ] Should see the correct fields for the different case types
    - [ ] Can create case successfully
    - [ ] Invalid values for case fields are rejected

---
+++ packages/backend/src/apps/gathersg/actions/create-case/index.ts
@@ -0,0 +1,184 @@
+import { IRawAction } from '@plumber/types'
+
+import { ZodError } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+import HttpError from '@/errors/http'
+import StepError, { GenericSolution } from '@/errors/step'
+import logger from '@/helpers/logger'
+import { ensureZodEnumValue } from '@/helpers/zod-utils'
+
+import { fetchCaseFields } from '../../common/fetch-case-fields'
+import throwGatherSGStepError from '../../common/throw-errors'
+
+import getDataOutMetadata from './get-data-out-metadata'
+import { fieldTypeEnum, requestSchema, responseSchema } from './schema'
+
+const action: IRawAction = {
+  name: 'Create case',
+  key: 'createCase',
+  description: 'Create a case',
+  arguments: [
+    {
+      label: 'Case type',
+      key: 'caseType',
+      type: 'dropdown' as const,
+      description: 'Enter the type of the case you want to create',
+      required: true,
+      variables: false,
+      showOptionValue: false,
+      source: {
+        type: 'query' as const,
+        name: 'getDynamicData' as const,
+        arguments: [
+          {
+            name: 'key',
+            value: 'getCaseTypes',
+          },
+        ],
+      },
+    },
+    {
+      label: 'Case status',
+      key: 'caseStatus',
+      type: 'dropdown' as const,
+      description: 'Select the status you want to update the case to.',
+      required: false,
+      variables: true,
+      showOptionValue: false,
+      hiddenIf: {
+        fieldKey: 'caseType',
+        op: 'is_empty',
+      },
+      source: {
+        type: 'query' as const,
+        name: 'getDynamicData' as const,
+        arguments: [{ name: 'key', value: 'getCaseStatuses' }],
+      },
+    },
+    {
+      label: 'Case fields',
+      key: 'caseFields',
+      type: 'multirow-multicol' as const,
+      required: true,
+      description:
+        'Specify values for each field you want to update in your case. Note that fields that require an array of objects as a value are not supported yet.',
+      hiddenIf: {
+        fieldKey: 'caseType',
+        op: 'is_empty',
+      },
+      subFields: [
+        {
+          placeholder: 'Field',
+          key: 'field',
+          type: 'dropdown' as const,
+          showOptionValue: false,
+          required: true,
+          variables: false,
+          allowArbitrary: true,
+          source: {
+            type: 'query' as const,
+            name: 'getDynamicData' as const,
+            arguments: [
+              {
+                name: 'key',
+                value: 'getCaseFields',
+              },
+              {
+                name: 'parameters.caseType',
+                value: '{parameters.caseType}',
+              },
+            ],
+          },
+          customStyle: { flex: 2 },
+        },
+        {
+          placeholder: 'Field type',
+          key: 'fieldType',
+          type: 'dropdown' as const,
+          showOptionValue: false,
+          required: true,
+          value: 'string',
+          variables: false,
+          options: [
+            {
+              label: 'Text',
+              value: ensureZodEnumValue(fieldTypeEnum, 'string'),
+            },
+            {
+              label: 'Number',
+              value: ensureZodEnumValue(fieldTypeEnum, 'number'),
+            },
+            {
+              label: 'Null',
+              value: ensureZodEnumValue(fieldTypeEnum, 'null'),
+            },
+          ],
+          customStyle: { flex: 1, maxWidth: 140 },
+        },
+        {
+          placeholder: 'Value',
+          key: 'value',
+          type: 'string' as const,
+          required: true,
+          variables: true,
+          hiddenIf: {
+            fieldKey: 'fieldType',
+            op: 'equals',
+            fieldValue: 'null',
+          },
+          customStyle: { flex: 3, minWidth: 0, maxWidth: '60%' },
+        },
+      ],
+    },
+  ],
+
+  getDataOutMetadata,
+
+  async run($) {
+    try {
+      const parameters = requestSchema.parse($.step.parameters)
+      const { caseType: caseTypeUuid, status, fields } = parameters
+
+      // get the case type name from the case type uuid
+      const { caseTypeName } = await fetchCaseFields({ $, caseTypeUuid })
+
+      const payload = {
+        ...(status && { status }),
+        fields,
+        type: caseTypeName,
+      }
+
+      const rawResponse = await $.http.post('/cases', payload)
+      const { data } = responseSchema.parse(rawResponse.data)
+
+      $.setActionItem({
+        raw: {
+          data,
+        },
+      })
+    } catch (error) {
+      logger.error('Failed to create case on GatherSG:', error)
+      if (error instanceof ZodError) {
+        const firstError = fromZodError(error).details[0]
+        throw new StepError(
+          `${firstError.path[0]}: ${firstError.message}`,
+          GenericSolution.ReconfigureInvalidField,
+          $.step.position,
+          $.app.name,
+        )
+      }
+      if (error instanceof HttpError) {
+        throwGatherSGStepError({ $, error })
+      }
+      throw new StepError(
+        `An error occurred: '${error.message}'`,
+        'Please check that you have configured your step correctly',
+        $.step.position,
+        $.app.name,
+      )
+    }
+  },
+}
+
+export default action
---
+++ packages/backend/src/apps/gathersg/common/fetch-case-fields.ts
@@ -0,0 +1,42 @@
+import { IGlobalVariable } from '@plumber/types'
+
+import { UNSUPPORTED_FIELDS } from './constants'
+
+export type GatherSGCaseField = {
+  name: string
+  type: string
+  optional: boolean
+}
+
+type GatherSGCaseFields = {
+  data: {
+    uuid: string
+    name: string
+    version: number
+    fields: GatherSGCaseField[]
+  }
+}
+
+export const fetchCaseFields = async ({
+  $,
+  caseTypeUuid,
+}: {
+  $: IGlobalVariable
+  caseTypeUuid: string
+}) => {
+  const { data } = await $.http.get<GatherSGCaseFields>(
+    `/admin/caseTypes/:caseTypeUuid`,
+    {
+      urlPathParams: { caseTypeUuid },
+    },
+  )
+
+  const filteredFields = data.data.fields.filter(
+    ({ type }) => !UNSUPPORTED_FIELDS.includes(type),
+  )
+
+  return {
+    filteredFields,
+    caseTypeName: data.data.name,
+  }
+}
