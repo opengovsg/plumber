8f6cb598694ca50b893ddfd30828d9214a3eefbe
kevinkim-ogp <kevinkim@open.gov.sg>
2025-10-31 00:01:40 +0800

    fix: linter errors

--- packages/frontend/src/components/FlowSubstep/index.tsx
+++ packages/frontend/src/components/FlowSubstep/index.tsx
@@ -127,7 +127,7 @@ function FlowSubstep(props: FlowSubstepProps): JSX.Element {
       try {
         await saveStep()
         await executeTestStep({ testRunMetadata })
-      } catch (error) {
+      } catch {
         console.error('Error saving and test step')
       }
     },
