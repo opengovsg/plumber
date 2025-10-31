c10ab6f6cb440afbe8416b5a9c2be6bdcd3b2e10
Ian Chen <ian@open.gov.sg>
2025-10-31 13:59:28 +0800

    fix(databricks): properly verify credentials

--- packages/backend/src/apps/databricks/auth/verify-credentials.ts
+++ packages/backend/src/apps/databricks/auth/verify-credentials.ts
@@ -3,6 +3,9 @@ import { IGlobalVariable } from '@plumber/types'
 import { ZodError } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 
+import { databricksConfig } from '@/config/app-env-vars/databricks'
+import logger from '@/helpers/logger'
+
 import { validateAuthData } from './auth-data'
 import { createClient } from './create-client'
 
@@ -10,11 +13,22 @@ const verifyCredentials = async ($: IGlobalVariable) => {
   try {
     validateAuthData($)
     const client = await createClient($)
+    const session = await client.openSession({
+      initialCatalog: databricksConfig.catalog,
+    })
+    await session.close()
     await client.close()
   } catch (error) {
     if (error instanceof ZodError) {
       // Auth data validation failed: throws message from first error
       throw new Error(fromZodError(error).details[0].message)
+    }
+    if (error.response?.status === 401) {
+      logger.info({
+        event: 'databricks-verify-credentials',
+        error: error,
+      })
+      throw new Error('Invalid credentials')
     }
     throw error
   }
