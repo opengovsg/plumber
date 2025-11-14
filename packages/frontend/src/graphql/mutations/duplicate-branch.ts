8fd93e6e67e8b470a10a87f97bca3a2057d2fc7a
kevin <kevinkim@open.gov.sg>
2025-11-14 08:04:15 +0800

    [COLLAB-20]: fix duplicate branch (#1277)
    ## Problem
    Cannot duplicate branch
    
    ## Solution
    Added a new `duplicateBranch` mutation to duplicate multiple steps in a branch with a single API call.
    
    ### What changed?
    Created a new GraphQL mutation `duplicateBranch` that duplicates multiple steps in a single transaction so that any failure will not create the branch at akll
    
    ### Why make this change?
    The implementation ensures proper transaction handling, permission checking, and position updates for all steps in a single database transaction, which is more efficient than the previous approach of creating steps sequentially.
    
    ## How to test?
    Create steps within an if-then branch
    - [ ] Can duplicate branch with all steps and parameters

---
+++ packages/frontend/src/graphql/mutations/duplicate-branch.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client'
+
+export const DUPLICATE_BRANCH = gql`
+  mutation DuplicateBranch($input: DuplicateBranchInput) {
+    duplicateBranch(input: $input) {
+      flow {
+        updatedAt
+      }
+      steps {
+        id
+        type
+        key
+        appKey
+        parameters
+        position
+        status
+        connection {
+          id
+        }
+        config {
+          stepName
+        }
+      }
+    }
+  }
+`
