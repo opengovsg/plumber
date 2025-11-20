ae278877b1da5184ded4d1c54e0dcdd4ef3ad0a3
kevinkim-ogp <kevinkim@open.gov.sg>
2025-11-20 22:34:26 +0800

    feat: chat feedback

--- packages/frontend/src/config/app.ts
+++ packages/frontend/src/config/app.ts
@@ -7,6 +7,8 @@ interface AppConfig {
   lensSurveyClientKey: string
   ssoClientId: string
   ssoHostname: string
+  pairRomeBaseUrl: string
+  pairRomePublicKey: string
 }
 
 function getAppConfig(): AppConfig {
@@ -28,6 +30,8 @@ function getAppConfig(): AppConfig {
         lensSurveyClientKey: 'cm85ca2f300053ooz4vydrmyw',
         ssoClientId: 'plumber-prod',
         ssoHostname: 'https://sso.open.gov.sg',
+        pairRomeBaseUrl: 'https://rome.pair.gov.sg',
+        pairRomePublicKey: '', // TODO: add public key
         ...commonEnv,
       }
     // UAT and staging differ for the lens survey client key only
@@ -39,6 +43,8 @@ function getAppConfig(): AppConfig {
         lensSurveyClientKey: 'cm8fp8i030008zm2tbuc07xe5',
         ssoClientId: 'plumber-uat',
         ssoHostname: 'https://sso.open.gov.sg',
+        pairRomeBaseUrl: 'https://rome.pair.gov.sg',
+        pairRomePublicKey: '', // TODO: add public key
         ...commonEnv,
       }
     case 'staging':
@@ -49,6 +55,9 @@ function getAppConfig(): AppConfig {
         lensSurveyClientKey: 'cm86psst900052orfqetz3gz5',
         ssoClientId: 'plumber-staging',
         ssoHostname: 'https://sso.open.gov.sg',
+        // TODO: update to use Rome when ready
+        pairRomeBaseUrl: 'https://istanbul.pair.gov.sg',
+        pairRomePublicKey: 'pk-lf-3874d01f-530f-42f9-afd7-ea46d1bcfac3',
         ...commonEnv,
       }
     default:
@@ -59,6 +68,9 @@ function getAppConfig(): AppConfig {
         lensSurveyClientKey: 'cm8fpeah2000gzm2t572lhfti',
         ssoClientId: 'plumber-local',
         ssoHostname: 'http://localhost:5354',
+        // TODO: update to use Rome when ready
+        pairRomeBaseUrl: 'https://istanbul.pair.gov.sg',
+        pairRomePublicKey: 'pk-lf-3874d01f-530f-42f9-afd7-ea46d1bcfac3',
         ...commonEnv,
       }
   }
--- packages/frontend/src/pages/AiBuilder/components/ChatMessages/ChatMessageToolbar.tsx
+++ packages/frontend/src/pages/AiBuilder/components/ChatMessages/ChatMessageToolbar.tsx
@@ -1,5 +1,31 @@
+import React, { useState } from 'react'
 import { FaRegThumbsDown } from 'react-icons/fa'
-import { Flex, Icon, IconButton, Tooltip } from '@chakra-ui/react'
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  FocusLock,
+  FormControl,
+  FormLabel,
+  Icon,
+  IconButton,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Stack,
+  Textarea,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { LangfuseWeb } from 'langfuse'
+
+import appConfig from '@/config/app'
+
+const langfuse = new LangfuseWeb({
+  baseUrl: appConfig.pairRomeBaseUrl,
+  publicKey: appConfig.pairRomePublicKey,
+})
 
 const DEFAULT_BUTTON_PROPS = {
   size: 'xs',
@@ -8,21 +34,90 @@ const DEFAULT_BUTTON_PROPS = {
   _hover: { color: 'gray.700', bg: 'gray.100' },
 }
 
-export default function ChatMessageToolbar() {
-  const handleThumbsDown = () => {
-    // TODO: Implement feedback functionality
+interface ChatMessageToolbarProps {
+  traceId: string
+}
+
+export default function ChatMessageToolbar({
+  traceId,
+}: ChatMessageToolbarProps) {
+  const { onOpen, onClose, isOpen } = useDisclosure()
+  const firstFieldRef = React.useRef(null)
+  const [comment, setComment] = useState('')
+
+  const handleSubmitFeedback = (comment: string) => {
+    try {
+      if (!traceId) {
+        return
+      }
+
+      // Send feedback to Rome / Istanbul
+      langfuse.score({
+        traceId,
+        id: `user-feedback-${traceId}`,
+        name: 'user-feedback',
+        value: 0, // 1 for positive, 0 for negative
+        comment,
+      })
+    } catch (error) {
+      console.error('Error submitting feedback:', error)
+    } finally {
+      onClose()
+    }
   }
 
   return (
     <Flex gap={1} mt={2}>
-      <Tooltip label="Not helpful" placement="top">
-        <IconButton
-          {...DEFAULT_BUTTON_PROPS}
-          aria-label="Thumbs down"
-          icon={<Icon as={FaRegThumbsDown} />}
-          onClick={handleThumbsDown}
-        />
-      </Tooltip>
+      <Popover
+        isOpen={isOpen}
+        initialFocusRef={firstFieldRef}
+        onOpen={onOpen}
+        onClose={onClose}
+      >
+        <PopoverTrigger>
+          <IconButton
+            {...DEFAULT_BUTTON_PROPS}
+            aria-label="Thumbs down"
+            icon={<Icon as={FaRegThumbsDown} />}
+            onClick={onOpen}
+          />
+        </PopoverTrigger>
+        <PopoverContent>
+          <FocusLock persistentFocus={false}>
+            <PopoverArrow />
+
+            <PopoverBody>
+              <Stack spacing={4}>
+                <FormControl>
+                  <FormLabel htmlFor="why-not-helpful">
+                    Why was this not helpful?
+                  </FormLabel>
+                  <Textarea
+                    ref={firstFieldRef}
+                    id="why-not-helpful"
+                    rows={3}
+                    resize="none"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  />
+                </FormControl>
+                <ButtonGroup display="flex" justifyContent="flex-end">
+                  <Button variant="outline" onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    isDisabled={!comment}
+                    colorScheme="teal"
+                    onClick={() => handleSubmitFeedback(comment)}
+                  >
+                    Submit feedback
+                  </Button>
+                </ButtonGroup>
+              </Stack>
+            </PopoverBody>
+          </FocusLock>
+        </PopoverContent>
+      </Popover>
     </Flex>
   )
 }
