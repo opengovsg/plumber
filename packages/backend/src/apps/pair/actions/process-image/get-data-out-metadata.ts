78201139315f2490c262faad573e052a75a2be7b
kevinkim-ogp <kevinkim@open.gov.sg>
2025-12-17 22:09:22 +0800

    feat: pair process image

---
+++ packages/backend/src/apps/pair/actions/process-image/get-data-out-metadata.ts
@@ -0,0 +1,26 @@
+import {
+  IDataOutMetadata,
+  IDataOutMetadatum,
+  IExecutionStep,
+} from '@plumber/types'
+
+async function getDataOutMetadata(
+  executionStep: IExecutionStep,
+): Promise<IDataOutMetadata> {
+  const { dataOut } = executionStep
+  if (!dataOut) {
+    return null
+  }
+
+  const metadata = Object.keys(dataOut).reduce((acc, key) => {
+    acc[key] = {
+      label: key,
+      type: 'ai_response',
+    }
+    return acc
+  }, {} as Record<string, IDataOutMetadatum>)
+
+  return metadata
+}
+
+export default getDataOutMetadata
---
+++ packages/backend/src/apps/pair/actions/process-image/schema.ts
@@ -0,0 +1,22 @@
+import z from 'zod/v3'
+
+export const schema = z.object({
+  image: z.array(z.string()),
+  responseFields: z
+    .array(
+      z.object({
+        fieldName: z
+          .string()
+          .min(1)
+          .max(64)
+          .regex(
+            /^[a-zA-Z0-9_-]+$/,
+            'Field name cannot contain spaces. Use only letters, numbers, underscores (_), and hyphens (-)',
+          ),
+
+        description: z.string().min(1),
+      }),
+    )
+    .optional()
+    .default([]),
+})
