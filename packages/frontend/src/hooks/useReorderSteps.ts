1b664ea33297feca00d604dfeccd9e194e14c132
Ian Chen <ian@open.gov.sg>
2026-01-31 17:51:50 +0800

    [MRF-15] PLU-520: (frontend) allow reordering of steps across MRF steps (#1343)

--- packages/frontend/src/hooks/useReorderSteps.ts
+++ packages/frontend/src/hooks/useReorderSteps.ts
@@ -1,24 +1,127 @@
-import { IStep } from '@plumber/types'
+import { IStep, IStepApprovalBranch } from '@plumber/types'
 
-import { useContext } from 'react'
+import { useCallback, useContext } from 'react'
 import { useMutation } from '@apollo/client'
 
 import { EditorContext } from '@/contexts/Editor'
-import { StepEnumType } from '@/graphql/__generated__/graphql'
+import {
+  StepEnumType,
+  StepPositionInput,
+} from '@/graphql/__generated__/graphql'
 import { UPDATE_STEP_POSITIONS } from '@/graphql/mutations/update-step-positions'
 import { GET_FLOW } from '@/graphql/queries/get-flow'
-
-interface StepPositionInput {
-  id: string
-  position: number
-  type: StepEnumType
-}
 
 const useReorderSteps = (flowId: string) => {
   const { flow } = useContext(EditorContext)
   const [updateStepPositions] = useMutation(UPDATE_STEP_POSITIONS, {
     refetchQueries: [GET_FLOW],
   })
+
+  // TODO: write unit test for this function
+  const calculateReorderedSteps = useCallback(
+    ({
+      reorderedSteps,
+      allSteps,
+      mrfSteps,
+      mrfApprovalSteps,
+      approvalBranches,
+    }: {
+      reorderedSteps: IStep[]
+      allSteps: IStep[]
+      mrfSteps: IStep[]
+      mrfApprovalSteps: IStep[]
+      approvalBranches: { [stepId: string]: IStepApprovalBranch }
+    }): StepPositionInput[] => {
+      // we start from 2 because the first step is the trigger step and not part of the sortable list
+      let nextPosition = 2
+      let currentApprovalBranch: {
+        stepId: string
+        branch: 'approve' | 'reject'
+      } | null = null
+      const allReorderedSteps: StepPositionInput[] = []
+      reorderedSteps.forEach((reorderingStep) => {
+        const isMrfStep = mrfSteps.some(
+          (mrfStep) => mrfStep.id === reorderingStep.id,
+        )
+
+        if (isMrfStep) {
+          // if the previous approval config is in the approve branch
+          // we need to add the steps in the reject branch that was hidden
+          if (currentApprovalBranch?.branch === 'approve') {
+            const stepsInRejectBranch = allSteps.filter(
+              (step) =>
+                step.config?.approval?.branch === 'reject' &&
+                step.config?.approval?.stepId === currentApprovalBranch?.stepId,
+            ) as IStep[]
+            stepsInRejectBranch.forEach((step) => {
+              allReorderedSteps.push({
+                id: step.id,
+                position: nextPosition++,
+                type: step.type as StepEnumType,
+              })
+            })
+          }
+          currentApprovalBranch = null
+        }
+
+        // add the current step to the list
+        allReorderedSteps.push({
+          id: reorderingStep.id,
+          position: nextPosition++,
+          type: reorderingStep.type as StepEnumType,
+          config:
+            currentApprovalBranch?.branch === 'reject'
+              ? {
+                  approval: currentApprovalBranch,
+                }
+              : { approval: null }, // this sets it to approval branch
+        })
+
+        const isMrfApprovalStep = mrfApprovalSteps.some(
+          (mrfApprovalStep) => mrfApprovalStep.id === reorderingStep.id,
+        )
+        if (isMrfApprovalStep) {
+          currentApprovalBranch = {
+            branch: approvalBranches[reorderingStep.id],
+            stepId: reorderingStep.id,
+          }
+          // if the current approval config is in the reject branch
+          // we need to add the steps in the approve branch that was hidden
+          if (currentApprovalBranch?.branch === 'reject') {
+            const reorderingStepIndex = allSteps.findIndex(
+              (step) => step.id === reorderingStep.id,
+            )
+            const stepsInApproveBranch = []
+            // Search for non-mrf steps directly after the approval step
+            for (let i = reorderingStepIndex + 1; i < allSteps.length; i++) {
+              const step = allSteps[i]
+              if (
+                !step.config?.approval &&
+                !mrfSteps.some((mrfStep) => mrfStep.id === step.id)
+              ) {
+                stepsInApproveBranch.push(step)
+              } else {
+                break
+              }
+            }
+            stepsInApproveBranch.forEach((step) => {
+              allReorderedSteps.push({
+                id: step.id,
+                position: nextPosition++,
+                type: step.type as StepEnumType,
+              })
+            })
+          }
+        }
+      })
+
+      // all later steps not in the sortable list need not be updated since
+      // reordering of visible steps will not affect them
+
+      return allReorderedSteps
+    },
+    [],
+  )
 
   const handleReorderUpdate = async (stepPositions: StepPositionInput[]) => {
     try {
@@ -32,6 +135,7 @@ const useReorderSteps = (flowId: string) => {
             steps: stepPositions.map((sp) => ({
               id: sp.id,
               position: sp.position,
+              config: sp.config ? { approval: sp.config.approval } : undefined,
               __typename: 'Step' as const,
             })),
           },
@@ -45,15 +149,22 @@ const useReorderSteps = (flowId: string) => {
 
           if (flow) {
             // Create a map of step positions for quick lookup
-            const positionMap = new Map(
-              stepPositions.map((sp) => [sp.id, sp.position]),
+            const updatedStepMap = new Map(
+              stepPositions.map((sp) => [
+                sp.id,
+                { position: sp.position, config: sp.config },
+              ]),
             )
 
             // Update steps with new positions
             const updatedSteps = flow.steps.map((step: IStep) => {
-              const newPosition = positionMap.get(step.id)
-              return newPosition !== undefined
-                ? { ...step, position: newPosition }
+              const updatedStep = updatedStepMap.get(step.id)
+              return updatedStep !== undefined
+                ? {
+                    ...step,
+                    position: updatedStep.position,
+                    config: { ...step.config, ...updatedStep.config },
+                  }
                 : step
             })
 
@@ -84,7 +195,7 @@ const useReorderSteps = (flowId: string) => {
     }
   }
 
-  return { handleReorderUpdate }
+  return { handleReorderUpdate, calculateReorderedSteps }
 }
 
 export default useReorderSteps
