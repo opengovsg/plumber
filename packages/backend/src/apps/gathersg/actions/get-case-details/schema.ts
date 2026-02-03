40b93508a10a44e5771e25faf34df46e3ac7d370
kevinkim-ogp <kevinkim@open.gov.sg>
2026-02-03 08:43:07 +0800

    chore: update dataOutSchema

--- packages/backend/src/apps/gathersg/actions/get-case-details/schema.ts
+++ packages/backend/src/apps/gathersg/actions/get-case-details/schema.ts
@@ -3,6 +3,11 @@ import { z } from 'zod'
 export const dataOutSchema = z.object({
   data: z
     .object({
+      type: z.object({
+        name: z.string().min(1),
+        uuid: z.string().min(1),
+        slaDay: z.number().nullish(),
+      }),
       fields: z.record(z.string(), z.any()).nullish(),
       formsg: z
         .object({
@@ -23,6 +28,18 @@ export const dataOutSchema = z.object({
         })
         .nullish(),
       tags: z.array(z.string()).nullish(),
+      attachments: z
+        .record(
+          z.string(),
+          z.object({
+            name: z.string().min(1),
+            mimeType: z.string().min(1),
+            size: z.number(),
+          }),
+        )
+        .nullish(),
+      email: z.record(z.string(), z.any()).nullish(),
     })
     .nullish(),
+  traceId: z.string().min(1),
 })
