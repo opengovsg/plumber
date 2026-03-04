856e81a6eac5236b4dd02d9cb8fd849ee67d9ecf
Ian Chen <ian@open.gov.sg>
2026-03-04 17:28:22 +0800

    fix: do not close side bar when toggling approval branches unless step disappears

--- packages/frontend/src/components/FlowStep/components/ApproveReject.tsx
+++ packages/frontend/src/components/FlowStep/components/ApproveReject.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useContext } from 'react'
 import { Flex, Tab, TabList, TabProps, Tabs } from '@chakra-ui/react'
 
-import { EditorContext } from '@/contexts/Editor'
 import { MrfContext } from '@/contexts/MrfContext'
 
 const tabStyle = (primaryColor: string): TabProps => {
@@ -29,7 +28,6 @@ const tabStyle = (primaryColor: string): TabProps => {
 }
 
 export function ApproveReject({ stepId }: { stepId: string }) {
-  const { setCurrentStepId, onDrawerClose } = useContext(EditorContext)
   const { approvalBranches, setApprovalBranch } = useContext(MrfContext)
 
   const isApproveBranch = approvalBranches[stepId] === 'approve'
@@ -37,10 +35,8 @@ export function ApproveReject({ stepId }: { stepId: string }) {
   const onChange = useCallback(
     (index: number) => {
       setApprovalBranch(stepId, index === 0 ? 'approve' : 'reject')
-      setCurrentStepId(null)
-      onDrawerClose()
     },
-    [stepId, setApprovalBranch, setCurrentStepId, onDrawerClose],
+    [stepId, setApprovalBranch],
   )
 
   return (
