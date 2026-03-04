5fbd575f291d686df36e6d18cdf3f5b5f8bfc9c5
kevinkim-ogp <kevinkim@open.gov.sg>
2026-03-04 13:57:07 +0800

    refactor: remove refineFormInput

--- packages/frontend/src/graphql/mutations/ai/refine-form-input.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from '@apollo/client'
-
-export const REFINE_FORM_INPUT = gql`
-  mutation refineFormInput($input: RefineFormInputInput!) {
-    refineFormInput(input: $input)
-  }
-`
