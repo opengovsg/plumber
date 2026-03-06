a0a564f7d55891b4b47f9c9d7babcb5e2b49ffb2
kevinkim-ogp <kevinkim@open.gov.sg>
2026-03-06 15:51:11 +0800

    fix: remove unused type

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -213,7 +213,6 @@ export interface IFlowConfig {
   attachments?: IFlowAttachmentsConfig[]
   // AI Builder config
   aiBuilderConfig?: {
-    type: string
     traceId: string // trace id on Rome (Langfuse)
   }
 }
