4e66a8630df0336cb3783eeb52a5f92bb4dc04db
kevinkim-ogp <kevinkim@open.gov.sg>
2026-03-30 13:38:29 +0800

    chore: update display name to Ownself Gather

--- packages/backend/src/apps/gathersg/index.ts
+++ packages/backend/src/apps/gathersg/index.ts
@@ -7,7 +7,7 @@ import dynamicData from './dynamic-data'
 import triggers from './triggers'
 
 const app: IApp = {
-  name: 'GatherSG',
+  name: 'Ownself Gather',
   key: 'gathersg',
   description: 'Case management system',
   iconUrl: '{BASE_URL}/apps/gathersg/assets/favicon.svg',
