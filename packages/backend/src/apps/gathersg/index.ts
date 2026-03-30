5454492edf66588839eada9aa68596a44ec09814
kevinkim-ogp <kevinkim@open.gov.sg>
2026-03-30 17:43:20 +0800

    chore: update authDocUrl to plumber guide

--- packages/backend/src/apps/gathersg/index.ts
+++ packages/backend/src/apps/gathersg/index.ts
@@ -11,7 +11,7 @@ const app: IApp = {
   key: 'gathersg',
   description: 'Case management system',
   iconUrl: '{BASE_URL}/apps/gathersg/assets/favicon.svg',
-  authDocUrl: 'https://gather.gov.sg/', // TODO: update this to our own guide when it is ready
+  authDocUrl: 'https://guide.plumber.gov.sg/user-guides/actions/ownself-gather',
   baseUrl: '',
   apiBaseUrl: 'https://gather.gov.sg/cms/api',
   primaryColor: '000000',
