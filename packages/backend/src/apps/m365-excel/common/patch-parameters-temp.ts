96ce852a1fb7d13f8ef376f465aebadf1429f471
kevinkim-ogp <kevinkim@open.gov.sg>
2026-04-14 13:58:12 +0800

    refactor: update step parameters in-memory

--- packages/backend/src/apps/m365-excel/common/patch-parameters-temp.ts
+++ packages/backend/src/apps/m365-excel/common/patch-parameters-temp.ts
@@ -8,27 +8,24 @@ import Step from '@/models/step'
  *
  * We manually patch the step's parameter's so that it contains the new 'filters' field.
  * this ensures that the 'filters' field is automatically populated when we move to this.
+ *
+ * We also update $.step.parameters in memory so that the filters are automatically populated
+ * in the ExecutionStep's dataIn when the test run is executed.
  */
 const patchParameters = async ($: IGlobalVariable) => {
   try {
-    //  manually patch the step's parameter's so that it contains the new 'filters' field.
+    // Transform in memory - add filters
+    $.step.parameters.filters = [
+      {
+        lookupColumn: $.step.parameters.lookupColumn,
+        lookupValue: $.step.parameters.lookupValue,
+      },
+    ]
+
+    // Persist to database
     await Step.query()
-      .patch({
-        parameters: {
-          ...$.step.parameters,
-          filters: [
-            {
-              lookupColumn: $.step.parameters.lookupColumn,
-              lookupValue: $.step.parameters.lookupValue,
-            },
-          ],
-        },
-      })
-      .where({
-        'steps.id': $.step.id,
-        'steps.flow_id': $.flow.id,
-        'steps.app_key': 'm365-excel',
-      })
+      .patch({ parameters: $.step.parameters })
+      .where({ 'steps.id': $.step.id })
       .throwIfNotFound()
   } catch (error) {
     // NOTE: we do not want to throw error here so that the test run can continue
