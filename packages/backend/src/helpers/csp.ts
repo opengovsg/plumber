f6a0ec1fcea2ef1fcb664e1033016e0dc12a19bf
m0nggh <malcolmong27@gmail.com>
2026-05-13 10:05:58 +0800

    add proxy for confetti

--- packages/backend/src/helpers/csp.ts
+++ packages/backend/src/helpers/csp.ts
@@ -17,8 +17,8 @@ const helmetOptions: HelmetOptions = {
         'https://*.launchdarkly.com',
         // For proxying datadog rum
         'https://rum-proxy.plumber.gov.sg',
-        // For Confetti Survey
-        'https://confetti.gov.sg',
+        // For proxying Confetti Survey
+        'https://confetti.plumber.gov.sg',
         // For S3 bucket
         'https://plumber-uat-attachment-bucket-private-0d9400e.s3.ap-southeast-1.amazonaws.com',
         'https://plumber-staging-attachment-bucket-private-ab28487.s3.ap-southeast-1.amazonaws.com',
--- packages/frontend/src/components/EditorLayout/ConfettiSurvey.tsx
+++ packages/frontend/src/components/EditorLayout/ConfettiSurvey.tsx
@@ -17,6 +17,7 @@ export function ConfettiSurvey() {
   return (
     <div className="confetti-survey">
       <PopoverConfetti
+        apiBaseUrl="https://confetti.plumber.gov.sg/api"
         surveyId={appConfig.confettiSurveyId}
         publishableKey={appConfig.confettiSurveyPublishableKey}
         respondent={`${userEmail}-${flowId}`} // key to maintain the state
