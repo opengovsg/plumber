212a5c31e59685529368db1f63a1f24ef34a9dd7
Ian Chen <ian@open.gov.sg>
2026-06-02 16:46:17 +0800

    chore: upgrade dd-trace and bullboard

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -30,6 +30,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      '/admin/queues': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
