453a3c3cd4de59fe64bf418c13efb36af18d01c5
kevinkim-ogp <kevinkim@open.gov.sg>
2026-06-09 21:29:41 +0800

    fix(archival): add NaN guards on numeric config, add timestamp+errors to logger

--- packages/backend/src/helpers/archival/logger.ts
+++ packages/backend/src/helpers/archival/logger.ts
@@ -6,10 +6,15 @@ const archivalLogger = winston.createLogger({
   level: archivalConfig.isDev ? 'debug' : 'info',
   format: archivalConfig.isDev
     ? winston.format.combine(
+        winston.format.errors({ stack: true }),
         winston.format.colorize(),
         winston.format.simple(),
       )
-    : winston.format.json(),
+    : winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
   transports: [new winston.transports.Console()],
 })
 
